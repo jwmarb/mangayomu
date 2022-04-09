f10e9d18c01e387a4e014f68a656063f9da7c52a
specify_ <dieptuber@gmail.com>
2022-04-09 11:04:57 -0700

    Created Root component

--- App.tsx
+++ App.tsx
@@ -2,23 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from '@redux/store';
+import Root from './src/Root';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style='auto' />
-      </View>
+      <Root />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
