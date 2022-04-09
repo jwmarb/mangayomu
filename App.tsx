20efe44f59ff940d23e2d173c5801db13f2fed8b
specify_ <dieptuber@gmail.com>
2022-04-09 16:26:57 -0700

    Added statusbar to show status bar in app

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ export default function App() {
     <Provider store={store}>
       <ThemeProvider theme={theme()}>
         <Root />
+        <StatusBar translucent />
       </ThemeProvider>
     </Provider>
   );
