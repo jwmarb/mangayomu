2a6ff856f9325e6a89f7b217403a4d0338a392f3
specify_ <dieptuber@gmail.com>
2022-04-10 16:10:15 -0700

    Added button component to exports

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -2,3 +2,4 @@ export * from '../Flex';
 export * from '../Typography';
 export { default as Icon } from '../Icon';
 export { default as Screen } from '../Screen';
+export { default as Button } from '../Button';
