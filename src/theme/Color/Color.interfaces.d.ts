f59f7f0ff551112cb0af4c703f1fde610ce18d65
specify_ <dieptuber@gmail.com>
2022-04-09 22:30:33 -0700

    Added types for constant colors

--- src/theme/Color/Color.interfaces.d.ts
+++ src/theme/Color/Color.interfaces.d.ts
@@ -30,4 +30,23 @@ export type ThemedPalette = {
   background: BackgroundColors;
 };
 
+export type ColorConstant = {
+  1: Color;
+  2: Color;
+  3: Color;
+  4: Color;
+  5: Color;
+  6: Color;
+  7: Color;
+  8: Color;
+  9: Color;
+  10: Color;
+};
+
+export type NeutralColorConstant = {
+  11: Color;
+  12: Color;
+  13: Color;
+} & ColorConstant;
+
 export type AppColors = keyof typeof applicableColors | 'textPrimary' | 'textSecondary' | Color;
