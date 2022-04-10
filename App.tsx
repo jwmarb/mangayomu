12bbd0fcf9affc25d7806c0cf74a410358755c0c
specify_ <dieptuber@gmail.com>
2022-04-09 21:08:48 -0700

    Added component dependencies

--- App.tsx
+++ App.tsx
@@ -1,18 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 import { Provider } from 'react-redux';
 import store from '@redux/store';
 import Root from './src/Root';
 import { ThemeProvider } from 'styled-components/native';
 import theme from '@theme/core';
+import ResourceLoader from '@utils/ResourceLoader';
+import 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { NavigationContainer, ThemeProvider as NavigationThemeProvider } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme()}>
-        <Root />
-        <StatusBar translucent />
-      </ThemeProvider>
-    </Provider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <ResourceLoader
+          onFinishedLoading={
+            <Provider store={store}>
+              <ThemeProvider theme={theme()}>
+                <Root />
+                <StatusBar translucent />
+              </ThemeProvider>
+            </Provider>
+          }
+        />
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
