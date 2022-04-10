71a70b8c9573e61d1b0a5b976afa1561aaeb8ae4
specify_ <dieptuber@gmail.com>
2022-04-09 22:30:23 -0700

    Added gray color constant

---
+++ src/theme/Color/Color.constants.ts
@@ -0,0 +1,18 @@
+import { ColorConstant, NeutralColorConstant } from '@theme/Color/Color.interfaces';
+import { Color } from '@theme/core';
+
+export const GRAY: NeutralColorConstant = {
+  1: Color.constant('#ffffff'),
+  2: Color.constant('#fafafa'),
+  3: Color.constant('#f5f5f5'),
+  4: Color.constant('#f0f0f0'),
+  5: Color.constant('#d9d9d9'),
+  6: Color.constant('#bfbfbf'),
+  7: Color.constant('#8c8c8c'),
+  8: Color.constant('#595959'),
+  9: Color.constant('#434343'),
+  10: Color.constant('#262626'),
+  11: Color.constant('#1f1f1f'),
+  12: Color.constant('#141414'),
+  13: Color.constant('#000000'),
+};
