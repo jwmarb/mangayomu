9c80e4fc5d4380919a0204f0a075461eba4cccde
specify_ <dieptuber@gmail.com>
2022-04-09 19:51:19 -0700

    Changed folder to suit purpose

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,7 @@ module.exports = function (api) {
             '@services': './src/services',
             '@redux': './src/redux',
             '@hooks': './src/hooks',
-            '@navigation': './src/navigation',
+            '@navigators': './src/navigators',
             '@screens': './src/screens',
             '@utils': './src/utils',
             '@theme': './src/theme',
