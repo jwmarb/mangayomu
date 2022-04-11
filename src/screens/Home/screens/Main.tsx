0f856639764d505612f08b142aab7ab9e2385303
EGGaming <dieptuber@gmail.com>
2022-04-11 09:59:09 -0700

    added main as subscreen of home

---
+++ src/screens/Home/screens/Main.tsx
@@ -0,0 +1,13 @@
+import Screen from '@components/Screen';
+import { Typography } from '@components/Typography';
+import React from 'react';
+
+const Main: React.FC = (props) => {
+  return (
+    <Screen>
+      <Typography>Hello World</Typography>
+    </Screen>
+  );
+};
+
+export default Main;
