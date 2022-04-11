0352fcf7b7eef2d46600d4e5bfe65a3725c7732a
EGGaming <dieptuber@gmail.com>
2022-04-11 10:28:27 -0700

    removed unused imports

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,7 +1,6 @@
 import { ScreenProps } from '@components/Screen/Screen.interfaces';
 import { Screen as View, ScrollableScreen as ScrollView } from './Screen.base';
 import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Screen: React.FC<ScreenProps> = (props) => {
   const { scrollable = false, ...rest } = props;
