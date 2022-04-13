726e608ed330215be2564ceb3e6bcd98ea636c49
EGGaming <dieptuber@gmail.com>
2022-04-13 13:59:29 -0700

    todo

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import RootStack from '@navigators/Root';
 import WelcomeScreen from '@screens/Welcome';
 import HomeScreen from '@screens/Home';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppState } from '@redux/store';
 
 const Root: React.FC = () => {
+  const dispatch = useDispatch();
+  const showIntroduction = useSelector((state: AppState) => state.settings.showIntroduction);
+  React.useLayoutEffect(() => {}, []);
   return (
     <RootStack.Navigator initialRouteName='Welcome' screenOptions={{ headerShown: false }}>
       <RootStack.Screen component={WelcomeScreen} name='Welcome' options={{ headerShown: false }} />
