8e4b6ec01245ceb622f90790a5520b2ab5539534
EGGaming <dieptuber@gmail.com>
2022-04-13 13:59:20 -0700

    Added types for redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
+
 import reducers from './reducers';
 import thunk from 'redux-thunk';
+import { useDispatch, useSelector } from 'react-redux';
 
 const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 export default store;
+
+export type AppDispatch = typeof store.dispatch;
+export type AppState = ReturnType<typeof store.getState>;
+export type UseSelector = typeof useSelector;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
