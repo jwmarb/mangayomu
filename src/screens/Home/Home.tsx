e3a4c1a8e16e1945f6c9e3d9cec9be12c715d73e
EGGaming <dieptuber@gmail.com>
2022-04-13 13:15:24 -0700

    Added a search button

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { StackHeader } from '@components/core';
+import { StackHeader, IconButton, Icon } from '@components/core';
 import HomeStack from '@navigators/Home';
 import React from 'react';
 import { HomeProps } from './Home.interfaces';
@@ -6,7 +6,13 @@ import Main from './screens/Main';
 
 const HomeScreen: React.FC<HomeProps> = (props) => {
   return (
-    <HomeStack.Navigator initialRouteName='Main' screenOptions={{ header: StackHeader }}>
+    <HomeStack.Navigator
+      initialRouteName='Main'
+      screenOptions={{
+        header: StackHeader,
+
+        headerRight: () => <IconButton icon={<Icon bundle='AntDesign' name='search1' />} />,
+      }}>
       <HomeStack.Screen component={Main} name='Main' options={{ headerTitle: 'Explore' }} />
     </HomeStack.Navigator>
   );
