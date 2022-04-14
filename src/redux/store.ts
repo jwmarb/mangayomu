cef354f458a61ce27706cf653a0dc173c362683f
specify_ <dieptuber@gmail.com>
2022-04-13 21:41:52 -0700

    converted to persistent state

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import reducers from './reducers';
 import thunk from 'redux-thunk';
 import { useDispatch, useSelector } from 'react-redux';
+import { PersistConfig, persistReducer } from 'redux-persist';
+const persistConfig: PersistConfig<AppState> = {
+  key: '@root',
+  storage: AsyncStorage,
+};
 
-const store = createStore(reducers, {}, applyMiddleware(thunk));
+const store = createStore(persistReducer<AppState>(persistConfig, reducers), {}, applyMiddleware(thunk));
 
 export default store;
 
 export type AppDispatch = typeof store.dispatch;
-export type AppState = ReturnType<typeof store.getState>;
+export type AppState = ReturnType<typeof reducers>;
 export type UseSelector = typeof useSelector;
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
