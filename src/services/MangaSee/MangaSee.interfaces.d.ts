3f1d6c50f9295e7ab63b30a06004bfa1ca70b42b
specify_ <dieptuber@gmail.com>
2022-04-14 00:44:27 -0700

    extracted types from website

---
+++ src/services/MangaSee/MangaSee.interfaces.d.ts
@@ -0,0 +1,37 @@
+export interface MangaSeeVMObject {
+  HotArr: string[];
+  SubArr: string[];
+  TopTenJSON: TopTenJSON[];
+  HotUpdateJSON: HotUpdateJSON[];
+  LatestJSON: LatestJSON[];
+}
+
+export interface TopTenJSON {
+  IndexName: string;
+  SeriesName: string;
+}
+
+export interface HotUpdateJSON {
+  Chapter: string;
+  Date: string;
+  IndexName: string;
+  IsEdd: boolean;
+  SeriesID: string;
+  SeriesName: string;
+}
+
+export interface LatestJSON {
+  Chapter: string;
+  Date: string;
+  Genres: string;
+  IndexName: string;
+  IsEdd: boolean;
+  ScanStatus: string;
+  SeriesID: string;
+  SeriesName: string;
+}
+
+export interface NewSeriesJSON {
+  IndexName: string;
+  SeriesName: string;
+}
