8a9e2692f7d678af11d3c0ae036e83b76fa90a7e
specify_ <dieptuber@gmail.com>
2022-04-13 21:47:29 -0700

    added PersistGate

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from '@redux/store';
+import store, { persistor } from '@redux/store';
 import Root from './src/Root';
 import { ThemeProvider } from 'styled-components/native';
 import theme from '@theme/core';
@@ -9,6 +9,7 @@ import ResourceLoader from '@utils/ResourceLoader';
 import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer, ThemeProvider as NavigationThemeProvider } from '@react-navigation/native';
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function App() {
   const generated = theme();
@@ -18,12 +19,14 @@ export default function App() {
         <ResourceLoader
           onFinishedLoading={
             <Provider store={store}>
-              <NavigationThemeProvider value={generated['@react-navigation']}>
-                <ThemeProvider theme={generated}>
-                  <Root />
-                  <StatusBar translucent />
-                </ThemeProvider>
-              </NavigationThemeProvider>
+              <PersistGate persistor={persistor}>
+                <NavigationThemeProvider value={generated['@react-navigation']}>
+                  <ThemeProvider theme={generated}>
+                    <Root />
+                    <StatusBar translucent />
+                  </ThemeProvider>
+                </NavigationThemeProvider>
+              </PersistGate>
             </Provider>
           }
         />
