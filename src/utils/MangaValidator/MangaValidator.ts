9037d8daffd0a8455e97ffd208da8a78a9085f5e
specify_ <dieptuber@gmail.com>
2022-04-15 11:53:07 -0700

    added instanceof interfaces

---
+++ src/utils/MangaValidator/MangaValidator.ts
@@ -0,0 +1,25 @@
+import { Manga, WithGenres, WithHentai, WithStatus, WithType } from '@services/scraper/scraper.interfaces';
+
+class MangaValidator {
+  public hasGenres(manga: any): manga is WithGenres {
+    return 'genre' in manga;
+  }
+
+  public isManga(obj: any): obj is Manga {
+    return 'title' in obj && 'imageCover' in obj && 'link' in obj;
+  }
+
+  public hasHentai(manga: any): manga is WithHentai {
+    return 'isHentai' in manga;
+  }
+
+  public hasType(manga: any): manga is WithType {
+    return 'type' in manga;
+  }
+
+  public hasStatus(manga: any): manga is WithStatus {
+    return 'status' in manga;
+  }
+}
+
+export default new MangaValidator();
