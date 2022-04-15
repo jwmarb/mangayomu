0a21800a0c1ccba49e51b173339d400de91b6788
specify_ <dieptuber@gmail.com>
2022-04-15 15:50:09 -0700

    added manga source button

--- src/components/Screen/Header/Header.tsx
+++ src/components/Screen/Header/Header.tsx
@@ -1,12 +1,12 @@
-import { StackHeaderProps } from '@react-navigation/stack';
 import React from 'react';
 import { HeaderBaseContainer } from './Header.base';
 import { Typography } from '../../Typography';
 import IconButton from '../../IconButton';
-import Icon from '../../Icon';
 import Spacer from '../../Spacer';
 import Flex from '../../Flex';
 import { BottomTabHeaderProps } from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native';
+import MangaSee from '@services/MangaSee';
 
 const Header: React.FC<BottomTabHeaderProps> = (props) => {
   const {
@@ -17,6 +17,8 @@ const Header: React.FC<BottomTabHeaderProps> = (props) => {
 
   return (
     <HeaderBaseContainer>
+      <IconButton icon={<Image source={{ uri: MangaSee.icon }} style={{ width: 24, height: 24 }} />} />
+      <Spacer x={1} />
       <Typography variant='subheader'>{headerTitle}</Typography>
       <Flex grow justifyContent='flex-end'>
         {HeaderRightComponent && <HeaderRightComponent />}
