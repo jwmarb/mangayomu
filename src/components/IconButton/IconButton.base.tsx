7d3d9e6244073cfac121ab42e8a264c5f29f593f
specify_ <dieptuber@gmail.com>
2022-04-15 15:13:25 -0700

    removed redundant styling

--- src/components/IconButton/IconButton.base.tsx
+++ src/components/IconButton/IconButton.base.tsx
@@ -3,7 +3,6 @@ import styled, { css } from 'styled-components/native';
 
 export const IconButtonBaseContainer = styled.View`
   ${(props) => css`
-    padding: ${props.theme.spacing(1)};
     width: 40px;
     align-items: center;
     justify-content: center;
