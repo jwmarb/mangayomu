a6ce65487be03d519b67a49bf03b8a4286c7ee60
specify_ <dieptuber@gmail.com>
2022-04-16 00:07:20 -0700

    refactored to put any scrollable content

--- src/components/Category/CategoryHeader/CategoryHeader.tsx
+++ src/components/Category/CategoryHeader/CategoryHeader.tsx
@@ -1,10 +1,23 @@
+import { ScrollView } from 'react-native-gesture-handler';
 import styled, { css } from 'styled-components/native';
-export const CategoryHeader = styled.View`
+const CategoryHeaderBase = styled.View`
   ${(props) => css`
     padding-horizontal: ${props.theme.spacing(3)};
-    display: flex;
+    flex-grow: 1;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
   `}
 `;
+
+export const CategoryHeader: React.FC = ({ children }) => {
+  return (
+    <ScrollView
+      horizontal
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsHorizontalScrollIndicator={false}
+      showsVerticalScrollIndicator={false}>
+      <CategoryHeaderBase>{children}</CategoryHeaderBase>
+    </ScrollView>
+  );
+};
