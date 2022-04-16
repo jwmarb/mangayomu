6cc4cabfefc573fd86a1eb6bd0fe70d7e826d47d
specify_ <dieptuber@gmail.com>
2022-04-15 22:02:47 -0700

    added to show latest manga

--- src/screens/Home/screens/Explore/Explore.tsx
+++ src/screens/Home/screens/Explore/Explore.tsx
@@ -1,11 +1,22 @@
+import Flex from '@components/Flex';
 import Screen from '@components/Screen';
+import { AppState } from '@redux/store';
 import Genres from '@screens/Home/screens/Explore/components/Genres';
+import HotManga from '@screens/Home/screens/Explore/components/HotManga';
+import LatestManga from '@screens/Home/screens/Explore/components/LatestManga';
+import { useMangaSource } from '@services/scraper';
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Explore: React.FC = (props) => {
+  const source = useMangaSource();
   return (
     <Screen>
-      <Genres />
+      <Flex direction='column' spacing={2}>
+        {source.genres.length > 0 && <Genres />}
+        {source.hasHotMangas && <HotManga />}
+        {source.hasLatestMangas && <LatestManga />}
+      </Flex>
     </Screen>
   );
 };
---
+++ src/screens/Home/screens/Explore/components/LatestManga/LatestManga.tsx
@@ -0,0 +1,45 @@
+import { Button, Category, Icon, Spacer, Typography } from '@components/core';
+import Flex from '@components/Flex';
+import useAPICall from '@hooks/useAPICall';
+import { keyExtractor, renderItem } from '@screens/Home/screens/Explore/components/HotManga/HotManga.flatlist';
+import { useMangaSource } from '@services/scraper';
+import React from 'react';
+
+const HotManga: React.FC = (props) => {
+  const {} = props;
+  const source = useMangaSource();
+  const {
+    state: [mangas, setMangas],
+    loading,
+    error,
+    refresh,
+  } = useAPICall(() => source.listRecentlyUpdatedManga());
+
+  return (
+    <Flex direction='column'>
+      <Category.Header>
+        <Typography variant='subheader'>
+          Recently updated <Icon bundle='MaterialCommunityIcons' name='update' color='secondary' />
+        </Typography>
+        <Button title='View All' />
+      </Category.Header>
+      <Spacer y={1} />
+      {loading ? (
+        <Typography>Loading...</Typography>
+      ) : error ? (
+        <Typography>{error}</Typography>
+      ) : (
+        <Category.FlatList
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+          horizontal
+          data={mangas}
+          renderItem={renderItem}
+          keyExtractor={keyExtractor}
+        />
+      )}
+    </Flex>
+  );
+};
+
+export default HotManga;
