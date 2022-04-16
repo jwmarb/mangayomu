7cedb27727ebb87076985669d1fa86d651fdbeff
specify_ <dieptuber@gmail.com>
2022-04-15 23:55:38 -0700

    added to exports

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -11,3 +11,16 @@ export { Tabs as StackTabs } from '../Screen';
 export { default as Genre } from '../Genre';
 export { default as Category } from '../Category';
 export { default as MangaComponent } from '../Manga';
+import { MangaSkeleton } from '@components/Manga';
+import { TypographySkeleton } from '@components/Typography';
+export const Skeleton = {
+  MangaComponent: MangaSkeleton,
+  Typography: TypographySkeleton,
+};
+import { MangaPlaceholder } from '@components/Manga';
+/**
+ * The difference between Placeholder and Skeleton is that Placeholder is meant for lists
+ */
+export const Placeholder = {
+  MangaComponent: MangaPlaceholder,
+};
