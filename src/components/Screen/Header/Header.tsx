3f3cddd4e305335f7c3bb146087999631dcc08ff
specify_ <dieptuber@gmail.com>
2022-04-16 01:32:19 -0700

    added header component for stack screen

--- src/components/Screen/Header/Header.tsx
+++ src/components/Screen/Header/Header.tsx
@@ -10,8 +10,10 @@ import { Image } from 'react-native';
 import IconButton from '../../IconButton';
 import { MenuItemProps } from 'react-native-hold-menu/lib/typescript/components/menu/types';
 import { Linking } from 'react-native';
+import { StackHeaderProps } from '@react-navigation/stack';
+import Icon from '@components/Icon';
 
-const Header: React.FC<BottomTabHeaderProps> = (props) => {
+const BottomTabHeader: React.FC<BottomTabHeaderProps> = (props) => {
   const {
     options: { headerTitle = props.route.name, headerRight },
     navigation,
@@ -30,4 +32,40 @@ const Header: React.FC<BottomTabHeaderProps> = (props) => {
   );
 };
 
-export default (props: BottomTabHeaderProps) => <Header {...props} />;
+export const _BottomTabHeader = (props: BottomTabHeaderProps) => <BottomTabHeader {...props} />;
+
+const Header: React.FC<StackHeaderProps> = (props) => {
+  const {
+    options: { headerTitle = props.route.name, headerRight, headerLeft },
+    navigation,
+  } = props;
+  const HeaderRightComponent = headerRight as React.FC;
+  const HeaderLeftComponent = headerLeft as React.FC;
+
+  function handleOnPress() {
+    navigation.goBack();
+  }
+
+  return (
+    <HeaderBaseContainer>
+      {navigation.canGoBack() && (
+        <>
+          <IconButton icon={<Icon bundle='AntDesign' name='back' />} onPress={handleOnPress} />
+          <Spacer x={1} />
+        </>
+      )}
+      {HeaderLeftComponent && (
+        <>
+          <HeaderLeftComponent />
+          <Spacer x={1} />
+        </>
+      )}
+      <Typography variant='subheader'>{headerTitle}</Typography>
+      <Flex grow justifyContent='flex-end'>
+        {HeaderRightComponent && <HeaderRightComponent />}
+      </Flex>
+    </HeaderBaseContainer>
+  );
+};
+
+export const _Header = (props: StackHeaderProps) => <Header {...props} />;
