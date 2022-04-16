ab6a6f1d05a7dc5744be2a71af8441779c23505a
specify_ <dieptuber@gmail.com>
2022-04-15 17:27:44 -0700

    added home screen

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,8 +1,14 @@
 import { StackHeader, IconButton, Icon, StackTabs } from '@components/core';
+import { IconPack, IconProps } from '@components/Icon/Icon.interfaces';
 import BottomTab from '@navigators/BottomTab';
+import Browse from '@screens/Home/screens/Browse';
 import React from 'react';
 import { HomeProps } from './Home.interfaces';
-import Main from './screens/Main';
+import Main from './screens/Explore';
+
+function createTabIcon<T extends IconPack>(bundle: T, name: IconProps<T>['name']) {
+  return (props: any) => <Icon {...props} bundle={bundle} name={name} />;
+}
 
 const HomeScreen: React.FC<HomeProps> = (props) => {
   return (
@@ -10,11 +16,37 @@ const HomeScreen: React.FC<HomeProps> = (props) => {
       initialRouteName='Explore'
       screenOptions={{
         header: StackHeader,
-
-        headerRight: () => <IconButton icon={<Icon bundle='AntDesign' name='search1' />} />,
       }}
       tabBar={StackTabs}>
-      <BottomTab.Screen component={Main} name='Explore' options={{ headerTitle: 'Explore' }} />
+      <BottomTab.Screen
+        component={Main}
+        name='Explore'
+        options={{ headerTitle: 'Explore', tabBarIcon: createTabIcon('Feather', 'home') }}
+      />
+      <BottomTab.Screen
+        component={Browse}
+        name='Library'
+        options={{
+          headerTitle: 'Library',
+          tabBarIcon: createTabIcon('Feather', 'bookmark'),
+          headerRight: () => <IconButton icon={<Icon bundle='AntDesign' name='search1' />} />,
+        }}
+      />
+      <BottomTab.Screen
+        component={Browse}
+        name='Browse'
+        options={{ headerTitle: 'Browse', tabBarIcon: createTabIcon('Feather', 'search') }}
+      />
+      <BottomTab.Screen
+        component={Browse}
+        name='History'
+        options={{ headerTitle: 'History', tabBarIcon: createTabIcon('Feather', 'clock') }}
+      />
+      <BottomTab.Screen
+        component={Browse}
+        name='Settings'
+        options={{ headerTitle: 'Settings', tabBarIcon: createTabIcon('Feather', 'settings') }}
+      />
     </BottomTab.Navigator>
   );
 };
