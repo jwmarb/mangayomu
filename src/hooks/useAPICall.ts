74733ab06644c7bec2304a8915d0f7c36a154690
specify_ <dieptuber@gmail.com>
2022-04-15 21:21:04 -0700

    added to improve dev experience

---
+++ src/hooks/useAPICall.ts
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function useAPICall<T>(apiCall: () => Promise<T[]>) {
+  const [items, setItems] = React.useState<T[]>([]);
+  const [error, setError] = React.useState<string>('');
+  const [loading, setLoading] = React.useState<boolean>(true);
+  React.useEffect(() => {
+    refresh();
+  }, []);
+  async function refresh() {
+    setLoading(true);
+    try {
+      const response = await apiCall();
+      setItems(response);
+    } catch (e) {
+      setError(e as any);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return {
+    state: [items, setItems],
+    error,
+    loading,
+    refresh,
+  };
+}
