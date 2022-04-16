0349eedc60b56831fcee13e002d68747152cd68f
specify_ <dieptuber@gmail.com>
2022-04-15 20:52:37 -0700

    added support for evenly spaced components

--- src/components/Category/CategoryHeader/CategoryHeader.tsx
+++ src/components/Category/CategoryHeader/CategoryHeader.tsx
@@ -2,5 +2,9 @@ import styled, { css } from 'styled-components/native';
 export const CategoryHeader = styled.View`
   ${(props) => css`
     padding-horizontal: ${props.theme.spacing(3)};
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
   `}
 `;
