a2c71bc4fec97ca196acf76c1a67a2b66be61cd3
specify_ <dieptuber@gmail.com>
2022-04-16 17:51:02 -0700

    finished getmeta to get manga information

--- src/services/MangaSee/MangaSee.ts
+++ src/services/MangaSee/MangaSee.ts
@@ -3,12 +3,15 @@ import {
   Directory,
   HotUpdateJSON,
   LatestJSON,
+  MainEntityJSON,
+  MangaSeeChapterJSON,
   MangaSeeFilter,
   MangaSeeManga,
+  MangaSeeMangaMeta,
 } from '@services/MangaSee/MangaSee.interfaces';
-import { processScript } from '@services/MangaSee/MangaSee.utils';
+import { extractDataFromApplicationLDJson, processScript } from '@services/MangaSee/MangaSee.utils';
 import { MangaHostWithFilters, MangaSortType } from '@services/scraper/scraper.filters';
-import { Manga, MangaChapter } from '@services/scraper/scraper.interfaces';
+import { Manga, MangaChapter, MangaMeta } from '@services/scraper/scraper.interfaces';
 import titleIncludes from '@utils/MangaFilters';
 
 class MangaSee extends MangaHostWithFilters<MangaSeeFilter> {
@@ -22,7 +25,7 @@ class MangaSee extends MangaHostWithFilters<MangaSeeFilter> {
 
     return Promise.resolve<Manga[]>(
       LatestJSON.map((x) => ({
-        link: `https://${super.getLink()}/read-online/${x.IndexName}${ChapterURLEncode(x.Chapter)}`,
+        link: `https://${super.getLink()}/manga/${x.IndexName}`,
         title: x.SeriesName,
         imageCover: `https://cover.nep.li/cover/${x.IndexName}.jpg`,
         source: super.name,
@@ -38,7 +41,7 @@ class MangaSee extends MangaHostWithFilters<MangaSeeFilter> {
     const HotUpdateJSON = variable<HotUpdateJSON[]>('vm.HotUpdateJSON');
     return Promise.resolve<Manga[]>(
       HotUpdateJSON.map((x) => ({
-        link: `https://${super.getLink()}/read-online/${x.IndexName}${ChapterURLEncode(x.Chapter)}`,
+        link: `https://${super.getLink()}/manga/${x.IndexName}`,
         title: x.SeriesName,
         imageCover: `https://cover.nep.li/cover/${x.IndexName}.jpg`,
         source: super.name,
@@ -73,8 +76,51 @@ class MangaSee extends MangaHostWithFilters<MangaSeeFilter> {
     return this.memo;
   }
 
-  getMeta(manga: Manga) {
-    return Promise.resolve({} as any);
+  public async getMeta(manga: Manga): Promise<MangaSeeMangaMeta> {
+    const $ = await super.route({ url: manga.link });
+    const html = $.html();
+    const { mainEntity: data } = extractDataFromApplicationLDJson<MainEntityJSON>(html);
+    const { variable, fn } = processScript(html);
+    const Chapters = variable<MangaSeeChapterJSON[]>('vm.Chapters');
+    const ChapterDisplay = fn<(e: string) => string>('vm.ChapterDisplay');
+
+    /**
+     * Taken directly from https://mangasee123.com
+     */
+    const ChapterURLEncode = new Function(`var v = function(ChapterString){
+      Index = "";
+      var IndexString = ChapterString.substring(0, 1);
+      if(IndexString != 1){
+        Index = "-index-"+IndexString;
+      }
+
+      var Chapter = parseInt(ChapterString.slice(1,-1));
+
+      var Odd = "";
+      var OddString = ChapterString[ChapterString.length -1];
+      if(OddString != 0){
+        Odd = "." + OddString;
+      }
+      
+      return "-chapter-" + Chapter + Odd + Index;
+    }; return v`)();
+    const IndexName = variable<string>('vm.IndexName');
+    const description = $('span.mlabel').siblings('div.Content').text();
+
+    return {
+      authors: data.author,
+      genres: data.genre,
+      description,
+      date: {
+        modified: data.dateModified,
+        published: data.datePublished,
+      },
+      chapters: Chapters.map((chapter) => ({
+        date: chapter.Date,
+        name: `${chapter.Type != '' ? chapter.Type : 'Chapter'} ${ChapterDisplay(chapter.Chapter)}`,
+        link: `https://${super.getLink()}/read-online/${IndexName}${ChapterURLEncode(chapter.Chapter)}`,
+      })),
+    };
   }
 
   getPages(chapter: MangaChapter) {
