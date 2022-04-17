33faa765b208c76a2c46b9dd691078749e705173
specify_ <dieptuber@gmail.com>
2022-04-16 22:49:16 -0700

    added to improve the process of making UIs faster

---
+++ src/utils/animate.ts
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * Animate an element without creating a new file or calling useAnimated hook
+ * @param element The element that needs an animation
+ * @param preset The animation preset
+ * @returns Returns the element but animated
+ */
+export default function animate(
+  element: React.ReactElement,
+  preset: (component: React.FC) => React.FC
+): React.ReactElement {
+  return React.createElement(preset(() => element));
+}
