fca3dcd5bf3226a8bf24a946554478b4c7fecf6a
specify_ <dieptuber@gmail.com>
2022-04-16 20:27:15 -0700

    added conditional animation start

--- src/hooks/useAnimatedMounting.tsx
+++ src/hooks/useAnimatedMounting.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useSharedValue, withSpring, withTiming, useAnimatedStyle } from 'react-native-reanimated';
 
-export default function useAnimatedMounting() {
+export default function useAnimatedMounting(condition: boolean = true) {
   const opacity = useSharedValue(0);
   const transformY = useSharedValue(-10);
   React.useEffect(() => {
-    opacity.value = withSpring(1);
-    transformY.value = withTiming(0, { duration: 500 });
-  }, []);
+    if (condition) {
+      opacity.value = withSpring(1);
+      transformY.value = withTiming(0, { duration: 500 });
+    }
+  }, [condition]);
 
   const style = useAnimatedStyle(() => ({
     opacity: opacity.value,
