93e1e5533e21084dd937b10e9f693f07e6b8481a
specify_ <dieptuber@gmail.com>
2022-04-16 19:45:50 -0700

    changed to be animated

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,6 +1,7 @@
 import styled, { css } from 'styled-components/native';
+import Animated from 'react-native-reanimated';
 
-export const Container = styled.View`
+export const Container = styled(Animated.View)`
   ${(props) => css`
     padding-horizontal: ${props.theme.spacing(3)};
   `}
