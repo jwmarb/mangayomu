ad79ee586333a3b97b5ebac1b30f1105890638fc
specify_ <dieptuber@gmail.com>
2022-04-16 23:42:31 -0700

    added more types

--- src/redux/reducers/mangaReducer/mangaReducer.interfaces.d.ts
+++ src/redux/reducers/mangaReducer/mangaReducer.interfaces.d.ts
@@ -1,4 +1,38 @@
-import { MangaMeta } from '@services/scraper/scraper.interfaces';
+import { MangaChapter, MangaMeta, MangaPage } from '@services/scraper/scraper.interfaces';
+
+export interface ReadingChapterInfo extends MangaChapter {
+  /**
+   * The scroll position of the reader
+   */
+  scrollPosition: number;
+
+  /**
+   * An array of manga pages
+   */
+  pages: MangaPage[];
+
+  /**
+   * The index of the current page the user is on
+   */
+  indexPage: number;
+}
+
+export interface ReadingMangaInfo extends MangaMeta {
+  /**
+   * Determines whether or not the user has the manga added in their library
+   */
+  inLibrary: boolean;
+
+  /**
+   * The chapters of the manga
+   */
+  chapters: MangaChapter[];
+
+  /**
+   * The current chapter the user is reading
+   */
+  currentlyReadingChapter: MangaChapter | null;
+}
 
 export type MangaReducerState = Record<string, MangaMeta>;
 
