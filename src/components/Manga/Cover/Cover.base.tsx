02e6f807de4f98268e50e183981aa70ea9a55363
specify_ <dieptuber@gmail.com>
2022-04-16 17:52:32 -0700

    refactoredto fit on screen

--- src/components/Manga/Cover/Cover.base.tsx
+++ src/components/Manga/Cover/Cover.base.tsx
@@ -16,8 +16,28 @@ export const MangaCoverBase = styled.Image<Omit<MangaCoverProps, 'uri'>>`
         `;
       case 'large':
         return css`
-          width: ${RFValue(pixelToNumber(props.theme.spacing(26)))}px;
-          height: ${RFValue(pixelToNumber(props.theme.spacing(40)))}px;
+          width: ${RFValue(pixelToNumber(props.theme.spacing(16)))}px;
+          height: ${RFValue(pixelToNumber(props.theme.spacing(24.6153846154)))}px;
+        `;
+    }
+  }}
+`;
+
+export const MangaCoverBaseContainer = styled.View<Omit<MangaCoverProps, 'uri'>>`
+  ${(props) => css`
+    border-radius: ${props.theme.borderRadius}px;
+  `}
+  ${(props) => {
+    switch (props.size) {
+      case 'medium':
+        return css`
+          width: ${RFValue(pixelToNumber(props.theme.spacing(13)))}px;
+          height: ${RFValue(pixelToNumber(props.theme.spacing(20)))}px;
+        `;
+      case 'large':
+        return css`
+          width: ${RFValue(pixelToNumber(props.theme.spacing(16)))}px;
+          height: ${RFValue(pixelToNumber(props.theme.spacing(24.6153846154)))}px;
         `;
     }
   }}
--- src/components/Manga/Cover/Cover.tsx
+++ src/components/Manga/Cover/Cover.tsx
@@ -1,10 +1,14 @@
-import { MangaCoverBase } from '@components/Manga/Cover/Cover.base';
+import { MangaCoverBase, MangaCoverBaseContainer } from '@components/Manga/Cover/Cover.base';
 import { MangaCoverProps } from '@components/Manga/Cover/Cover.interfaces';
 import React from 'react';
 
 const Cover: React.FC<MangaCoverProps> = (props) => {
   const { size = 'medium', uri } = props;
-  return <MangaCoverBase source={{ uri }} size={size} />;
+  return (
+    <MangaCoverBaseContainer size={size}>
+      <MangaCoverBase source={{ uri }} size={size} />
+    </MangaCoverBaseContainer>
+  );
 };
 
 export default Cover;
