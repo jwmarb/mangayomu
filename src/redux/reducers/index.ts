975d389d261856340c038068015fba196b99239f
specify_ <dieptuber@gmail.com>
2022-04-17 13:13:50 -0700

    exported new reducer

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,3 +1,4 @@
+import mangalibReducer from '@redux/reducers/mangalibReducer/mangalibReducer';
 import { combineReducers } from 'redux';
 import mangaReducer from './mangaReducer';
 import settingsReducer from './settingsReducer';
@@ -5,6 +6,7 @@ import settingsReducer from './settingsReducer';
 const reducers = combineReducers({
   mangas: mangaReducer,
   settings: settingsReducer,
+  library: mangalibReducer,
 });
 
 export default reducers;
