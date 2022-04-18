74c3a9e608e20fd66254a63cab3e6c350105fb22
specify_ <dieptuber@gmail.com>
2022-04-17 20:27:35 -0700

    experiment for ui look

--- src/screens/Home/screens/MangaViewer/MangaViewer.base.tsx
+++ src/screens/Home/screens/MangaViewer/MangaViewer.base.tsx
@@ -4,5 +4,6 @@ import styled, { css } from 'styled-components/native';
 export const MangaViewerContainer = styled.View`
   ${(props) => css`
     padding: ${props.theme.spacing(3)};
+    background-color: ${props.theme.palette.background.default.get()};
   `}
 `;
