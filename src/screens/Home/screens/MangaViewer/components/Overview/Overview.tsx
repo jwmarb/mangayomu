6d804e508e175390c13b0a2a2907e768bb3a5171
specify_ <dieptuber@gmail.com>
2022-04-17 22:10:40 -0700

    created floating action button

--- src/screens/Home/screens/MangaViewer/components/Overview/Overview.tsx
+++ src/screens/Home/screens/MangaViewer/components/Overview/Overview.tsx
@@ -1,7 +1,15 @@
+import ButtonBase from '@components/Button/ButtonBase';
+import { Icon } from '@components/core';
+import { Typography } from '@components/Typography';
+import {
+  FloatingActionButton,
+  FloatingActionButtonContainer,
+  FloatingContainer,
+} from '@screens/Home/screens/MangaViewer/components/Overview/Overview.base';
 import { renderItem, keyExtractor } from '@screens/Home/screens/MangaViewer/components/Overview/Overview.flat.list';
 import { OverviewProps } from '@screens/Home/screens/MangaViewer/components/Overview/Overview.interfaces';
 import React from 'react';
-import { Animated } from 'react-native';
+import { Animated, View } from 'react-native';
 
 const Overview: React.FC<OverviewProps> = (props) => {
   const {
@@ -11,18 +19,29 @@ const Overview: React.FC<OverviewProps> = (props) => {
   } = props;
 
   return (
-    <Animated.FlatList
-      keyExtractor={keyExtractor}
-      scrollIndicatorInsets={{ top: scrollIndicatorInsetTop }}
-      contentContainerStyle={{ paddingTop: containerPaddingTop }}
-      onScroll={onScroll}
-      renderItem={renderItem}
-      data={chapters}
-      initialNumToRender={8}
-      maxToRenderPerBatch={20}
-      windowSize={7}
-      ListHeaderComponent={<>{children}</>}
-    />
+    <>
+      <FloatingContainer>
+        <FloatingActionButtonContainer>
+          <ButtonBase round onPress={() => {}} color='primary'>
+            <FloatingActionButton>
+              <Icon bundle='MaterialCommunityIcons' name='play' />
+            </FloatingActionButton>
+          </ButtonBase>
+        </FloatingActionButtonContainer>
+      </FloatingContainer>
+      <Animated.FlatList
+        keyExtractor={keyExtractor}
+        scrollIndicatorInsets={{ top: scrollIndicatorInsetTop }}
+        contentContainerStyle={{ paddingTop: containerPaddingTop }}
+        onScroll={onScroll}
+        renderItem={renderItem}
+        data={chapters}
+        initialNumToRender={8}
+        maxToRenderPerBatch={20}
+        windowSize={7}
+        ListHeaderComponent={<>{children}</>}
+      />
+    </>
   );
 };
 
