dddaf19f9c7dc51f2e6a7055cbf4830b57bbb0ef
EGGaming <dieptuber@gmail.com>
2022-04-19 16:45:43 -0700

    todo

--- src/utils/Algorithms/Comparator/Comparator.ts
+++ src/utils/Algorithms/Comparator/Comparator.ts
@@ -1,11 +1,16 @@
 import { Comparator } from './Comparator.interfaces';
 
 export const StringComparator: Comparator<string, string> = (item1, item2) => {
-  return item2.localeCompare(item1);
+  return item1.localeCompare(item2);
 };
 
-export const NumberComparator: Comparator<number, number> = (item1, elementToFind) => {
-  if (item1 > elementToFind) return -1;
-  else if (item1 === elementToFind) return 0;
+export const NumberComparator: Comparator<number, number> = (elementToFind, item2) => {
+  if (item2 > elementToFind) return -1;
+  else if (item2 === elementToFind) return 0;
   else return 1;
+};
+
+export const StringAlphabeticalComparator: Comparator<string, string> = (item1, item2) => {
+  if (item2.toLowerCase().trim().startsWith(item2.toLowerCase().trim())) return 0;
+  return item1.localeCompare(item2);
 };
--- src/utils/Algorithms/binarySearch.ts
+++ src/utils/Algorithms/binarySearch.ts
@@ -1,6 +1,6 @@
 import { Comparator } from './Comparator/Comparator.interfaces';
 
-export default function binarySearch<T, E>(arr: T[], elementToFind: E, comparator: Comparator<T, E>): number {
+export function binarySearch<T, E>(arr: T[], elementToFind: E, comparator: Comparator<T, E>): number {
   let l = 0;
   let r = arr.length - 1;
   while (l < r) {
@@ -10,6 +10,19 @@ export default function binarySearch<T, E>(arr: T[], elementToFind: E, comparato
     else if (comparison > 0) l = m + 1;
     else r = m - 1;
   }
+  return -1;
+}
 
+export function lowerBound<T, E>(arr: T[], elementToFind: E, comparator: Comparator<T, E>): number {
+  let l = 0;
+  let r = arr.length - 1;
+  while (l < r) {
+    const m = Math.floor((l + r) / 2);
+    const comparison = comparator(arr[m], elementToFind);
+    if (comparison === 0) {
+      return m;
+    } else if (comparison > 0) l = m + 1;
+    else r = m - 1;
+  }
   return -1;
 }
