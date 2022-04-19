134f0cfa63f305ebda5350117ec1f6d2f97554cd
EGGaming <dieptuber@gmail.com>
2022-04-19 10:37:32 -0700

    shorter animation = good ux

--- src/hooks/useAnimatedFadeIn.tsx
+++ src/hooks/useAnimatedFadeIn.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useAnimatedStyle, useSharedValue, withTiming, Easing } from 'react-native-reanimated';
 
 export default function useAnimatedFadeIn() {
-  const opacity = useSharedValue(0.5);
+  const opacity = useSharedValue(0);
   React.useEffect(() => {
-    opacity.value = withTiming(1, { duration: 1000, easing: Easing.ease });
+    opacity.value = withTiming(1, { duration: 500, easing: Easing.ease });
   }, []);
   const style = useAnimatedStyle(() => ({
     opacity: opacity.value,
