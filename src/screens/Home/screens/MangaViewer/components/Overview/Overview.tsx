432b10cdae00fe1918ccee967d26f8c8caeec242
specify_ <dieptuber@gmail.com>
2022-04-18 19:36:45 -0700

    added to improve flatlist performance

--- src/screens/Home/screens/MangaViewer/components/Overview/Overview.tsx
+++ src/screens/Home/screens/MangaViewer/components/Overview/Overview.tsx
@@ -2,12 +2,26 @@ import ButtonBase from '@components/Button/ButtonBase';
 import { Icon, Skeleton, Flex } from '@components/core';
 import { AnimatedProvider } from '@context/AnimatedContext';
 import useAnimatedLoading from '@hooks/useAnimatedLoading';
+import { ReadingChapterInfo } from '@redux/reducers/mangaReducer/mangaReducer.interfaces';
 import { LoadingContainer } from '@screens/Home/screens/MangaViewer/components/Overview/Overview.base';
 import { renderItem, keyExtractor } from '@screens/Home/screens/MangaViewer/components/Overview/Overview.flat.list';
 import { OverviewProps } from '@screens/Home/screens/MangaViewer/components/Overview/Overview.interfaces';
 import React from 'react';
 import { Animated, NativeScrollEvent, NativeSyntheticEvent, View } from 'react-native';
 import FloatingActionButton from '../FloatingActionButton/FloatingActionButton';
+
+const getItemLayout: (
+  data: ReadingChapterInfo[] | null | undefined,
+  index: number
+) => {
+  length: number;
+  offset: number;
+  index: number;
+} = (data, index) => ({
+  length: 70.0952377319336,
+  offset: 70.0952377319336 * index,
+  index,
+});
 
 const Overview: React.FC<OverviewProps> = (props) => {
   const {
@@ -38,9 +52,10 @@ const Overview: React.FC<OverviewProps> = (props) => {
         contentContainerStyle={{ paddingTop: containerPaddingTop }}
         onScroll={onScroll}
         renderItem={renderItem}
+        getItemLayout={getItemLayout}
         data={chapters}
         initialNumToRender={8}
-        maxToRenderPerBatch={20}
+        maxToRenderPerBatch={12}
         ListEmptyComponent={
           <AnimatedProvider style={loadingAnim}>
             <LoadingContainer>
