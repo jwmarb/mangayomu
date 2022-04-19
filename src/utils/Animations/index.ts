2e65efcb5f3c067a91b185f1f57a22f4d90635eb
EGGaming <dieptuber@gmail.com>
2022-04-19 10:35:23 -0700

    added to barrel file

--- src/utils/Animations/index.ts
+++ src/utils/Animations/index.ts
@@ -1,3 +1,4 @@
 export { default as withAnimatedFadeIn } from './withAnimatedFadeIn';
 export { default as withAnimatedLoading } from './withAnimatedLoading';
 export { default as withAnimatedMounting } from './withAnimatedMounting';
+export { default as animate } from './animate';
