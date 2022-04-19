b77da56f9d77296351169b284f3a501aeffeac3f
EGGaming <dieptuber@gmail.com>
2022-04-19 09:23:20 -0700

    added documentation

--- src/utils/CancelablePromise/CancelablePromise.ts
+++ src/utils/CancelablePromise/CancelablePromise.ts
@@ -1,3 +1,6 @@
+/**
+ * A Promise wrapper that converts a regular Promise into a cancellable one. This should only be used for components performing asynchronous tasks (such as API requests) so that when the promise is fulfilled, the component should not rerender when it is unmounted
+ */
 export default class CancelablePromise<T> {
   private _isCanceled: boolean;
   private promise: Promise<T>;
@@ -11,6 +14,10 @@ export default class CancelablePromise<T> {
     this._isCanceled = false;
   }
 
+  /**
+   * Create/start a promise to perform an asynchronous operation
+   * @returns Returns the promise
+   */
   public start() {
     return this.promise;
   }
@@ -19,6 +26,9 @@ export default class CancelablePromise<T> {
     return this._isCanceled;
   }
 
+  /**
+   * Cancels the promise
+   */
   public cancel() {
     this._isCanceled = true;
   }
