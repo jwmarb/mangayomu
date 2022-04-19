59595b200c1f740b8b53a5ab6dc0937a756d47eb
EGGaming <dieptuber@gmail.com>
2022-04-19 10:09:46 -0700

    redirect to manga list

--- src/components/Genre/Genre.tsx
+++ src/components/Genre/Genre.tsx
@@ -2,6 +2,7 @@ import ButtonBase from '@components/Button/ButtonBase';
 import { GenreContainer } from '@components/Genre/Genre.base';
 import { GenreProps } from '@components/Genre/Genre.interfaces';
 import { Typography } from '@components/Typography';
+import { useRootNavigation } from '@navigators/Root';
 import { AppState } from '@redux/store';
 import { useMangaSource } from '@services/scraper';
 import MangaHost from '@services/scraper/scraper.abstract';
@@ -12,10 +13,11 @@ import { useSelector } from 'react-redux';
 
 const Genre: React.FC<GenreProps> = (props) => {
   const { genre } = props;
+  const navigation = useRootNavigation();
   const source = useMangaSource();
   async function handleOnPress() {
     if (FilterValidator.isMangaHostWithFilters<WithGenresFilter>(source)) {
-      console.log('Redirect to genres screen');
+      navigation.navigate('GenericMangaList', { genre });
     }
   }
   return (
