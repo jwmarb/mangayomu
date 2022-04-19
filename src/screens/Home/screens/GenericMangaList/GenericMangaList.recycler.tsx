0586fee29f5dc1349def5667907ddb77c92be82d
EGGaming <dieptuber@gmail.com>
2022-04-19 08:33:15 -0700

    moved to file for organization

---
+++ src/screens/Home/screens/GenericMangaList/GenericMangaList.recycler.tsx
@@ -0,0 +1,42 @@
+import { RowRenderer } from '@utils/RecyclerListView.interfaces';
+import { LayoutProvider } from 'recyclerlistview';
+import { Dimensions } from 'react-native';
+import {
+  MangaItemContainerEven,
+  MangaItemContainerOdd,
+} from '@screens/Home/screens/GenericMangaList/GenericMangaList.base';
+import Manga from '@components/Manga';
+
+const { width } = Dimensions.get('window');
+enum Type {
+  ODD,
+  EVEN,
+}
+
+export const rowRenderer: RowRenderer = (type, data) => {
+  switch (type) {
+    default:
+      return null;
+    case Type.EVEN:
+      return (
+        <MangaItemContainerEven>
+          <Manga {...data} />
+        </MangaItemContainerEven>
+      );
+    case Type.ODD: {
+      return (
+        <MangaItemContainerOdd>
+          <Manga {...data} />
+        </MangaItemContainerOdd>
+      );
+    }
+  }
+};
+
+export const layoutProvider = new LayoutProvider(
+  (index) => (index % 2 === 1 ? Type.ODD : Type.EVEN),
+  (type, dim) => {
+    dim.width = width / 2 - 1;
+    dim.height = 265;
+  }
+);
--- src/screens/Home/screens/GenericMangaList/GenericMangaList.tsx
+++ src/screens/Home/screens/GenericMangaList/GenericMangaList.tsx
@@ -4,41 +4,12 @@ import { RootStackParamList } from '@navigators/Root/Root.interfaces';
 import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
 import { useCollapsibleHeader, UseCollapsibleOptions } from 'react-navigation-collapsible';
-import { DataProvider, LayoutProvider } from 'recyclerlistview';
-import { Dimensions } from 'react-native';
+import { DataProvider } from 'recyclerlistview';
 import pixelToNumber from '@utils/pixelToNumber';
 import { useTheme } from 'styled-components/native';
-import Manga from '@components/Manga';
-import {
-  MangaItemContainerEven,
-  MangaItemContainerOdd,
-} from '@screens/Home/screens/GenericMangaList/GenericMangaList.base';
-import { ApplyWindowCorrectionEventHandler, RowRenderer } from '@utils/RecyclerListView.interfaces';
-const { width, height } = Dimensions.get('window');
 
-enum Type {
-  ODD,
-  EVEN,
-}
-const rowRenderer: RowRenderer = (type, data) => {
-  switch (type) {
-    default:
-      return null;
-    case Type.EVEN:
-      return (
-        <MangaItemContainerEven>
-          <Manga {...data} />
-        </MangaItemContainerEven>
-      );
-    case Type.ODD: {
-      return (
-        <MangaItemContainerOdd>
-          <Manga {...data} />
-        </MangaItemContainerOdd>
-      );
-    }
-  }
-};
+import { ApplyWindowCorrectionEventHandler, RowRenderer } from '@utils/RecyclerListView.interfaces';
+import { layoutProvider, rowRenderer } from './GenericMangaList.recycler';
 
 const GenericMangaList: React.FC<StackScreenProps<RootStackParamList, 'GenericMangaList'>> = (props) => {
   const {
@@ -60,15 +31,6 @@ const GenericMangaList: React.FC<StackScreenProps<RootStackParamList, 'GenericMa
   const theme = useTheme();
 
   const dataProvider = React.useRef(new DataProvider((r1, r2) => r1 !== r2).cloneWithRows(mangas)).current;
-  const layoutProvider = React.useRef(
-    new LayoutProvider(
-      (index) => (index % 2 === 1 ? Type.ODD : Type.EVEN),
-      (type, dim) => {
-        dim.width = width / 2 - 1;
-        dim.height = 265;
-      }
-    )
-  ).current;
 
   const collapsible = useCollapsibleHeader(options);
 
