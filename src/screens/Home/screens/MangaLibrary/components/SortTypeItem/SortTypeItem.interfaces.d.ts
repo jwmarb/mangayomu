3cf907f7fbc516d644343a9f9097f3c27aaff376
EGGaming <dieptuber@gmail.com>
2022-04-22 12:44:01 -0700

    moved to components

--- src/screens/Home/screens/MangaLibrary/components/SortTypeItem/SortTypeItem.interfaces.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export interface SortTypeItemProps {
-  selected: boolean;
-  sortBy: string;
-  reverse: boolean;
-  setReverse: React.Dispatch<React.SetStateAction<boolean>>;
-  setSort: React.Dispatch<React.SetStateAction<string>>;
-}
