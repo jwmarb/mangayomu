bfca717154312970f8218946348c116206b65c4c
specify_ <dieptuber@gmail.com>
2022-04-22 22:40:54 -0700

    added to prevent boilerplate code

---
+++ src/hooks/useStatefulHeader.ts
@@ -0,0 +1,15 @@
+import { useNavigation } from '@react-navigation/native';
+import React, { DependencyList } from 'react';
+
+/**
+ * Convert the dummy header into a stateful header, where the state of the screen can be manipulated through the header
+ * @param customHeader The custom header component
+ * @param deps Dependencies that dictate when the header should rerender
+ */
+export default function useStatefulHeader(customHeader: JSX.Element, deps?: DependencyList) {
+  const navigation = useNavigation();
+
+  React.useEffect(() => {
+    navigation.setOptions({ header: () => customHeader });
+  }, deps);
+}
