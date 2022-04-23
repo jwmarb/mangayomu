326179515ae3269be7d7543589a4e58d6f39d85d
specify_ <dieptuber@gmail.com>
2022-04-23 16:08:28 -0700

    created browse screen

--- src/screens/Home/screens/Browse/Browse.tsx
+++ src/screens/Home/screens/Browse/Browse.tsx
@@ -1,11 +1,23 @@
+import { Screen } from '@components/core';
+import useSearchBar from '@hooks/useSearchBar';
 import useStatefulHeader from '@hooks/useStatefulHeader';
+import MangaHostSearch from '@screens/Home/screens/Browse/components/MangaHostSearch';
+import MangaHost from '@services/scraper/scraper.abstract';
 import React from 'react';
 
 const Browse: React.FC = (props) => {
   const {} = props;
+  const { query, header } = useSearchBar({ event: 'onSubmitEditing', title: 'Browse' });
+  const [mangaHosts, setMangaHosts] = React.useState<MangaHost[]>(Array.from(MangaHost.availableSources.values()));
+  useStatefulHeader(header);
 
-  useStatefulHeader(<></>);
-  return null;
+  return (
+    <Screen scrollable>
+      {mangaHosts.map((x) => (
+        <MangaHostSearch key={x.getLink()} host={x} query={query} />
+      ))}
+    </Screen>
+  );
 };
 
 export default Browse;
