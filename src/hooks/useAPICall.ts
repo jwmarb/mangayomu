9ec6220b1ddef18321faefaf6fb3d830219e1910
specify_ <dieptuber@gmail.com>
2022-04-23 16:04:29 -0700

    added dependencies to dictate when to fetch again

--- src/hooks/useAPICall.ts
+++ src/hooks/useAPICall.ts
@@ -1,12 +1,12 @@
 import CancelablePromise from '@utils/CancelablePromise';
-import React from 'react';
+import React, { DependencyList } from 'react';
 
 /**
  * A generated layout for calling APIs. No need to create your own useState variables since everything is done for you.
  * @param apiCall The API call is a function that returns a promise. This function is invoked upon the component mounting
  * @returns Returns the necessary variables needed for a component that uses an API call
  */
-export default function useAPICall<T>(apiCall: () => Promise<T>) {
+export default function useAPICall<T>(apiCall: () => Promise<T>, deps: DependencyList = []) {
   const cancelable = React.useRef(new CancelablePromise<T>(apiCall));
   const [items, setItems] = React.useState<T>();
   const [error, setError] = React.useState<string>('');
@@ -19,7 +19,7 @@ export default function useAPICall<T>(apiCall: () => Promise<T>) {
         cancelable.current.cancel();
       };
     } else mounted.current = true;
-  }, []);
+  }, deps);
   const refresh = React.useCallback(async () => {
     cancelable.current = new CancelablePromise(apiCall);
     setLoading(true);
@@ -31,7 +31,7 @@ export default function useAPICall<T>(apiCall: () => Promise<T>) {
     } finally {
       if (!cancelable.current.isCanceled()) setLoading(false);
     }
-  }, [setLoading, cancelable, setItems, setError]);
+  }, [setLoading, cancelable, setItems, setError, ...deps]);
 
   return {
     state: [items, setItems] as [T | undefined, React.Dispatch<React.SetStateAction<T>>],
