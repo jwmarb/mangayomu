9b6aba144965b055fc02275fc7382c5d86325dbb
specify_ <dieptuber@gmail.com>
2022-04-24 11:03:11 -0700

    refactored to include alternate titles in query

--- src/utils/MangaFilters/titleIncludes.ts
+++ src/utils/MangaFilters/titleIncludes.ts
@@ -1,4 +1,5 @@
 import { Manga } from '@services/scraper/scraper.interfaces';
+import MangaValidator from '@utils/MangaValidator';
 
 /**
  * Create a filter query predicate
@@ -6,5 +7,7 @@ import { Manga } from '@services/scraper/scraper.interfaces';
  * @returns Returns a filter predicate that determines whether query is in title of a manga
  */
 export default function titleIncludes(query: string): (x: Manga) => boolean {
-  return (x) => x.title.trim().toLowerCase().includes(query.trim().toLowerCase());
+  return (x) =>
+    x.title.trim().toLowerCase().includes(query.trim().toLowerCase()) ||
+    (MangaValidator.hasAltTitles(x) && x.altTitles.some((x) => x.toLowerCase().includes(query.trim().toLowerCase())));
 }
