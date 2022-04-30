0f790f4ebf6eb8f30660cba87d81a5a26219c17a
specify_ <dieptuber@gmail.com>
2022-04-30 15:44:58 -0700

    fixed text not wrapping

--- src/components/List/ListItem/ListItem.base.tsx
+++ src/components/List/ListItem/ListItem.base.tsx
@@ -1,5 +1,10 @@
+import { Dimensions } from 'react-native';
 import styled, { css } from 'styled-components/native';
 
 export const ListItemBase = styled.View`
   flex-grow: 1;
+`;
+
+export const ListItemSubtitleContainer = styled.View`
+  max-width: 90%;
 `;
--- src/components/List/ListItem/ListItem.tsx
+++ src/components/List/ListItem/ListItem.tsx
@@ -1,7 +1,7 @@
 import ButtonBase from '@components/Button/ButtonBase';
 import { Container } from '@components/Container';
 import Flex from '@components/Flex';
-import { ListItemBase } from '@components/List/ListItem/ListItem.base';
+import { ListItemBase, ListItemSubtitleContainer } from '@components/List/ListItem/ListItem.base';
 import { ListItemProps } from '@components/List/ListItem/ListItem.interfaces';
 import Spacer from '@components/Spacer';
 import { Typography } from '@components/Typography';
@@ -25,11 +25,11 @@ const ListItem: React.FC<ListItemProps> = (props) => {
               {title}
             </Typography>
             {subtitle && (
-              <>
-                <Typography variant='body2' color='textSecondary' numberOfLines={1} {...typographyProps}>
+              <ListItemSubtitleContainer>
+                <Typography variant='body2' color='textSecondary' {...typographyProps}>
                   {subtitle}
                 </Typography>
-              </>
+              </ListItemSubtitleContainer>
             )}
           </Flex>
         </Flex>
