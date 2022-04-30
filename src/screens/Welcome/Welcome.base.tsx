860ad09582edd7427994f6f298bc5ea02597b9cc
specify_ <dieptuber@gmail.com>
2022-04-29 21:37:11 -0700

    fixed ui issue

--- src/screens/Welcome/Welcome.base.tsx
+++ src/screens/Welcome/Welcome.base.tsx
@@ -4,5 +4,6 @@ import styled, { css } from 'styled-components/native';
 export const WelcomeBase = styled(Screen)`
   ${(props) => css`
     padding: ${props.theme.spacing(0, 3)};
+    height: 100%;
   `}
 `;
