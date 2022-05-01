3ab8f0c69ffbd149b834d77c761d8257e6e225e1
specify_ <dieptuber@gmail.com>
2022-05-01 16:20:35 -0700

    refactored to set user preferred size

--- src/components/Manga/Cover/Cover.tsx
+++ src/components/Manga/Cover/Cover.tsx
@@ -1,14 +1,34 @@
-import { MangaCoverBase, MangaCoverBaseContainer } from '@components/Manga/Cover/Cover.base';
-import { MangaCoverProps } from '@components/Manga/Cover/Cover.interfaces';
+import {
+  MangaCoverBase,
+  MangaCoverBaseContainer,
+  FixedMangaCoverBaseContainer,
+  FixedMangaCoverBase,
+} from '@components/Manga/Cover/Cover.base';
+import connector, { ProcessedMangaCoverProps } from '@components/Manga/Cover/Cover.redux';
+import { SPACE_MULTIPLIER } from '@theme/Spacing';
 import React from 'react';
+import { Dimensions, useWindowDimensions } from 'react-native';
+import { useTheme } from 'styled-components/native';
+const { width } = Dimensions.get('window');
 
-const Cover: React.FC<MangaCoverProps> = (props) => {
-  const { size = 'medium', uri } = props;
+const Cover: React.FC<ProcessedMangaCoverProps> = (props) => {
+  const { uri, cols, fixedSize } = props;
+  const theme = useTheme();
+  const imageWidth = theme.spacing(cols * 0.002 * width * 13);
+  const imageHeight = theme.spacing(cols * 0.002 * width * 20);
+
+  if (fixedSize)
+    return (
+      <FixedMangaCoverBaseContainer>
+        <FixedMangaCoverBase source={{ uri }} />
+      </FixedMangaCoverBaseContainer>
+    );
+
   return (
-    <MangaCoverBaseContainer size={size}>
-      <MangaCoverBase source={{ uri }} size={size} />
+    <MangaCoverBaseContainer imageWidth={imageWidth} imageHeight={imageHeight}>
+      <MangaCoverBase source={{ uri }} imageWidth={imageWidth} imageHeight={imageHeight} />
     </MangaCoverBaseContainer>
   );
 };
 
-export default Cover;
+export default connector(Cover);
