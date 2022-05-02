d05de177795d47f931dcbdde3dc65089df548f25
specify_ <dieptuber@gmail.com>
2022-05-02 00:19:44 -0700

    created to set custom layout

---
+++ src/screens/Settings/screens/MangasColumn/MangasColumn.base.tsx
@@ -0,0 +1,38 @@
+import Flex from '@components/Flex';
+import { calculateCoverWidth } from '@components/Manga/Cover/Cover.helpers';
+import { Dimensions } from 'react-native';
+import Animated from 'react-native-reanimated';
+import styled, { css } from 'styled-components/native';
+const { height } = Dimensions.get('window');
+
+export const MangasColumnPreviewContainer = styled.View`
+  ${(props) => css`
+    padding: ${props.theme.spacing(2, 3, 24, 3)};
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+  `}
+`;
+
+export const MangasColumnSettingsContainer = styled(Flex).attrs({ spacing: 2, direction: 'column' })`
+  ${(props) => css`
+    padding: ${props.theme.spacing(2, 3)};
+    background-color: ${props.theme.palette.background.paper.get()};
+    min-height: 100%;
+  `}
+`;
+
+export const MangaCoverPreview = styled(Animated.Image).attrs({ cache: 'force-cache' })`
+  ${(props) => css`
+    border-radius: ${props.theme.borderRadius}px;
+  `}
+`;
+
+export const MangaCoverPreviewContainer = styled(Animated.View)`
+  ${(props) => css`
+    display: flex;
+    flex-direction: column;
+    margin: ${props.theme.spacing(2)};
+  `}
+`;
--- src/screens/Settings/screens/MangasColumn/MangasColumn.tsx
+++ src/screens/Settings/screens/MangasColumn/MangasColumn.tsx
@@ -1,38 +1,112 @@
 import React from 'react';
-import { Slider, Screen, Spacer, Divider, Flex, Icon } from '@components/core';
+import { Slider, Screen, Spacer, Divider, Flex, Icon, Typography, Container, Header, Modal } from '@components/core';
 import Cover from '@components/Manga/Cover';
 import { useSelector } from 'react-redux';
 import { AppState, useAppDispatch } from '@redux/store';
 import { bindActionCreators } from 'redux';
-import { adjustColumns as _adjustColumns } from '@redux/reducers/settingsReducer/settingsReducer.actions';
-import { useWindowDimensions } from 'react-native';
+import { Animated, Dimensions, useWindowDimensions } from 'react-native';
+import {
+  MangaCoverPreview,
+  MangaCoverPreviewContainer,
+  MangasColumnPreviewContainer,
+  MangasColumnSettingsContainer,
+} from '@screens/Settings/screens/MangasColumn/MangasColumn.base';
+import { ScrollView } from 'react-native-gesture-handler';
+import { MangaBaseContainer } from '@components/Manga/Manga.base';
+import CoverSlider from '@screens/Settings/screens/MangasColumn/components/CoverSlider/CoverSlider';
+import { useCollapsibleHeader, UseCollapsibleOptions } from 'react-navigation-collapsible';
+import { useIsFocused } from '@react-navigation/native';
+import TitleSlider from '@screens/Settings/screens/MangasColumn/components/TitleSlider/TitleSlider';
+import { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
+import { calculateCoverHeight, calculateCoverWidth } from '@components/Manga/Cover/Cover.helpers';
+import { SPACE_MULTIPLIER } from '@theme/Spacing';
+import { adjustColumns as _adjustColumns } from '@redux/reducers/settingsReducer';
+const { height } = Dimensions.get('window');
+
+const sampleMangas = [
+  {
+    uri: 'https://cover.nep.li/cover/One-Piece.jpg',
+    title: 'One Piece',
+  },
+  {
+    uri: 'https://cover.nep.li/cover/Boruto.jpg',
+    title: 'Boruto - Naruto Next Generations',
+  },
+  {
+    uri: 'https://cover.nep.li/cover/Bleach.jpg',
+    title: 'Bleach',
+  },
+  {
+    uri: 'https://cover.nep.li/cover/Shingeki-No-Kyojin.jpg',
+    title: 'Shingeki no Kyojin',
+  },
+];
 
 const MangasColumn: React.FC = (props) => {
   const {} = props;
-  const { width } = useWindowDimensions();
-  const value = useSelector((state: AppState) => state.settings.mangaCover.perColumn);
+  const cols = useSelector((state: AppState) => state.settings.mangaCover.perColumn);
   const dispatch = useAppDispatch();
   const { adjustColumns } = bindActionCreators({ adjustColumns: _adjustColumns }, dispatch);
+  const colValue = useSharedValue(cols);
+  const width = useSharedValue(calculateCoverWidth(cols) * SPACE_MULTIPLIER);
+  const imageHeight = useSharedValue(calculateCoverHeight(cols) * SPACE_MULTIPLIER);
+  React.useEffect(() => {
+    return () => {
+      adjustColumns(colValue.value);
+    };
+  }, []);
+  const setWidth = React.useCallback((w: number) => {
+    colValue.value = w;
+    width.value = calculateCoverWidth(w) * SPACE_MULTIPLIER;
+    imageHeight.value = calculateCoverHeight(w) * SPACE_MULTIPLIER;
+  }, []);
+
+  const f = useIsFocused();
+  const options: UseCollapsibleOptions = {
+    navigationOptions: {
+      header: Header,
+    },
+    config: {
+      useNativeDriver: true,
+    },
+  };
+  const { containerPaddingTop, onScroll, scrollIndicatorInsetTop } = useCollapsibleHeader(options);
+  const style = useAnimatedStyle(() => ({
+    width: width.value,
+  }));
+  const imageStyle = useAnimatedStyle(() => ({
+    width: width.value,
+    height: imageHeight.value,
+  }));
 
   return (
-    <Screen scrollable>
-      <Flex container horizontalPadding={3} verticalPadding={0}>
-        <Cover uri='https://cover.nep.li/cover/One-Piece.jpg' />
-      </Flex>
-      <Spacer y={3} />
-      <Divider />
-      <Flex container horizontalPadding={3} direction='column' alignItems='center'>
-        <Slider
-          noFixedIncremental
-          range={[1, 3]}
-          width={width * 0.7}
-          value={value}
-          onChange={adjustColumns}
-          left={<Icon bundle='MaterialCommunityIcons' name='book-minus' size='small' />}
-          right={<Icon bundle='MaterialCommunityIcons' name='book-plus' />}
-        />
-      </Flex>
-    </Screen>
+    <>
+      <Animated.ScrollView
+        onScroll={onScroll}
+        contentContainerStyle={{ paddingTop: containerPaddingTop }}
+        scrollIndicatorInsets={{ top: scrollIndicatorInsetTop }}>
+        <MangasColumnPreviewContainer>
+          {sampleMangas.map((x, i) => (
+            <MangaCoverPreviewContainer style={style} key={i}>
+              <MangaCoverPreview source={{ uri: x.uri }} style={imageStyle} />
+              <Spacer y={1} />
+              <Typography numberOfLines={2}>{x.title}</Typography>
+            </MangaCoverPreviewContainer>
+          ))}
+        </MangasColumnPreviewContainer>
+      </Animated.ScrollView>
+      <Modal
+        visible={f}
+        onClose={() => {}}
+        minimumHeight={height * 0.85}
+        backdrop={false}
+        closeThreshold={height * 0.85}>
+        <MangasColumnSettingsContainer>
+          <CoverSlider value={cols} setValue={setWidth} />
+          <TitleSlider />
+        </MangasColumnSettingsContainer>
+      </Modal>
+    </>
   );
 };
 
