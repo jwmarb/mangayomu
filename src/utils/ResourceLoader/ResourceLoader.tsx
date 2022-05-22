18cb772a7c12f02d33e8a39b1c770e406ff1e3c0
specify_ <dieptuber@gmail.com>
2022-05-21 20:43:53 -0700

    initialization for downloadmanager

--- src/utils/ResourceLoader/ResourceLoader.tsx
+++ src/utils/ResourceLoader/ResourceLoader.tsx
@@ -3,24 +3,32 @@ import React from 'react';
 import { Nunito_400Regular, Nunito_300Light, Nunito_700Bold, Nunito_600SemiBold } from '@expo-google-fonts/nunito';
 import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
+import DownloadManager from '@utils/DownloadManager';
+
+async function asyncLoader() {
+  await Font.loadAsync({
+    Nunito: Nunito_400Regular,
+    'Nunito-light': Nunito_300Light,
+    'Nunito-heavy': Nunito_700Bold,
+    'Nunito-semi': Nunito_600SemiBold,
+  });
+}
 
 const ResourceLoader: React.FC<ResourceLoaderProps> = (props) => {
   const { onFinishedLoading: Component } = props;
   const [ready, setReady] = React.useState<boolean>(false);
-  async function asyncLoader() {
-    await Font.loadAsync({
-      Nunito: Nunito_400Regular,
-      'Nunito-light': Nunito_300Light,
-      'Nunito-heavy': Nunito_700Bold,
-      'Nunito-semi': Nunito_600SemiBold,
-    });
-  }
+
   React.useEffect(() => {
-    asyncLoader()
-      .then(() => {
+    (async () => {
+      try {
+        await asyncLoader();
+        await DownloadManager.initialize();
+      } catch (e) {
+        console.error(e);
+      } finally {
         setReady(true);
-      })
-      .catch(console.error);
+      }
+    })();
   }, []);
   return <>{!ready ? <AppLoading /> : Component}</>;
 };
