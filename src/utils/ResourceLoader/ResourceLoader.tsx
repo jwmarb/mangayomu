cbb94f003e64eecc8eb0d7200d42bfadb5748e11
specify_ <dieptuber@gmail.com>
2022-06-14 18:24:02 -0700

    replaced deprecated app-loading package

--- src/utils/ResourceLoader/ResourceLoader.tsx
+++ src/utils/ResourceLoader/ResourceLoader.tsx
@@ -1,11 +1,11 @@
 import { ResourceLoaderProps } from '@utils/ResourceLoader/ResourceLoader.interfaces';
 import React from 'react';
 import { Nunito_400Regular, Nunito_300Light, Nunito_700Bold, Nunito_600SemiBold } from '@expo-google-fonts/nunito';
-import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
 import StorageManager from '@utils/StorageManager';
 import DownloadManager from '@utils/DownloadManager';
-import { AppState } from 'react-native';
+import { AppState, View } from 'react-native';
 
 async function asyncLoader() {
   await Font.loadAsync({
@@ -23,6 +23,7 @@ const ResourceLoader: React.FC<ResourceLoaderProps> = (props) => {
   React.useEffect(() => {
     (async () => {
       try {
+        await SplashScreen.preventAutoHideAsync();
         await asyncLoader();
         await StorageManager.initialize();
         await DownloadManager.initialize();
@@ -44,7 +45,18 @@ const ResourceLoader: React.FC<ResourceLoaderProps> = (props) => {
       }
     })();
   }, []);
-  return <>{!ready ? <AppLoading /> : Component}</>;
+
+  const handleOnLayout = React.useCallback(async () => {
+    if (ready) await SplashScreen.hideAsync();
+  }, [ready]);
+
+  if (!ready) return null;
+
+  return (
+    <View style={{ flex: 1 }} onLayout={handleOnLayout}>
+      {Component}
+    </View>
+  );
 };
 
 export default ResourceLoader;
