6b56b3b7a80ac5bb5ed70bc5bbcf7a084aedf464
specify_ <dieptuber@gmail.com>
2022-07-12 21:18:05 -0700

    Fixed "Should have queue"

--- src/screens/GenericMangaList/GenericMangaList.tsx
+++ src/screens/GenericMangaList/GenericMangaList.tsx
@@ -22,44 +22,84 @@ import useMangaLayout from '@hooks/useMangaLayout';
 import { useSelector } from 'react-redux';
 import { AppState } from '@redux/store';
 
-const GenericMangaList: React.FC<StackScreenProps<RootStackParamList, 'GenericMangaList'>> = (props) => {
-  if ('mangas' in props.route.params && 'type' in props.route.params) {
-    const {
-      route: {
-        params: { mangas, type },
-      },
-    } = props;
+const InfiniteMangaList: React.FC<{ genre: string; source: string }> = (props) => {
+  const { genre, source } = props;
+  const mangaSource = useMangaSource(source) as MangaHostWithFilters<{ Genres: ExclusiveInclusiveFilter<string> }>;
+  const {
+    state: [mangas],
+    loading,
+    error,
+    refresh,
+  } = useAPICall(() => mangaSource.search('', { Genres: { include: [genre], exclude: [] } }));
+  const [dataProvider, setDataProvider] = React.useState<DataProvider>(new DataProvider((r1, r2) => r1 !== r2));
+  const { layoutProvider, rowRenderer } = useMangaLayout();
+  const [showFooter, setShowFooter] = React.useState<boolean>(true);
+  const [reachedEnd, setReachedEnd] = React.useState<boolean>(false);
+  const { ready, Fallback } = useLazyLoading();
+  const options: UseCollapsibleOptions = {
+    navigationOptions: {
+      header: Header,
+      headerTitle: genre,
+    },
+    config: {
+      useNativeDriver: true,
+    },
+  };
+  const collapsible = useCollapsibleHeader(options);
 
-    const options: UseCollapsibleOptions = {
-      navigationOptions: {
-        header: Header,
-        headerTitle: type,
-      },
-      config: {
-        useNativeDriver: true,
-      },
-    };
+  const applyWindowCorrection: ApplyWindowCorrectionEventHandler = React.useCallback(
+    (offsetX, offsetY, windowCorrection) => {
+      windowCorrection.windowShift = -collapsible.containerPaddingTop;
+    },
+    [collapsible.containerPaddingTop]
+  );
 
-    const { layoutProvider, rowRenderer } = useMangaLayout();
+  const handleOnEndReached = async () => {
+    if (!loading && !reachedEnd) {
+      setShowFooter(true);
+      try {
+        mangaSource.addPage();
+        const mangas = await mangaSource.search('', { Genres: { include: [genre], exclude: [] } });
+        if (
+          (dataProvider.getAllData() as Manga[])[dataProvider.getSize() - 1]?.title !== mangas[mangas.length - 1].title
+        )
+          setDataProvider((prev) => prev.cloneWithRows([...prev.getAllData(), ...mangas]));
+        else setReachedEnd(true);
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setShowFooter(false);
+      }
+    }
+  };
 
-    const theme = useTheme();
+  const handleOnItemLayout = () => {
+    setShowFooter(false);
+  };
 
-    const dataProvider = React.useRef(new DataProvider((r1, r2) => r1 !== r2).cloneWithRows(mangas)).current; // may be an anti-pattern, but this screen does not need to rerender since it is required to initialize this anyways
+  const theme = useTheme();
 
-    const collapsible = useCollapsibleHeader(options);
-
-    const applyWindowCorrection: ApplyWindowCorrectionEventHandler = React.useCallback(
-      (offsetX, offsetY, windowCorrection) => {
-        windowCorrection.windowShift = -collapsible.containerPaddingTop;
-      },
-      [collapsible.containerPaddingTop]
-    );
-
+  React.useEffect(() => {
+    if (!loading && mangas) setDataProvider((prev) => prev.cloneWithRows(mangas));
+  }, [mangas, loading]);
+  if (ready) {
+    if (loading || !mangas)
+      return (
+        <View
+          style={{
+            paddingTop: collapsible.containerPaddingTop + pixelToNumber(theme.spacing(3)),
+            paddingBottom: pixelToNumber(theme.spacing(3)),
+          }}>
+          {animate(<MangaItemsLoading />, withAnimatedMounting)}
+        </View>
+      );
     return (
       <RecyclerListViewScreen
         collapsible={collapsible}
         dataProvider={dataProvider}
+        onEndReached={handleOnEndReached}
         layoutProvider={layoutProvider}
+        onItemLayout={handleOnItemLayout}
         rowRenderer={rowRenderer as any}
         applyWindowCorrection={applyWindowCorrection}
         scrollViewProps={{
@@ -68,105 +108,66 @@ const GenericMangaList: React.FC<StackScreenProps<RootStackParamList, 'GenericMa
             paddingBottom: pixelToNumber(theme.spacing(3)),
           },
         }}
+        renderFooter={() => (showFooter ? <MangaItemsLoading /> : null)}
       />
     );
-  } else {
-    const {
-      route: {
-        params: { genre, source },
-      },
-    } = props;
-    const mangaSource = useMangaSource(source) as MangaHostWithFilters<{ Genres: ExclusiveInclusiveFilter<string> }>;
-    const {
-      state: [mangas],
-      loading,
-      error,
-      refresh,
-    } = useAPICall(() => mangaSource.search('', { Genres: { include: [genre], exclude: [] } }));
-    const [dataProvider, setDataProvider] = React.useState<DataProvider>(new DataProvider((r1, r2) => r1 !== r2));
-    const { layoutProvider, rowRenderer } = useMangaLayout();
-    const [showFooter, setShowFooter] = React.useState<boolean>(true);
-    const [reachedEnd, setReachedEnd] = React.useState<boolean>(false);
-    const { ready, Fallback } = useLazyLoading();
-    const options: UseCollapsibleOptions = {
-      navigationOptions: {
-        header: Header,
-        headerTitle: genre,
-      },
-      config: {
-        useNativeDriver: true,
-      },
-    };
-    const collapsible = useCollapsibleHeader(options);
-
-    const applyWindowCorrection: ApplyWindowCorrectionEventHandler = React.useCallback(
-      (offsetX, offsetY, windowCorrection) => {
-        windowCorrection.windowShift = -collapsible.containerPaddingTop;
-      },
-      [collapsible.containerPaddingTop]
-    );
-
-    const handleOnEndReached = async () => {
-      if (!loading && !reachedEnd) {
-        setShowFooter(true);
-        try {
-          mangaSource.addPage();
-          const mangas = await mangaSource.search('', { Genres: { include: [genre], exclude: [] } });
-          if (
-            (dataProvider.getAllData() as Manga[])[dataProvider.getSize() - 1]?.title !==
-            mangas[mangas.length - 1].title
-          )
-            setDataProvider((prev) => prev.cloneWithRows([...prev.getAllData(), ...mangas]));
-          else setReachedEnd(true);
-        } catch (e) {
-          console.error(e);
-        } finally {
-          setShowFooter(false);
-        }
-      }
-    };
-
-    const handleOnItemLayout = () => {
-      setShowFooter(false);
-    };
-
-    const theme = useTheme();
-
-    React.useEffect(() => {
-      if (!loading && mangas) setDataProvider((prev) => prev.cloneWithRows(mangas));
-    }, [mangas, loading]);
-    if (ready) {
-      if (loading || !mangas)
-        return (
-          <View
-            style={{
-              paddingTop: collapsible.containerPaddingTop + pixelToNumber(theme.spacing(3)),
-              paddingBottom: pixelToNumber(theme.spacing(3)),
-            }}>
-            {animate(<MangaItemsLoading />, withAnimatedMounting)}
-          </View>
-        );
-      return (
-        <RecyclerListViewScreen
-          collapsible={collapsible}
-          dataProvider={dataProvider}
-          onEndReached={handleOnEndReached}
-          layoutProvider={layoutProvider}
-          onItemLayout={handleOnItemLayout}
-          rowRenderer={rowRenderer as any}
-          applyWindowCorrection={applyWindowCorrection}
-          scrollViewProps={{
-            contentContainerStyle: {
-              paddingTop: collapsible.containerPaddingTop + pixelToNumber(theme.spacing(3)),
-              paddingBottom: pixelToNumber(theme.spacing(3)),
-            },
-          }}
-          renderFooter={() => (showFooter ? <MangaItemsLoading /> : null)}
-        />
-      );
-    }
-    return Fallback;
   }
+  return Fallback;
+};
+
+const GenericMangaListWithMangas: React.FC<{
+  mangas: Manga[];
+  type: string;
+}> = (props) => {
+  const { mangas, type } = props;
+
+  const options: UseCollapsibleOptions = {
+    navigationOptions: {
+      header: Header,
+      headerTitle: type,
+    },
+    config: {
+      useNativeDriver: true,
+    },
+  };
+
+  const { layoutProvider, rowRenderer } = useMangaLayout();
+
+  const theme = useTheme();
+
+  const dataProvider = React.useRef(new DataProvider((r1, r2) => r1 !== r2).cloneWithRows(mangas)).current; // may be an anti-pattern, but this screen does not need to rerender since it is required to initialize this anyways
+
+  const collapsible = useCollapsibleHeader(options);
+
+  const applyWindowCorrection: ApplyWindowCorrectionEventHandler = React.useCallback(
+    (offsetX, offsetY, windowCorrection) => {
+      windowCorrection.windowShift = -collapsible.containerPaddingTop;
+    },
+    [collapsible.containerPaddingTop]
+  );
+
+  return (
+    <RecyclerListViewScreen
+      collapsible={collapsible}
+      dataProvider={dataProvider}
+      layoutProvider={layoutProvider}
+      rowRenderer={rowRenderer as any}
+      applyWindowCorrection={applyWindowCorrection}
+      scrollViewProps={{
+        contentContainerStyle: {
+          paddingTop: collapsible.containerPaddingTop + pixelToNumber(theme.spacing(3)),
+          paddingBottom: pixelToNumber(theme.spacing(3)),
+        },
+      }}
+    />
+  );
+};
+
+const GenericMangaList: React.FC<StackScreenProps<RootStackParamList, 'GenericMangaList'>> = (props) => {
+  if ('mangas' in props.route.params && 'type' in props.route.params)
+    return <GenericMangaListWithMangas {...props.route.params} />;
+
+  return <InfiniteMangaList {...props.route.params} />;
 };
 
 export default GenericMangaList;
