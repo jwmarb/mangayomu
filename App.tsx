d21163cfd1d79878e57497722ede96d5e9df9aac
specify_ <dieptuber@gmail.com>
2022-07-13 23:27:02 -0700

    removed unused imports

--- App.tsx
+++ App.tsx
@@ -7,16 +7,11 @@ import { ThemeProvider } from 'styled-components/native';
 import theme from '@theme/core';
 import ResourceLoader from '@utils/ResourceLoader';
 import 'react-native-gesture-handler';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
 import { PersistGate } from 'redux-persist/integration/react';
-import { HoldMenuProvider } from 'react-native-hold-menu';
-import { RootSiblingParent } from 'react-native-root-siblings';
 import { PortalProvider } from '@gorhom/portal';
 import { LogBox } from 'react-native';
 import '@services/MangaPark_v3';
 import '@services/MangaSee';
-import ExpoStorage from '@utils/ExpoStorage';
 
 LogBox.ignoreLogs([
   `ViewPropTypes will be removed from React Native. Migrate to ViewPropTypes exported from 'deprecated-react-native-prop-types'.`,
