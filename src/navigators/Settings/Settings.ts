ce3bec04daaf11e98fc7794596257980ff1fe4cf
specify_ <dieptuber@gmail.com>
2022-07-13 19:48:58 -0700

    created hook to easily access navigation

--- src/navigators/Settings/Settings.ts
+++ src/navigators/Settings/Settings.ts
@@ -1,6 +1,8 @@
 import { SettingsStackParamList } from '@navigators/Settings/Settings.interfaces';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const SettingsStack = createStackNavigator<SettingsStackParamList>();
+export const useSettingsNavigation = () => useNavigation<NavigationProp<SettingsStackParamList>>();
 
 export default SettingsStack;
