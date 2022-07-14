e135a7202bcad51d2e24bffb50ac052deed28b95
specify_ <dieptuber@gmail.com>
2022-07-14 02:24:15 -0700

    fixed layout

--- src/screens/Settings/screens/Main/Main.tsx
+++ src/screens/Settings/screens/Main/Main.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { List, Icon, ListItem, Typography, Divider, ListSection } from '@components/core';
+import { List, Icon, ListItem, Typography, Divider, ListSection, Screen } from '@components/core';
 import { StackScreenProps } from '@react-navigation/stack';
 import { SettingsStackParamList } from '@navigators/Settings/Settings.interfaces';
 import Reader from '@screens/Settings/screens/Reader';
 import Appearance from '@screens/Settings/screens/Appearance';
+import { ScrollView } from 'react-native-gesture-handler';
+import axios from 'axios';
 
 const Main: React.FC<StackScreenProps<SettingsStackParamList, 'Main'>> = (props) => {
   const { navigation } = props;
@@ -13,19 +15,21 @@ const Main: React.FC<StackScreenProps<SettingsStackParamList, 'Main'>> = (props)
     };
   }
   return (
-    <List>
-      <ListItem
-        adornment={<Icon bundle='MaterialCommunityIcons' name='cog-outline' color='primary' />}
-        title='General'
-      />
-      <Divider />
-      <Appearance />
-      <Divider />
-      <Reader />
-      <Divider />
-      <ListItem adornment={<Icon bundle='Feather' name='compass' color='primary' />} title='Browse' />
-      <ListItem adornment={<Icon bundle='Feather' name='code' color='primary' />} title='Advanced' />
-    </List>
+    <ScrollView>
+      <List>
+        <ListItem
+          adornment={<Icon bundle='MaterialCommunityIcons' name='cog-outline' color='primary' />}
+          title='General'
+        />
+        <Divider />
+        <Appearance />
+        <Divider />
+        <Reader />
+        <Divider />
+        <ListItem adornment={<Icon bundle='Feather' name='compass' color='primary' />} title='Browse' />
+        <ListItem adornment={<Icon bundle='Feather' name='code' color='primary' />} title='Advanced' />
+      </List>
+    </ScrollView>
   );
 };
 
