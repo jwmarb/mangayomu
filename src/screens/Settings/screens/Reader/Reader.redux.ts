f6758fd199f3b2f05dbc21f51d0daf057e271eb9
specify_ <dieptuber@gmail.com>
2022-07-13 19:48:40 -0700

    todo reader settings

---
+++ src/screens/Settings/screens/Reader/Reader.redux.ts
@@ -0,0 +1,17 @@
+import { AppState } from '@redux/store';
+import React from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import { changeReaderBackground, changeReaderDirection } from '@redux/reducers/settingsReducer';
+import { StackScreenProps } from '@react-navigation/stack';
+import { SettingsStackParamList } from '@navigators/Settings/Settings.interfaces';
+
+const mapStateToProps = (state: AppState, props: React.PropsWithChildren<{}>) => ({
+  ...props,
+  settings: state.settings.reader,
+});
+
+const connector = connect(mapStateToProps, { changeReaderBackground, changeReaderDirection });
+
+export type ConnectedReaderSettingsScreenProps = ConnectedProps<typeof connector>;
+
+export default connector;
--- src/screens/Settings/screens/Reader/Reader.tsx
+++ src/screens/Settings/screens/Reader/Reader.tsx
@@ -1,8 +1,51 @@
+import { Button, Icon, List, ListItem, ListSection } from '@components/core';
+import { ReaderBackgroundColor, ReaderDirection } from '@redux/reducers/settingsReducer/settingsReducer';
+import connector, { ConnectedReaderSettingsScreenProps } from '@screens/Settings/screens/Reader/Reader.redux';
 import React from 'react';
+import { Switch } from 'react-native-gesture-handler';
+import { HoldItem } from 'react-native-hold-menu';
+import { MenuItemProps } from 'react-native-hold-menu/lib/typescript/components/menu/types';
 
-const Reader: React.FC = (props) => {
-  const {} = props;
-  return null;
+const Reader: React.FC<ConnectedReaderSettingsScreenProps> = (props) => {
+  const { settings, changeReaderBackground, changeReaderDirection } = props;
+  const bgColorOptions: MenuItemProps[] = React.useMemo(
+    () =>
+      Object.values(ReaderBackgroundColor).map(
+        (x): MenuItemProps => ({
+          text: x,
+          onPress: () => {
+            changeReaderBackground(x);
+          },
+        })
+      ),
+    []
+  );
+  const readerDirectionOptions: MenuItemProps[] = React.useMemo(
+    () =>
+      Object.values(ReaderDirection).map(
+        (x): MenuItemProps => ({
+          text: x,
+          onPress: () => {
+            changeReaderDirection(x);
+          },
+        })
+      ),
+    []
+  );
+  return (
+    <>
+      <ListSection title='Reader' />
+      <ListItem title='Background color' subtitle={settings.backgroundColor} holdItem={bgColorOptions} />
+      <ListItem
+        holdItem={readerDirectionOptions}
+        title='Reading direction'
+        subtitle={settings.preferredReadingDirection}
+      />
+      <ListItem title='Keep screen awake' />
+      <ListItem title='Show page number' />
+      <ListItem title='Skip chapters marked read' />
+    </>
+  );
 };
 
-export default Reader;
+export default connector(React.memo(Reader));
