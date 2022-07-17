75f7633cbd2b2ceb9f5d77b3921c5c42a6285e2a
specify_ <dieptuber@gmail.com>
2022-07-16 19:02:02 -0700

    fixed broken type def

--- src/theme/Typography/typography.interfaces.d.ts
+++ src/theme/Typography/typography.interfaces.d.ts
@@ -1,6 +1,6 @@
 import { typographyTheme, typographyVariants } from '@theme/Typography/typography';
 import React from 'react';
 
-export type TypographyVariants = keyof typeof typographyVariants;
+export type TypographyVariants = keyof ReturnType<typeof typographyVariants>;
 
 export type TypographyTheme = typeof typographyTheme;
