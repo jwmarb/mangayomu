c0c7dd52c2fddffb149a1c1a9e4fe7fd532210cc
specify_ <dieptuber@gmail.com>
2022-08-02 10:13:00 -0700

    fix undefined value

--- src/screens/Reader/components/Overlay/components/OverlayFooter/components/Selector.helpers.ts
+++ src/screens/Reader/components/Overlay/components/OverlayFooter/components/Selector.helpers.ts
@@ -7,7 +7,9 @@ import { AppState } from '@redux/store';
 import { Manga } from '@services/scraper/scraper.interfaces';
 
 export function getOrUseGlobalSetting<T extends keyof ReaderSettingProfile>(state: AppState, mangaKey: string, key: T) {
-  return state.readerSetting[mangaKey][key] != null && state.readerSetting[mangaKey][key] !== OverloadedSetting.AUTO
+  return mangaKey in state.readerSetting &&
+    state.readerSetting[mangaKey][key] != null &&
+    state.readerSetting[mangaKey][key] !== OverloadedSetting.AUTO
     ? (state.readerSetting[mangaKey][key] as GlobalReadingSettingProfile[T])
     : state.settings.reader._global[key];
 }
