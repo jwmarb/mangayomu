65e9b597df8526e6aa959c9b769e1eaf007d5533
specify_ <dieptuber@gmail.com>
2022-09-07 17:44:45 -0700

    fix duplicate keys

--- src/screens/Home/screens/Explore/components/LatestManga/LatestManga.flatlist.tsx
+++ src/screens/Home/screens/Explore/components/LatestManga/LatestManga.flatlist.tsx
@@ -2,7 +2,7 @@ import { MangaComponent } from '@components/core';
 import { Manga } from '@services/scraper/scraper.interfaces';
 import { ListRenderItem } from 'react-native';
 
-export const keyExtractor = (item: Manga) => item.title;
+export const keyExtractor = (item: Manga, index: number) => item.title + index;
 export const renderItem: ListRenderItem<Manga> = ({ item }) => {
   return <MangaComponent {...item} />;
 };
