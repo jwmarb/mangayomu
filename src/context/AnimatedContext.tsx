b1b9d86f15713a5a090e16a70d6d440d57dd9322
specify_ <dieptuber@gmail.com>
2022-09-11 17:02:45 -0700

    fix children missing in type

--- src/context/AnimatedContext.tsx
+++ src/context/AnimatedContext.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export const AnimatedContext = React.createContext<Record<string, unknown>>(null as any);
 
-export const AnimatedProvider: React.FC<{ style: Record<string, unknown> }> = ({ children, style }) => {
+export const AnimatedProvider: React.FC<React.PropsWithChildren<{ style: Record<string, unknown> }>> = ({
+  children,
+  style,
+}) => {
   return <AnimatedContext.Provider value={style}>{children}</AnimatedContext.Provider>;
 };
