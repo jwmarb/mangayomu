d8221353c8a0e16f840d572da8198651da65c1f4
Joseph Marbella <dieptuber@gmail.com>
2022-10-18 11:16:12 -0700

    fix width not changing

--- src/screens/MangaViewer/components/ChapterHeader/ChapterHeader.base.tsx
+++ src/screens/MangaViewer/components/ChapterHeader/ChapterHeader.base.tsx
@@ -1,8 +1,7 @@
 import { Dimensions } from 'react-native';
 import Animated from 'react-native-reanimated';
 import styled, { css } from 'styled-components/native';
-const { width } = Dimensions.get('window');
-const halfWidth = width / 2;
+
 export const ChapterHeaderContainer = styled.View`
   ${(props) => css`
     padding: ${props.theme.spacing(1, 3)};
@@ -15,25 +14,32 @@ export const ChapterHeaderContainer = styled.View`
 `;
 
 export const ChapterLoadingIndicator = styled(Animated.View)`
-  ${(props) => css`
-    background-color: ${props.theme.palette.primary.main.get()};
-    height: 2px;
-    width: ${halfWidth}px;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-  `}
+  ${(props) => {
+    const { width } = Dimensions.get('window');
+    return css`
+      background-color: ${props.theme.palette.primary.main.get()};
+      height: 2px;
+      width: ${width / 2}px;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+    `;
+  }}
 `;
 
 export const ChapterLoadingIndicatorBackground = styled(Animated.View)`
-  ${(props) => css`
-    background-color: ${props.theme.palette.primary[props.theme.palette.mode ?? 'light'].get()};
-    height: 2px;
-    width: ${width}px;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-  `}
+  ${(props) => {
+    const { width } = Dimensions.get('window');
+
+    return css`
+      background-color: ${props.theme.palette.primary[props.theme.palette.mode ?? 'light'].get()};
+      height: 2px;
+      width: ${width}px;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+    `;
+  }}
 `;
