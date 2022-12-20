79dd39d53a022721e51990020f655bd300c4ce21
Joseph Marbella <dieptuber@gmail.com>
2022-12-20 07:48:23 -0700

    fix undefined `readingDirection`

--- src/screens/Reader/components/TransitioningPage/TransitioningPage.redux.ts
+++ src/screens/Reader/components/TransitioningPage/TransitioningPage.redux.ts
@@ -3,12 +3,13 @@ import { TransitioningPageProps } from '@screens/Reader/components/Transitioning
 import React from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import { setReaderLoading, toggleOverlay } from '@redux/reducers/readerReducer';
+import { getOrUseGlobalSetting } from '@screens/Reader/components/Overlay/components/OverlayFooter/components/Selector.helpers';
 
 const mapStateToProps = (state: AppState, props: TransitioningPageProps) => ({
   nextChapter: state.mangas[props.manga.link].chapters[props.next],
   currentChapterInView: state.reader.chapterInView,
   previousChapter: state.mangas[props.manga.link].chapters[props.previous],
-  readingDirection: state.settings.reader.preferredReadingDirection,
+  readingDirection: getOrUseGlobalSetting(state, props.manga.link, 'readingDirection'),
   loading: state.reader.extendedState[props.extendedStateKey].loading,
   hasAlreadyFetched: state.reader.extendedState[props.extendedStateKey].hasAlreadyFetched,
   extendedStateKey: props.extendedStateKey,
