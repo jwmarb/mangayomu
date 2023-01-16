6f134c6a69949a0493ee3ae7c9abc71c76e0971e
specify_ <dieptuber@gmail.com>
2023-01-16 00:18:55 -0700

    test theme

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -28,15 +28,48 @@ import { NavigationContainer } from '@react-navigation/native';
 import { RootStack } from '@navigators/Root';
 import Home from '@screens/Home';
 import MangaView from '@screens/MangaView';
+import { createTheme } from '@mangayomu/theme';
 
 function App(): JSX.Element {
+  const mode = useColorScheme();
+  const theme = createTheme(({ color, colorConstant }) => ({
+    mode,
+    palette: {
+      primary: {
+        light: colorConstant('#69c0ff'),
+        main: colorConstant('#1890ff'),
+        dark: colorConstant('#0050b3'),
+      },
+      secondary: {
+        light: colorConstant('#ffa39e'),
+        main: colorConstant('#ff7875'),
+        dark: colorConstant('#ff4d4f'),
+      },
+      text: {
+        primary: color('rgba(255, 255, 255, 1)', 'rgba(0, 0, 0, 0.87)'),
+        secondary: color('rgba(255, 255, 255, 0.7)', 'rgba(0, 0, 0, 0.6)'),
+        disabled: color('rgba(255, 255, 255, 0.5)', 'rgba(0, 0, 0, 0.38)'),
+        hint: color('rgba(255, 255, 255, 0.5)', 'rgba(0, 0, 0, 0.38)'),
+      },
+      background: {
+        default: color('#141414', '#fafafa'),
+        paper: color('#262626', '#ffffff'),
+      },
+    },
+    style: {
+      borderRadius: 24,
+    },
+  }));
   return (
-    <NavigationContainer>
-      <RootStack.Navigator>
-        <RootStack.Screen name="Home" component={Home} />
-        <RootStack.Screen name="MangaView" component={MangaView} />
-      </RootStack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar translucent backgroundColor="transparent" />
+      <NavigationContainer>
+        <RootStack.Navigator>
+          <RootStack.Screen name="Home" component={Home} />
+          <RootStack.Screen name="MangaView" component={MangaView} />
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
