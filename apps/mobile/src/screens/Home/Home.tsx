12a5bfc8bd6178041377a79d987836e1ba3aa2a8
specify_ <dieptuber@gmail.com>
2023-01-16 00:18:35 -0700

    hello world

--- apps/mobile/src/screens/Home/Home.tsx
+++ apps/mobile/src/screens/Home/Home.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 const Home: React.FC = () => {
   return (
     <View>
-      <Text>Hello from Home</Text>
+      <Text>Hello World!</Text>
     </View>
   );
 };
