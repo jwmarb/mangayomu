201c5e9691cc4af861817243761874341d749eb8
specify_ <dieptuber@gmail.com>
2023-01-15 23:17:11 -0700

    update lint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 /* eslint-env node */
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
-    quotes: ["warn", "double"],
-    semi: ["warn", "always"],
-    "arrow-parens": ["warn", "always"],
-    "array-element-newline": ["warn", "consistent"],
+    quotes: ['warn', 'single'],
+    semi: ['warn', 'always'],
+    'arrow-parens': ['warn', 'always'],
+    'array-element-newline': ['warn', 'consistent'],
   },
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  ignorePatterns: ["apps/expo/*", "dist/"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['apps/expo/*', 'dist/'],
   root: true,
 };
