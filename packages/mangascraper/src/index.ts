d252cef36010af2b57a006216cb25e3fb87e5d6a
specify_ <dieptuber@gmail.com>
2023-01-16 16:04:10 -0700

    move to `src`

---
+++ packages/mangascraper/src/index.ts
@@ -0,0 +1,4 @@
+export { default as MangaHost } from './scraper/scraper.abstract';
+export { default as validator } from './validator';
+export { default as MangaSee } from './MangaSee';
+export { default as MangaPark_v3 } from './MangaPark_v3';
