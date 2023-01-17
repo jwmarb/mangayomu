b91cd6f98cf649b78e701420c7e865a6df3aab58
Joseph Marbella <dieptuber@gmail.com>
2023-01-17 14:45:05 -0700

    add styling to `Text`

--- apps/mobile/src/components/Text/Text.interfaces.d.ts
+++ apps/mobile/src/components/Text/Text.interfaces.d.ts
@@ -1,9 +1,13 @@
 import { Colors } from '@mangayomu/theme';
 import { TextVariants } from '@theme/theme';
 import React from 'react';
-import { TextProps as NativeTextProps } from 'react-native';
+import { TextProps as NativeTextProps, TextStyle } from 'react-native';
 
 export interface TextProps extends React.PropsWithChildren, NativeTextProps {
   variant?: TextVariants;
   color?: Colors;
+  align?: TextStyle['textAlign'];
+  bold?: boolean;
+  italic?: boolean;
+  underline?: boolean;
 }
--- apps/mobile/src/components/Text/Text.tsx
+++ apps/mobile/src/components/Text/Text.tsx
@@ -4,10 +4,22 @@ import { TextProps } from './Text.interfaces';
 
 const Text = styled.Text<TextProps>`
   ${(props) => {
-    const { theme, variant = 'body', color = 'textPrimary' } = props;
+    const {
+      theme,
+      variant = 'body',
+      color = 'textPrimary',
+      bold = false,
+      italic = false,
+      underline = false,
+      align = 'auto',
+    } = props;
     return css`
       ${theme.typography[variant]}
       color: ${theme.helpers.getColor(color)};
+      text-align: ${align};
+      ${bold && 'font-weight: bold'};
+      ${italic && 'font-style: italic'};
+      ${underline && 'text-decoration-line: underline'};
     `;
   }}
 `;
