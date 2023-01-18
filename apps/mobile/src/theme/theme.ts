48619892d539b71a12b6cd9cf21fedbbc6adce47
specify_ <dieptuber@gmail.com>
2023-01-17 23:38:09 -0700

    add theme for storybook

--- apps/mobile/src/theme/theme.ts
+++ apps/mobile/src/theme/theme.ts
@@ -1,29 +1,31 @@
-import { DefaultTheme } from '@mangayomu/theme';
+import { createTheme, DefaultTheme } from '@mangayomu/theme';
 import '@emotion/react';
 import { css } from '@emotion/native';
 import { RFValue } from 'react-native-responsive-fontsize';
+import { Theme } from '@emotion/react';
+import { helpers } from '@theme/helpers';
 
 export type TextVariants = keyof typeof typography;
 
 export const typography = {
   header: css`
     font-family: Roboto;
-    font-size: ${RFValue(20)}px;
+    font-size: 20px;
     letter-spacing: -0.2px;
   `,
   body: css`
     font-family: Roboto;
-    font-size: ${RFValue(13)}px;
+    font-size: 13px;
     letter-spacing: -0.44px;
   `,
   button: css`
     font-family: Roboto;
-    font-size: ${RFValue(12)}px;
+    font-size: 12px;
     letter-spacing: -0.44px;
   `,
   'bottom-tab': css`
     font-family: Roboto;
-    font-size: ${RFValue(10)}px;
+    font-size: 10px;
     letter-spacing: -0.56px;
   `,
 } as const;
@@ -33,3 +35,42 @@ declare module '@emotion/react' {
     typography: typeof typography;
   }
 }
+
+export const __storybook_theme__ = createTheme<Theme>(
+  ({ color, colorConstant }) => ({
+    mode: 'dark',
+    palette: {
+      primary: {
+        light: colorConstant('#69c0ff'),
+        main: colorConstant('#1890ff'),
+        dark: colorConstant('#0050b3'),
+      },
+      secondary: {
+        light: colorConstant('#ffa39e'),
+        main: colorConstant('#ff7875'),
+        dark: colorConstant('#ff4d4f'),
+      },
+      text: {
+        primary: color('rgba(255, 255, 255, 1)', 'rgba(0, 0, 0, 0.87)'),
+        secondary: color('rgba(255, 255, 255, 0.7)', 'rgba(0, 0, 0, 0.6)'),
+        disabled: color('rgba(255, 255, 255, 0.5)', 'rgba(0, 0, 0, 0.38)'),
+        hint: color('rgba(255, 255, 255, 0.5)', 'rgba(0, 0, 0, 0.38)'),
+      },
+      background: {
+        default: color('#141414', '#fafafa'),
+        paper: color('#262626', '#ffffff'),
+      },
+    },
+    style: {
+      borderRadius: RFValue(24),
+      spacing: {
+        s: 2,
+        m: 6,
+        l: 10,
+        xl: 16,
+      },
+    },
+    typography,
+    helpers,
+  }),
+);
