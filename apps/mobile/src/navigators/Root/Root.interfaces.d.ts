3a8c14a05d4d9d6dbcaeb0d3055b149611865038
specify_ <dieptuber@gmail.com>
2023-01-19 22:17:47 -0700

    add `Welcome` screen

--- apps/mobile/src/navigators/Root/Root.interfaces.d.ts
+++ apps/mobile/src/navigators/Root/Root.interfaces.d.ts
@@ -3,6 +3,7 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 export type RootStackParamList = {
   Home: undefined;
   MangaView: undefined;
+  Welcome: undefined;
 };
 
 export type RootStackProps<T extends keyof RootStackParamList> =
---
+++ apps/mobile/src/screens/Welcome/Welcome.tsx
@@ -0,0 +1,21 @@
+import Box from '@components/Box';
+import Text from '@components/Text';
+import React from 'react';
+import { ScrollView } from 'react-native';
+
+const Welcome: React.FC = () => {
+  return (
+    <ScrollView
+      horizontal
+      showsVerticalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false}
+      pagingEnabled
+    >
+      <Box>
+        <Text>Hello World</Text>
+      </Box>
+    </ScrollView>
+  );
+};
+
+export default Welcome;
