017fe6211d8596f5f6f6faf92a2a2524f90f3e35
specify_ <dieptuber@gmail.com>
2023-01-19 21:52:13 -0700

    finish up redux

--- apps/mobile/src/redux/main.ts
+++ apps/mobile/src/redux/main.ts
@@ -1,5 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { __initialReducer__ } from '@redux/slices/__initial__';
 
-export default configureStore({
-  reducer: {},
+const store = configureStore({
+  reducer: {
+    __initial__: __initialReducer__,
+  },
 });
+
+export type AppState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
