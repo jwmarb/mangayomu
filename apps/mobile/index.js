1e8d42b9c8b03b59a8354e0d9c46ad74c843f8da
specify_ <dieptuber@gmail.com>
2023-01-19 22:29:23 -0700

    fix metro not working

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -6,9 +6,9 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-let RegisteredApp = App;
+// let RegisteredApp = App;
 // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
 // RegisteredApp = __DEV__ ? require('./storybook').default : App;
 // ^ comment this to go back to main app environment, and don't forget to go to metro.config.js
 
-AppRegistry.registerComponent(appName, () => RegisteredApp);
+AppRegistry.registerComponent(appName, () => App);
