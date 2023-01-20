f90dae578d2b80efa9a06cdb9215e2fda51efdf2
Joseph Marbella <dieptuber@gmail.com>
2023-01-20 11:56:45 -0700

    fix interpolating functions

--- apps/mobile/src/components/Box/Box.helpers.ts
+++ apps/mobile/src/components/Box/Box.helpers.ts
@@ -1,36 +1,39 @@
 import { css, ReactNativeStyle } from '@emotion/native';
+import { Theme } from '@emotion/react';
+import { Spacing } from '@mangayomu/theme';
 
 export function set<T>(
   propertyName: string,
   input?: T,
-): (() => ReactNativeStyle) | undefined {
+): ReactNativeStyle | undefined {
   if (input != null)
-    return () => css`
+    return css`
       ${propertyName}: ${input};
     `;
 }
 
-export function setu<T>(
+export function setu(
+  theme: Theme,
   propertyName: string,
-  input?: T,
-): (() => ReactNativeStyle) | undefined {
+  input?: Spacing,
+): ReactNativeStyle | undefined {
   if (input != null)
-    return () => css`
-      ${propertyName}: ${input}px;
+    return css`
+      ${propertyName}: ${theme.helpers.spacing(input)}px;
     `;
 }
 
 export function setwandh(
   propertyName: 'width' | 'height',
   widthOrHeightValue: string | number,
-): (() => ReactNativeStyle) | undefined {
+): ReactNativeStyle | undefined {
   switch (typeof widthOrHeightValue) {
     case 'number':
-      return () => css`
+      return css`
         ${propertyName}: ${widthOrHeightValue}px;
       `;
     case 'string':
-      return () => css`
+      return css`
         ${propertyName}: ${widthOrHeightValue};
       `;
     default:
--- apps/mobile/src/components/Box/Box.tsx
+++ apps/mobile/src/components/Box/Box.tsx
@@ -3,82 +3,81 @@ import { BackgroundColor, ButtonColors } from '@mangayomu/theme';
 import { BoxProps } from './Box.interfaces';
 import { set, setu, setwandh } from './Box.helpers';
 
-const style = (props) => {
-  const {
-    theme,
-    'align-items': alignItems = 'stretch',
-    'align-self': alignSelf = 'stretch',
-    'background-color': bgColor,
-    'box-shadow': boxShadow,
-    'justify-content': justifyContent = 'flex-start',
-    flex,
-    'flex-grow': flexGrow,
-    'flex-shrink': flexShrink,
-    'flex-wrap': flexWrap,
-    p,
-    m,
-    px,
-    pv,
-    mx,
-    mv,
-    ml,
-    mr,
-    mb,
-    mt,
-    pl,
-    pr,
-    pt,
-    pb,
-    width = 'auto',
-    height = 'auto',
-  } = props;
-
-  return css`
-    /* align-items: ${alignItems};
-    align-self: ${alignSelf};
-    ${() => {
-      if (bgColor == null) return;
-      if (bgColor in theme.palette.background)
-        return css`
-          background-color: ${theme.palette.background[
-            bgColor as keyof BackgroundColor
-          ]};
-        `;
-      return css`
-        background-color: ${theme.palette[bgColor as ButtonColors].main};
-      `;
-    }};
-    flex: ${flex ? 1 : 0};
-    flex-grow: ${flexGrow ? 1 : 0};
-    flex-shrink: ${flexShrink ? 1 : 0};
-    justify-content: ${justifyContent};
-    ${setwandh('height', height)};
-    ${setwandh('width', width)};
-    ${() => boxShadow && theme.style.shadow};
-    ${set('flex-wrap', flexWrap)};
-    ${setu('padding', theme.helpers.spacing(p))};
-    ${setu('margin', theme.helpers.spacing(m))};
-    ${setu('padding-horizontal', theme.helpers.spacing(px))};
-    ${setu('padding-vertical', theme.helpers.spacing(pv))};
-    ${setu('margin-horizontal', theme.helpers.spacing(mx))};
-    ${setu('margin-vertical', theme.helpers.spacing(mv))};
-    ${setu('margin-left', theme.helpers.spacing(ml))};
-    ${setu('margin-right', theme.helpers.spacing(mr))};
-    ${setu('margin-bottom', theme.helpers.spacing(mb))};
-    ${setu('margin-top', theme.helpers.spacing(mt))};
-    ${setu('padding-left', theme.helpers.spacing(pl))};
-    ${setu('padding-right', theme.helpers.spacing(pr))};
-    ${setu('padding-bottom', theme.helpers.spacing(pb))};
-    ${setu('padding-top', theme.helpers.spacing(pt))}; */
-  `;
-};
-
 /**
  * Box is an abstract component for building layouts. All MangaYomu components are built upon this component.
  * @component
  */
 const Box = styled.View<BoxProps>`
-  ${style}
+  ${(props) => {
+    const {
+      theme,
+      'align-items': alignItems = 'stretch',
+      'align-self': alignSelf = 'stretch',
+      'background-color': bgColor,
+      'box-shadow': boxShadow,
+      'justify-content': justifyContent = 'flex-start',
+      flex,
+      'flex-grow': flexGrow,
+      'flex-shrink': flexShrink,
+      'flex-wrap': flexWrap,
+      p,
+      m,
+      px,
+      pv,
+      mx,
+      mv,
+      ml,
+      mr,
+      mb,
+      mt,
+      pl,
+      pr,
+      pt,
+      pb,
+      width = 'auto',
+      height = 'auto',
+    } = props;
+
+    return css`
+      align-items: ${alignItems};
+      align-self: ${alignSelf};
+      ${boxShadow && theme.style.shadow};
+      flex: ${flex ? 1 : 0};
+      flex-grow: ${flexGrow ? 1 : 0};
+      flex-shrink: ${flexShrink ? 1 : 0};
+      justify-content: ${justifyContent};
+      ${(() => {
+        if (bgColor == null) return;
+        if (bgColor in theme.palette.background)
+          return css`
+            background-color: ${theme.palette.background[
+              bgColor as keyof BackgroundColor
+            ]};
+          `;
+        return css`
+          background-color: ${theme.palette[bgColor as ButtonColors].main};
+        `;
+      })()}
+      ${boxShadow && theme.style.shadow};
+      ${setwandh('height', height)};
+      ${setwandh('width', width)};
+      ${set('flex-wrap', flexWrap)};
+      ${setu(theme, 'padding', p)};
+      ${setu(theme, 'margin', m)};
+      ${setu(theme, 'padding-horizontal', px)};
+      ${setu(theme, 'padding-vertical', pv)};
+      ${setu(theme, 'margin-horizontal', mx)};
+      ${setu(theme, 'margin-vertical', mv)};
+      ${setu(theme, 'margin-left', ml)};
+      ${setu(theme, 'margin-right', mr)};
+      ${setu(theme, 'margin-bottom', mb)};
+      ${setu(theme, 'margin-top', mt)};
+      ${setu(theme, 'padding-left', pl)};
+      ${setu(theme, 'padding-right', pr)};
+      ${setu(theme, 'padding-bottom', pb)};
+      ${setu(theme, 'padding-top', pt)};
+    `;
+  }}
 `;
 
 export default Box;
