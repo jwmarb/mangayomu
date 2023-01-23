c6e6506df37a5bff2de0ce9a83e367d674c8dbdc
specify_ <dieptuber@gmail.com>
2023-01-22 20:58:57 -0700

    create helper for redux

---
+++ apps/mobile/src/helpers/getMangaHost.ts
@@ -0,0 +1,19 @@
+import { MangaHost } from '@mangayomu/mangascraper';
+import { AppState } from '@redux/main';
+
+/**
+ * Get the MangaHost from redux or not. Comes with safety from edge cases.
+ * @param state The App state from redux
+ * @param overrideSource Name of a different source. Use this parameter if the code does not need to use the user-defined selected source.
+ * @returns
+ */
+export default function getMangaHost(state: AppState, overrideSource?: string) {
+  const p = MangaHost.getAvailableSources();
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  if (overrideSource != null) return p.get(overrideSource)!;
+  if (state.host.name == null)
+    throw Error(
+      'Cannot derive a MangaHost from state.host.name because it is null.',
+    );
+  return p.get(state.host.name)!;
+}
