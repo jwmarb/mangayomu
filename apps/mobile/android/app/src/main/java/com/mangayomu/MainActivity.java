f1c70d74cb5b83c23973078b6789a0eb3a65c80d
specify_ <dieptuber@gmail.com>
2023-01-22 22:42:09 -0700

    rename `MyApp` to `mangayomu`

--- apps/mobile/android/app/src/main/java/com/mangayomu/MainActivity.java
+++ apps/mobile/android/app/src/main/java/com/mangayomu/MainActivity.java
@@ -1,4 +1,5 @@
-package com.myapp;
+package com.mangayomu;
+
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -10,7 +11,8 @@ import android.content.res.Configuration;
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
   @Override
@@ -25,15 +27,17 @@ public class MainActivity extends ReactActivity {
 
   @Override
   public void onConfigurationChanged(Configuration newConfig) {
-      super.onConfigurationChanged(newConfig);
-      Intent intent = new Intent("onConfigurationChanged");
-      intent.putExtra("newConfig", newConfig);
-      this.sendBroadcast(intent);
+    super.onConfigurationChanged(newConfig);
+    Intent intent = new Intent("onConfigurationChanged");
+    intent.putExtra("newConfig", newConfig);
+    this.sendBroadcast(intent);
   }
 
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util
+   * class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and
+   * Concurrent React
    * (aka React 18) with two boolean flags.
    */
   @Override
@@ -43,8 +47,9 @@ public class MainActivity extends ReactActivity {
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
-        // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
+        // If you opted-in for the New Architecture, we enable Concurrent React (i.e.
+        // React 18).
         DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
-        );
+    );
   }
 }
--- apps/mobile/android/app/src/main/java/com/mangayomu/MainApplication.java
+++ apps/mobile/android/app/src/main/java/com/mangayomu/MainApplication.java
@@ -1,4 +1,4 @@
-package com.myapp;
+package com.mangayomu;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
