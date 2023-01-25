cbcd67450b30ebb7201bf2cd6f4c62bc99cdce4a
specify_ <dieptuber@gmail.com>
2023-01-24 19:22:35 -0700

    create home header

--- apps/mobile/src/components/NavHeader/NavHeader.tsx
+++ apps/mobile/src/components/NavHeader/NavHeader.tsx
@@ -6,24 +6,7 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 
 const NavHeader: React.FC<NativeStackHeaderProps> = (props) => {
-  const {
-    route,
-    options: { headerTitle = route.name },
-    navigation,
-  } = props;
-  const theme = useTheme();
-  return (
-    <Box
-      pt={StatusBar.currentHeight ?? 0}
-      background-color={theme.palette.background.default}
-    >
-      <Box mx="s" my="m">
-        <Text variant="header" bold align="center">
-          {headerTitle as string}
-        </Text>
-      </Box>
-    </Box>
-  );
+  return null;
 };
 
 export default NavHeader;
--- apps/mobile/src/components/NavHeader/TabHeader.tsx
+++ apps/mobile/src/components/NavHeader/TabHeader.tsx
@@ -1,9 +1,29 @@
+import Box from '@components/Box';
+import Text from '@components/Text';
+import { useTheme } from '@emotion/react';
 import { BottomTabHeaderProps } from '@react-navigation/bottom-tabs';
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 const TabHeader: React.FC<BottomTabHeaderProps> = (props) => {
-  const { navigation, route, options } = props;
-  return null;
+  const {
+    route,
+    options: { headerTitle = route.name },
+    navigation,
+  } = props;
+  const theme = useTheme();
+  return (
+    <Box
+      pt={StatusBar.currentHeight ?? 0}
+      background-color={theme.palette.background.default}
+    >
+      <Box mx="s" my="m">
+        <Text variant="header" bold align="center">
+          {headerTitle as string}
+        </Text>
+      </Box>
+    </Box>
+  );
 };
 
 export default TabHeader;
