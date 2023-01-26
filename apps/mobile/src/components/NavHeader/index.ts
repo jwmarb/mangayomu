d953f4ba7ee98cea27d60f2ef1881dff9a1a43b5
specify_ <dieptuber@gmail.com>
2023-01-25 22:57:31 -0700

    expand on Navigation styyles

--- apps/mobile/src/components/NavHeader/index.ts
+++ apps/mobile/src/components/NavHeader/index.ts
@@ -1,2 +1,15 @@
+import { StatusBar } from 'react-native';
+import { moderateScale, ScaledSheet } from 'react-native-size-matters';
+export const NAVHEADER_HEIGHT = moderateScale(80);
+export const NavStyles = ScaledSheet.create({
+  header: {
+    height: '90@ms',
+    paddingTop: StatusBar.currentHeight ?? 0,
+    justifyContent: 'center',
+  },
+  offset: {
+    paddingTop: '90@ms',
+  },
+});
 export { default as StackHeader } from './NavHeader';
 export { default as TabHeader } from './TabHeader';
