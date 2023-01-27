a1a555cddf7388ddfb2f4286c225a0603814a329
Joseph Marbella <dieptuber@gmail.com>
2023-01-26 18:52:13 -0700

    create `Checkbox` component

---
+++ apps/mobile/src/components/Checkbox/Checkbox.interfaces.d.ts
@@ -0,0 +1,5 @@
+export interface CheckboxProps {
+  checked?: boolean;
+  onChange?: (newVal: boolean) => void;
+  defaultState?: boolean;
+}
---
+++ apps/mobile/src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,93 @@
+import Box from '@components/Box';
+import Icon from '@components/Icon';
+import { useTheme } from '@emotion/react';
+import React from 'react';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import Animated, {
+  interpolateColor,
+  runOnUI,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import { moderateScale, ScaledSheet } from 'react-native-size-matters';
+import { CheckboxProps } from './Checkbox.interfaces';
+
+const Checkbox: React.FC<CheckboxProps> = (props) => {
+  const { checked, onChange = () => void 0, defaultState = false } = props;
+  const theme = useTheme();
+  const opacity = useSharedValue(defaultState || checked ? 1 : 0);
+  const backgroundColor = useDerivedValue(() =>
+    interpolateColor(
+      opacity.value,
+      [0, 1],
+      [
+        theme.mode === 'light'
+          ? 'rgba(0, 0, 0, 0.15)'
+          : 'rgba(255, 255, 255, 0.1)',
+        theme.palette.primary.main,
+      ],
+    ),
+  );
+  const boxStyle = useAnimatedStyle(() => ({
+    backgroundColor: backgroundColor.value,
+    borderColor: backgroundColor.value,
+  }));
+  const checkmarkStyle = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+  }));
+
+  React.useEffect(() => {
+    if (checked != null) {
+      if (!checked) opacity.value = withTiming(0, { duration: 100 });
+      else opacity.value = withTiming(1, { duration: 100 });
+    }
+  }, [checked]);
+
+  function handleOnPress() {
+    if (checked == null) {
+      if (opacity.value === 1) {
+        onChange(false);
+        opacity.value = withTiming(0, { duration: 100 });
+      } else {
+        onChange(true);
+        opacity.value = withTiming(1, { duration: 100 });
+      }
+    }
+  }
+  return (
+    <Box
+      width={moderateScale(48)}
+      height={moderateScale(48)}
+      border-radius={10000}
+    >
+      <BorderlessButton onPress={handleOnPress} style={styles.borderlessButton}>
+        <Box
+          as={Animated.View}
+          p={moderateScale(2)}
+          align-self="center"
+          border-width={moderateScale(2)}
+          border-radius={moderateScale(4)}
+          style={boxStyle}
+        >
+          <Animated.View style={checkmarkStyle}>
+            <Icon type="font" name="check-bold" color="primary@contrast" />
+          </Animated.View>
+        </Box>
+      </BorderlessButton>
+    </Box>
+  );
+};
+
+const styles = ScaledSheet.create({
+  borderlessButton: {
+    width: '48@ms',
+    height: '48@ms',
+    borderRadius: 10000,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Checkbox;
