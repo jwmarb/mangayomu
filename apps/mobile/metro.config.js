6562ce8956f12feeec8610df3e86a61017fbe4f6
Joseph Marbella <dieptuber@gmail.com>
2023-01-27 12:49:23 -0700

    use `nohoist` to fix production build

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,5 +1,6 @@
 const { getMetroTools } = require('react-native-monorepo-tools');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
+const path = require('path');
 
 /**
  * Metro configuration for React Native
@@ -20,7 +21,10 @@ module.exports = {
     }),
   },
   // watchFolders: [path.resolve(__dirname, '../../node_modules')],
-  watchFolders: metroTools.watchFolders,
+  watchFolders: [
+    ...metroTools.watchFolders,
+    path.resolve(__dirname, 'node_modules'),
+  ],
   resolver: {
     blockList: exclusionList(metroTools.blockList),
     extraNodeModules: metroTools.extraNodeModules,
