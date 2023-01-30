6fddba088812bce274244d0728277a866118d7c8
specify_ <dieptuber@gmail.com>
2023-01-29 20:43:20 -0700

    include abstract types

--- packages/mangascraper/src/index.ts
+++ packages/mangascraper/src/index.ts
@@ -2,3 +2,4 @@ export { default as MangaHost } from './scraper/scraper.abstract';
 export { default as validator } from './validator';
 export { default as MangaSee } from './MangaSee';
 export { default as MangaPark_v3 } from './MangaPark_v3';
+export * from './scraper/scraper.interfaces';
