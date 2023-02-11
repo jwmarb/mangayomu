8da5e4d0314f626a99c01b4cccef28de916d22db
specify_ <dieptuber@gmail.com>
2023-02-11 10:02:03 -0700

    update schema

--- apps/mobile/src/realm/main.ts
+++ apps/mobile/src/realm/main.ts
@@ -1,9 +1,13 @@
 import { ChapterSchema } from '@database/schemas/Chapter';
 import { createRealmContext } from '@realm/react';
-import { MangaSchema, MangaRatingSchema } from './schemas/Manga';
+import {
+  MangaSchema,
+  MangaRatingSchema,
+  MangaStatusSchema,
+} from './schemas/Manga';
 
 export const { RealmProvider, useRealm, useQuery, useObject } =
   createRealmContext({
-    schema: [MangaSchema, ChapterSchema, MangaRatingSchema],
-    schemaVersion: 3,
+    schema: [MangaSchema, ChapterSchema, MangaRatingSchema, MangaStatusSchema],
+    schemaVersion: 4,
   });
