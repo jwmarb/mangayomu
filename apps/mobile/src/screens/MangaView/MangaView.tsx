5975ad14d90cbf40773950c8aaccfbfca0b0cea6
specify_ <dieptuber@gmail.com>
2023-02-12 10:33:11 -0700

    use useQuery to get chapters

--- apps/mobile/src/screens/MangaView/MangaView.tsx
+++ apps/mobile/src/screens/MangaView/MangaView.tsx
@@ -2,13 +2,13 @@ import Box from '@components/Box';
 import useCollapsibleHeader from '@hooks/useCollapsibleHeader';
 import { RootStackProps } from '@navigators/Root/Root.interfaces';
 import { useManga } from '@database/schemas/Manga';
-import { FlashList } from '@shopify/flash-list';
+import { FlashList, ListRenderItem } from '@shopify/flash-list';
 import React from 'react';
 import MangaViewerHeader from './components/MangaViewerHeader';
 import { useTheme } from '@emotion/react';
 import IconButton from '@components/IconButton';
 import Icon from '@components/Icon';
-import { useRealm } from '@database/main';
+import { useQuery, useRealm } from '@database/main';
 import displayMessage from '@helpers/displayMessage';
 import { NativeScrollEvent, NativeSyntheticEvent } from 'react-native';
 import {
@@ -18,6 +18,8 @@ import {
   useSharedValue,
 } from 'react-native-reanimated';
 import { NAVHEADER_HEIGHT } from '@components/NavHeader';
+import Text from '@components/Text';
+import { ChapterSchema } from '@database/schemas/Chapter';
 
 const MangaView: React.FC<RootStackProps<'MangaView'>> = (props) => {
   const {
@@ -70,11 +72,25 @@ const MangaView: React.FC<RootStackProps<'MangaView'>> = (props) => {
     onScroll(e);
     scrollPosition.value = e.nativeEvent.contentOffset.y;
   }
+
+  const chapters = useQuery(ChapterSchema);
+
+  const multilingualChapters = React.useMemo(
+    () => chapters.filtered(`manga == "${params.link}"`),
+    [chapters],
+  );
+
+  const selectedLanguageChapters = React.useMemo(
+    () => [...multilingualChapters.filtered('language == "en"')],
+    [multilingualChapters],
+  );
+
   return (
     <FlashList
-      data={manga?.chapters}
+      data={selectedLanguageChapters as ChapterSchema[]}
       ListHeaderComponent={
         <MangaViewerHeader
+          queriedChaptersForManga={multilingualChapters}
           onBookmark={handleOnBookmark}
           status={status}
           manga={params}
@@ -85,11 +101,15 @@ const MangaView: React.FC<RootStackProps<'MangaView'>> = (props) => {
         />
       }
       ListFooterComponent={<Box style={contentContainerStyle} />}
-      renderItem={null}
+      renderItem={renderItem}
       estimatedItemSize={5}
       {...{ onScroll: handleOnScroll }}
     />
   );
 };
+
+const renderItem: ListRenderItem<ChapterSchema> = ({ item }) => (
+  <Text>{item.name}</Text>
+);
 
 export default MangaView;
--- apps/mobile/src/screens/MangaView/components/MangaViewerHeader/MangaViewerHeader.interfaces.d.ts
+++ apps/mobile/src/screens/MangaView/components/MangaViewerHeader/MangaViewerHeader.interfaces.d.ts
@@ -1,7 +1,9 @@
 import { NavStyles } from '@components/NavHeader';
 import { IMangaSchema, FetchMangaMetaStatus } from '@database/schemas/Manga';
 import { Manga } from '@mangayomu/mangascraper';
+import Realm from 'realm';
 import React from 'react';
+import { ChapterSchema } from '@database/schemas/Chapter';
 
 export interface MangaViewerHeaderProps extends React.PropsWithChildren {
   meta?: IMangaSchema;
@@ -11,6 +13,9 @@ export interface MangaViewerHeaderProps extends React.PropsWithChildren {
   refresh: () => void;
   scrollViewStyle: typeof NavStyles.offset;
   onBookmark: () => void;
+  queriedChaptersForManga: Realm.Results<
+    ChapterSchema & Realm.Object<unknown, never>
+  >;
 }
 
 export interface MangaMetaProperty<TProperty extends keyof IMangaSchema> {
