d7c00cae03a33f4e4961cc3673cb94256d90553c
specify_ <dieptuber@gmail.com>
2023-02-12 15:26:21 -0700

    add touchups

--- apps/mobile/src/screens/MangaView/MangaView.tsx
+++ apps/mobile/src/screens/MangaView/MangaView.tsx
@@ -8,7 +8,7 @@ import MangaViewerHeader from './components/MangaViewerHeader';
 import { useTheme } from '@emotion/react';
 import IconButton from '@components/IconButton';
 import Icon from '@components/Icon';
-import { useQuery, useRealm } from '@database/main';
+import { useObject, useQuery, useRealm } from '@database/main';
 import displayMessage from '@helpers/displayMessage';
 import { NativeScrollEvent, NativeSyntheticEvent } from 'react-native';
 import {
@@ -20,6 +20,13 @@ import {
 import { NAVHEADER_HEIGHT } from '@components/NavHeader';
 import Text from '@components/Text';
 import { ChapterSchema } from '@database/schemas/Chapter';
+import RowChapter from '@screens/MangaView/components/RowChapter';
+import { ROW_CHAPTER_HEIGHT } from '@screens/MangaView/components/RowChapter/RowChapter';
+import Divider from '@components/Divider';
+import { CustomBottomSheet } from '@components/CustomBottomSheet';
+import BottomSheet from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheet/BottomSheet';
+import MangaViewModal from '@screens/MangaView/components/MangaViewModal';
+import { FlatList } from 'react-native-gesture-handler';
 
 const MangaView: React.FC<RootStackProps<'MangaView'>> = (props) => {
   const {
@@ -27,6 +34,7 @@ const MangaView: React.FC<RootStackProps<'MangaView'>> = (props) => {
     navigation,
   } = props;
   const theme = useTheme();
+  const ref = React.useRef<BottomSheet>(null);
   const { manga, status, error, refresh, update } = useManga(params, {
     preferLocal: false,
   });
@@ -73,6 +81,10 @@ const MangaView: React.FC<RootStackProps<'MangaView'>> = (props) => {
     scrollPosition.value = e.nativeEvent.contentOffset.y;
   }
 
+  function handleOnOpenMenu() {
+    ref.current?.snapToIndex(1);
+  }
+
   const chapters = useQuery(ChapterSchema);
 
   const multilingualChapters = React.useMemo(
@@ -81,36 +93,48 @@ const MangaView: React.FC<RootStackProps<'MangaView'>> = (props) => {
   );
 
   const selectedLanguageChapters = React.useMemo(
-    () => [...multilingualChapters.filtered('language == "en"')],
+    () => multilingualChapters.filtered('language == "en"'),
     [multilingualChapters],
   );
 
   return (
-    <FlashList
-      data={selectedLanguageChapters as ChapterSchema[]}
-      ListHeaderComponent={
-        <MangaViewerHeader
-          numberOfSelectedLanguageChapters={selectedLanguageChapters.length}
-          queriedChaptersForManga={multilingualChapters}
-          onBookmark={handleOnBookmark}
-          status={status}
-          manga={params}
-          meta={manga}
-          error={error}
-          refresh={refresh}
-          scrollViewStyle={scrollViewStyle}
+    <>
+      <FlashList
+        data={manga?.chapters ?? []}
+        ListHeaderComponent={
+          <MangaViewerHeader
+            onOpenMenu={handleOnOpenMenu}
+            numberOfSelectedLanguageChapters={selectedLanguageChapters.length}
+            queriedChaptersForManga={multilingualChapters}
+            onBookmark={handleOnBookmark}
+            status={status}
+            manga={params}
+            meta={manga}
+            error={error}
+            refresh={refresh}
+            scrollViewStyle={scrollViewStyle}
+          />
+        }
+        ListFooterComponent={<Box style={contentContainerStyle} />}
+        renderItem={renderItem}
+        ItemSeparatorComponent={Divider}
+        estimatedItemSize={ROW_CHAPTER_HEIGHT}
+        {...{ onScroll: handleOnScroll }}
+      />
+      {manga != null && (
+        <MangaViewModal
+          ref={ref}
+          sortMethod={manga?.sortChaptersBy ?? 'Chapter number'}
+          reversed={manga?.reversedSort ?? false}
+          mangaLink={params.link}
         />
-      }
-      ListFooterComponent={<Box style={contentContainerStyle} />}
-      renderItem={renderItem}
-      estimatedItemSize={5}
-      {...{ onScroll: handleOnScroll }}
-    />
+      )}
+    </>
   );
 };
 
-const renderItem: ListRenderItem<ChapterSchema> = ({ item }) => (
-  <Text>{item.name}</Text>
+const renderItem: ListRenderItem<string> = ({ item }) => (
+  <RowChapter rowChapterKey={item} />
 );
 
 export default MangaView;
