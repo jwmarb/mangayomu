114ad405f3a93e0656a1917ed33b27747075107b
specify_ <dieptuber@gmail.com>
2023-02-12 17:38:14 -0700

    remove unnecessary sort

--- apps/mobile/src/screens/MangaView/components/MangaViewModal/Tabs/Sort/Sort.tsx
+++ apps/mobile/src/screens/MangaView/components/MangaViewModal/Tabs/Sort/Sort.tsx
@@ -1,49 +1,28 @@
 import {
   KEYS_OF_SORT_CHAPTERS_BY,
   SortChaptersMethod,
-  SORT_CHAPTERS_BY,
   useManga,
 } from '@database/schemas/Manga';
 import { BottomSheetFlatList } from '@gorhom/bottom-sheet';
 import { SortProps } from './Sort.interfaces';
 import React from 'react';
 import { ListRenderItem } from 'react-native';
-import Text from '@components/Text';
 import SortItem from '@screens/Welcome/components/MainSourceSelector/components/Header/Tabs/Sort/Sort.item';
-import { inPlaceSort } from 'fast-sort';
 
 const Sort: React.FC<SortProps> = (props) => {
   const { update } = useManga(props.mangaLink);
 
   const onChange = React.useCallback(
     (t: SortChaptersMethod) => {
-      update((obj, getChapter) => {
-        const p = (a: string) => {
-          const parsedA = getChapter(a);
-          if (parsedA == null)
-            throw Error(`${a} does not exist in Chapters collection`);
-          return SORT_CHAPTERS_BY[t](parsedA);
-        };
+      update((obj) => {
         obj.sortChaptersBy = t;
-        obj.chapters = inPlaceSort([...obj.chapters]).by([
-          obj.reversedSort ? { desc: p } : { asc: p },
-        ]);
       });
     },
     [update],
   );
   const onToggleReverse = React.useCallback(() => {
-    update((obj, getChapter) => {
-      const p = (a: string) => {
-        const parsedA = getChapter(a);
-        if (parsedA == null)
-          throw Error(`${a} does not exist in Chapters collection`);
-        return SORT_CHAPTERS_BY[obj.sortChaptersBy](parsedA);
-      };
+    update((obj) => {
       obj.reversedSort = !obj.reversedSort;
-      obj.chapters = inPlaceSort([...obj.chapters]).by([
-        obj.reversedSort ? { desc: p } : { asc: p },
-      ]);
     });
   }, [update]);
   const renderItem = React.useCallback<ListRenderItem<SortChaptersMethod>>(
