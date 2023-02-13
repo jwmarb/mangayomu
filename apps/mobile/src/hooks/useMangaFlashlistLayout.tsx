f8080091b84dbf7e0668544a28f178927f9c84cf
specify_ <dieptuber@gmail.com>
2023-02-12 18:55:30 -0700

    move common functionality to hook

---
+++ apps/mobile/src/hooks/useMangaFlashlistLayout.tsx
@@ -0,0 +1,54 @@
+import Book from '@components/Book';
+import Box from '@components/Box';
+import { useTheme } from '@emotion/react';
+import { Manga } from '@mangayomu/mangascraper';
+import { ListRenderItem } from '@shopify/flash-list';
+import React from 'react';
+import { Dimensions, useWindowDimensions } from 'react-native';
+
+const Item: React.FC<{ item: Manga }> = React.memo(({ item }) => (
+  <Box my="s" align-items="center" flex-grow>
+    <Book manga={item} />
+  </Box>
+));
+
+export default function useMangaFlashlistLayout<
+  T extends Manga = Manga,
+>(bookDimensions: { width: number; height: number }) {
+  const keyExtractor = React.useCallback((i: T) => i.link, []);
+
+  const renderItem: ListRenderItem<T> = React.useCallback(
+    ({ item }) => <Item item={item} />,
+    [],
+  );
+
+  const { width } = useWindowDimensions();
+  const theme = useTheme();
+  const padding = 2 * theme.style.spacing.s;
+  const [columns, setColumns] = React.useState<number>(
+    Math.round(
+      (width - 2 * theme.style.spacing.m) / (bookDimensions.width + padding),
+    ),
+  );
+  React.useLayoutEffect(() => {
+    const p = Dimensions.addEventListener('change', ({ window: { width } }) => {
+      setColumns(
+        Math.round(
+          (width - 2 * theme.style.spacing.m) /
+            (bookDimensions.width + padding),
+        ),
+      );
+    });
+    return () => {
+      p.remove();
+    };
+  }, []);
+
+  return {
+    columns,
+    key: columns,
+    estimatedItemSize: bookDimensions.height + padding,
+    keyExtractor,
+    renderItem,
+  };
+}
--- apps/mobile/src/screens/BasicMangaList/BasicMangaList.tsx
+++ apps/mobile/src/screens/BasicMangaList/BasicMangaList.tsx
@@ -2,70 +2,24 @@ import connector, {
   ConnectedBasicMangaListProps,
 } from './BasicMangaList.redux';
 import React from 'react';
-import Animated from 'react-native-reanimated';
-import {
-  keyExtractor,
-  MangaSeparator,
-} from '@screens/Explore/Explore.flatlist';
 import useCollapsibleHeader from '@hooks/useCollapsibleHeader';
-import { Manga } from '@mangayomu/mangascraper';
-import Book, { bookDimensions } from '@components/Book';
-import {
-  Dimensions,
-  StyleProp,
-  useWindowDimensions,
-  ViewStyle,
-} from 'react-native';
-import { useTheme } from '@emotion/react';
-import { moderateScale } from 'react-native-size-matters';
+import { bookDimensions } from '@components/Book';
 import Box from '@components/Box';
-import { FlashList, ListRenderItem } from '@shopify/flash-list';
+import { FlashList } from '@shopify/flash-list';
+import useMangaFlashlistLayout from '@hooks/useMangaFlashlistLayout';
 
 const BasicMangaList: React.FC<ConnectedBasicMangaListProps> = (props) => {
   const { mangas, title } = props;
-  const { width } = useWindowDimensions();
   const { onScroll, contentContainerStyle, scrollViewStyle } =
     useCollapsibleHeader({ headerTitle: title });
-  const theme = useTheme();
-  const padding = 2 * theme.style.spacing.s;
-  // const getItemLayout = React.useCallback(
-  //   (data: Manga[] | null | undefined, index: number) => ({
-  //     length: bookDimensions.height + padding,
-  //     offset: (bookDimensions.height + padding) * index,
-  //     index,
-  //   }),
-  //   [padding],
-  // );
-  const [columns, setColumns] = React.useState<number>(
-    Math.round(
-      (width - 2 * theme.style.spacing.m) / (bookDimensions.width + padding),
-    ),
-  );
-  React.useLayoutEffect(() => {
-    const p = Dimensions.addEventListener('change', ({ window: { width } }) => {
-      setColumns(
-        Math.round(
-          (width - 2 * theme.style.spacing.m) /
-            (bookDimensions.width + padding),
-        ),
-      );
-    });
-    return () => {
-      p.remove();
-    };
-  }, []);
+  const { estimatedItemSize, columns, renderItem, keyExtractor, key } =
+    useMangaFlashlistLayout(bookDimensions);
 
   return (
     <FlashList
-      estimatedItemSize={bookDimensions.height + padding}
-      // windowSize={7}
-      // maxToRenderPerBatch={8}
-      // initialNumToRender={0}
-      // updateCellsBatchingPeriod={100}
-      // getItemLayout={getItemLayout}
-
+      estimatedItemSize={estimatedItemSize}
       data={mangas}
-      key={columns}
+      key={key}
       numColumns={columns}
       renderItem={renderItem}
       keyExtractor={keyExtractor}
@@ -78,13 +32,5 @@ const BasicMangaList: React.FC<ConnectedBasicMangaListProps> = (props) => {
     />
   );
 };
-
-const renderItem: ListRenderItem<Manga> = ({ item }) => <Item item={item} />;
-
-const Item: React.FC<{ item: Manga }> = React.memo(({ item }) => (
-  <Box my="s" align-items="center" flex-grow>
-    <Book manga={item} />
-  </Box>
-));
 
 export default connector(BasicMangaList);
