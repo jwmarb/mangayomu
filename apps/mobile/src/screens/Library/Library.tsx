890d78c5d9a585b64f1c935e0bf91c455edf498c
specify_ <dieptuber@gmail.com>
2023-02-19 23:28:59 -0700

    better filter results

--- apps/mobile/src/screens/Library/Library.tsx
+++ apps/mobile/src/screens/Library/Library.tsx
@@ -19,11 +19,15 @@ import { inPlaceSort } from 'fast-sort';
 import connector, { ConnectedLibraryProps } from './Library.redux';
 import { SORT_LIBRARY_BY } from '@redux/slices/library';
 import { FilterState } from '@redux/slices/mainSourceSelector';
+import Checkbox from '@components/Checkbox';
+import Badge from '@components/Badge';
 
 const Library: React.FC<ConnectedLibraryProps> = ({
   sortBy,
   reversed,
   filters,
+  noSourcesSelectedInFilter,
+  numberOfAppliedFilters,
 }) => {
   const ref = React.useRef<BottomSheetMethods>(null);
   const mangas = useQuery(MangaSchema);
@@ -74,13 +78,15 @@ const Library: React.FC<ConnectedLibraryProps> = ({
     useCollapsibleTabHeader({
       headerTitle: 'Library',
       headerRight:
-        data.length > 0 ? (
-          <IconButton
-            icon={<Icon type="font" name="filter-menu" />}
-            onPress={handleOnPress}
-          />
+        mangasInLibrary.length > 0 ? (
+          <Badge type="number" count={numberOfAppliedFilters} color="primary">
+            <IconButton
+              icon={<Icon type="font" name="filter-menu" />}
+              onPress={handleOnPress}
+            />
+          </Badge>
         ) : null,
-      dependencies: [data.length > 0],
+      dependencies: [mangasInLibrary.length > 0, numberOfAppliedFilters],
     });
   return (
     <>
@@ -98,50 +104,94 @@ const Library: React.FC<ConnectedLibraryProps> = ({
           <>{data.length > 0 && <Box style={contentContainerStyle} />}</>
         }
         estimatedItemSize={estimatedItemSize}
-        ListEmptyComponent={ListEmptyComponent}
+        ListEmptyComponent={
+          <ListEmptyComponent
+            libraryIsEmpty={mangasInLibrary.length === 0}
+            noSourcesSelected={noSourcesSelectedInFilter}
+          />
+        }
       />
-      <Freeze freeze={data.length === 0}>
+      <Freeze freeze={mangasInLibrary.length === 0}>
         <LibraryFilterMenu ref={ref} />
       </Freeze>
     </>
   );
 };
 
-const ListEmptyComponent = React.memo(() => {
+const ListEmptyComponent: React.FC<{
+  libraryIsEmpty: boolean;
+  noSourcesSelected: boolean;
+}> = React.memo(({ libraryIsEmpty, noSourcesSelected }) => {
   const { height } = useWindowDimensions();
 
+  if (libraryIsEmpty)
+    return (
+      <Box height={height} p="m" flex-grow justify-content="center">
+        <Text variant="header" align="center">
+          Your library is empty
+        </Text>
+        <Text color="textSecondary" align="center">
+          To add a manga to your library, navigate to a manga and press{' '}
+          <Icon type="font" name="bookmark-outline" /> at the top right corner,
+          or press the
+        </Text>
+        <Stack space="s" flex-direction="row" align-self="center">
+          <Box
+            background-color="secondary"
+            border-radius="@theme"
+            px="s"
+            py={moderateScale(3)}
+            align-self="center"
+          >
+            <Text color="secondary@contrast" variant="bottom-tab">
+              <Icon
+                type="font"
+                name="bookmark"
+                color="secondary@contrast"
+                variant="bottom-tab"
+              />{' '}
+              Add
+            </Text>
+          </Box>
+          <Text color="textSecondary" align="center">
+            button.
+          </Text>
+        </Stack>
+      </Box>
+    );
+  if (noSourcesSelected)
+    return (
+      <Box height={height} p="m" flex-grow justify-content="center">
+        <Text variant="header" align="center">
+          No sources selected
+        </Text>
+        <Text color="textSecondary" align="center">
+          Your library is showing nothing because you did not select which
+          sources to only display. Therefore, it will show no results.
+        </Text>
+        <Stack
+          flex-direction="row"
+          space="s"
+          align-items="center"
+          justif-content="center"
+          align-self="center"
+        >
+          <Text color="textSecondary">A checkbox looks like this:</Text>
+          <Checkbox />
+        </Stack>
+        <Text color="textSecondary" align="center">
+          Feel free to interact with it :)
+        </Text>
+      </Box>
+    );
   return (
     <Box height={height} p="m" flex-grow justify-content="center">
       <Text variant="header" align="center">
-        Your library is empty
+        No results found
       </Text>
       <Text color="textSecondary" align="center">
-        To add a manga to your library, navigate to a manga and press{' '}
-        <Icon type="font" name="bookmark-outline" /> at the top right corner, or
-        press the
+        There are no mangas in your library that match your desired filters.
       </Text>
-      <Stack space="s" flex-direction="row" align-self="center">
-        <Box
-          background-color="secondary"
-          border-radius="@theme"
-          px="s"
-          py={moderateScale(3)}
-          align-self="center"
-        >
-          <Text color="secondary@contrast" variant="bottom-tab">
-            <Icon
-              type="font"
-              name="bookmark"
-              color="secondary@contrast"
-              variant="bottom-tab"
-            />{' '}
-            Add
-          </Text>
-        </Box>
-        <Text color="textSecondary" align="center">
-          button.
-        </Text>
-      </Stack>
     </Box>
   );
 });
