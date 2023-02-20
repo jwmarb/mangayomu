f98696d97b039d3abc127b218f555233776e774f
specify_ <dieptuber@gmail.com>
2023-02-19 23:28:41 -0700

    add props to library

--- apps/mobile/src/screens/Library/Library.redux.ts
+++ apps/mobile/src/screens/Library/Library.redux.ts
@@ -5,6 +5,9 @@ const mapStateToProps = (state: AppState) => ({
   reversed: state.library.reversed,
   sortBy: state.library.sortBy,
   filters: state.library.filters,
+  noSourcesSelectedInFilter:
+    state.library.numberOfSelectedSourcesInFilter === 0,
+  numberOfAppliedFilters: state.library.numberOfFiltersApplied,
 });
 
 const connector = connect(mapStateToProps);
