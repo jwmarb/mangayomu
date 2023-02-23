1209a36d83b9c98698c8a66840dc3911073d10e9
specify_ <dieptuber@gmail.com>
2023-02-23 12:12:55 -0700

    combine genres to avoid repetition

--- apps/mobile/src/screens/Explore/components/GenresList/GenresList.tsx
+++ apps/mobile/src/screens/Explore/components/GenresList/GenresList.tsx
@@ -4,6 +4,8 @@ import { Stack } from '@components/Stack';
 import Tag from '@components/Tag';
 import Text from '@components/Text';
 import { useTheme } from '@emotion/react';
+import integrateSortedList from '@helpers/integrateSortedList';
+import { StringComparator } from '@mangayomu/algorithms';
 import { MangaHost } from '@mangayomu/mangascraper';
 import React from 'react';
 import { ListRenderItem } from 'react-native';
@@ -18,10 +20,21 @@ import connector, { ConnectedGenresListProps } from './GenresList.redux';
 const ItemSeparatorComponent = React.memo(() => <Box m={moderateScale(4)} />);
 const GenresList: React.FC<ConnectedGenresListProps> = (props) => {
   const { source } = props;
-  const genres = React.useMemo(
-    () => source.getGenres(),
-    [source.getSourcesLength()],
-  );
+  const genres = React.useMemo(() => {
+    const collection = source.getRawGenres();
+    const uniq = new Set<string>();
+    for (const source in collection) {
+      for (const genre of collection[source]) {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        uniq.add(genre);
+      }
+    }
+    const uniqArr: string[] = [];
+    for (const genre of uniq) {
+      integrateSortedList(uniqArr, StringComparator).add(genre);
+    }
+    return uniqArr;
+  }, [source.getSourcesLength()]);
   const theme = useTheme();
   const scrollViewStyles = React.useMemo(
     () => ({ paddingHorizontal: theme.style.spacing.m }),
@@ -53,22 +66,19 @@ const GenresList: React.FC<ConnectedGenresListProps> = (props) => {
   );
 };
 
-const renderItem: ListRenderItem<{ genre: string; source: string }> = ({
-  item,
-}) => (
+const renderItem: ListRenderItem<string> = ({ item }) => (
   <Tag
-    icon={
-      <Icon
-        type="image"
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        name={MangaHost.getAvailableSources().get(item.source)!.getIcon()}
-        size={moderateScale(18)}
-      />
-    }
-    label={item.genre}
+    // icon={
+    //   <Icon
+    //     type="image"
+    //     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    //     name={MangaHost.getAvailableSources().get(item.source)!.getIcon()}
+    //     size={moderateScale(18)}
+    //   />
+    // }
+    label={item}
   />
 );
-const keyExtractor = (i: { genre: string; source: string }) =>
-  i.source + ':' + i.genre;
+const keyExtractor = (i: string) => i;
 
 export default connector(GenresList);
