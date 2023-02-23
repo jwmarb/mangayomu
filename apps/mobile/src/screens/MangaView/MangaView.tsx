8096445599f328b4823c4c6e0229dae3540c6786
specify_ <dieptuber@gmail.com>
2023-02-23 09:45:49 -0700

    delete all unused imports

--- apps/mobile/src/screens/MangaView/MangaView.tsx
+++ apps/mobile/src/screens/MangaView/MangaView.tsx
@@ -9,28 +9,24 @@ import MangaViewerHeader from './components/MangaViewerHeader';
 import { useTheme } from '@emotion/react';
 import IconButton from '@components/IconButton';
 import Icon from '@components/Icon';
-import { useObject, useQuery, useRealm } from '@database/main';
+import { useQuery, useRealm } from '@database/main';
 import displayMessage from '@helpers/displayMessage';
 import { NativeScrollEvent, NativeSyntheticEvent } from 'react-native';
 import {
   interpolateColor,
   useAnimatedStyle,
-  useDerivedValue,
   useSharedValue,
 } from 'react-native-reanimated';
 import { NAVHEADER_HEIGHT } from '@components/NavHeader';
-import Text from '@components/Text';
 import { ChapterSchema } from '@database/schemas/Chapter';
 import RowChapter from '@screens/MangaView/components/RowChapter';
 import { ROW_CHAPTER_HEIGHT } from '@screens/MangaView/components/RowChapter/RowChapter';
 import Divider from '@components/Divider';
-import { CustomBottomSheet } from '@components/CustomBottomSheet';
 import BottomSheet from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheet/BottomSheet';
 import MangaViewModal from '@screens/MangaView/components/MangaViewModal';
-import { FlatList, RefreshControl } from 'react-native-gesture-handler';
-import languages, { ISOLangCode } from '@mangayomu/language-codes';
+import { RefreshControl } from 'react-native-gesture-handler';
+import { ISOLangCode } from '@mangayomu/language-codes';
 import { inPlaceSort } from 'fast-sort';
-import integrateSortedList from '@helpers/integrateSortedList';
 
 const DEFAULT_LANGUAGE: ISOLangCode = 'en';
 
