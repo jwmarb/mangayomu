42c88cc887c326c62c845af246600b4149eabaa3
specify_ <dieptuber@gmail.com>
2023-02-23 16:45:08 -0700

    create `SourceView` to edit host config

---
+++ apps/mobile/src/screens/SourceView/SourceView.redux.ts
@@ -0,0 +1,20 @@
+import { MangaHost } from '@mangayomu/mangascraper';
+import { RootStackProps } from '@navigators/Root/Root.interfaces';
+import { AppState } from '@redux/main';
+import { connect, ConnectedProps } from 'react-redux';
+import { toggleConfig } from '@redux/slices/host';
+
+const mapStateToProps = (
+  state: AppState,
+  props: RootStackProps<'SourceView'>,
+) => ({
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  source: MangaHost.getAvailableSources().get(props.route.params.source)!,
+  config: state.host.hostsConfig[props.route.params.source],
+});
+
+const connector = connect(mapStateToProps, { toggleConfig });
+
+export type ConnectedSourceViewProps = ConnectedProps<typeof connector>;
+
+export default connector;
---
+++ apps/mobile/src/screens/SourceView/SourceView.tsx
@@ -0,0 +1,148 @@
+import Box from '@components/Box';
+import Button from '@components/Button';
+import Icon from '@components/Icon';
+import { Stack } from '@components/Stack';
+import Switch from '@components/Switch';
+import Text from '@components/Text';
+import { useTheme } from '@emotion/react';
+import displayMessage from '@helpers/displayMessage';
+import useCollapsibleHeader from '@hooks/useCollapsibleHeader';
+import React from 'react';
+import { Linking } from 'react-native';
+import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import { moderateScale } from 'react-native-size-matters';
+import connector, { ConnectedSourceViewProps } from './SourceView.redux';
+
+const SourceView: React.FC<ConnectedSourceViewProps> = (props) => {
+  const { source, config, toggleConfig } = props;
+  const url = React.useRef<string>(`https://${source.getLink()}`).current;
+  const { onScroll, scrollViewStyle, contentContainerStyle } =
+    useCollapsibleHeader({ headerTitle: 'Source Info' });
+  async function handleOnViewWebsite() {
+    if (await Linking.canOpenURL(url)) Linking.openURL(url);
+  }
+  function handleOnLongPress() {
+    displayMessage(url);
+  }
+  const theme = useTheme();
+
+  function handleOnToggleHot() {
+    toggleConfig({ source: source.getName(), key: 'useHottestUpdates' });
+  }
+
+  function handleOnToggleLatest() {
+    toggleConfig({ source: source.getName(), key: 'useLatestUpdates' });
+  }
+
+  function handleOnToggleWithSearch() {
+    toggleConfig({ source: source.getName(), key: 'useWithUniversalSearch' });
+  }
+
+  return (
+    <ScrollView
+      onScroll={onScroll}
+      style={scrollViewStyle}
+      contentContainerStyle={contentContainerStyle}
+    >
+      <Stack space="m">
+        <Stack
+          mx="m"
+          my="s"
+          align-items="center"
+          space="s"
+          flex-direction="row"
+        >
+          <Icon
+            type="image"
+            name={source.getIcon()}
+            size={moderateScale(32)}
+            variant="inherit"
+          />
+          <Box flex-direction="column">
+            <Text>{source.getName()}</Text>
+            <Text color="textSecondary">Version: {source.getVersion()}</Text>
+            {source.isAdult() && (
+              <Text color="warning">This source may contain 18+ content</Text>
+            )}
+          </Box>
+        </Stack>
+        <Box mx="m" my="s">
+          <Button
+            label="Visit website"
+            icon={<Icon type="font" name="earth" />}
+            onPress={handleOnViewWebsite}
+            onLongPress={handleOnLongPress}
+          />
+        </Box>
+        <Stack space="s">
+          <Box mx="m" my="s">
+            <Text variant="header">Configuration</Text>
+          </Box>
+          <RectButton
+            rippleColor={theme.palette.action.ripple}
+            onPress={handleOnToggleWithSearch}
+          >
+            <Stack
+              mx="m"
+              my="s"
+              flex-direction="row"
+              space="s"
+              align-items="center"
+              justify-content="space-between"
+            >
+              <Text color="textSecondary">Use with universal search</Text>
+              <Switch
+                enabled={config.useWithUniversalSearch}
+                onChange={handleOnToggleWithSearch}
+              />
+            </Stack>
+          </RectButton>
+          {source.hasHotMangas() && (
+            <RectButton
+              rippleColor={theme.palette.action.ripple}
+              onPress={handleOnToggleHot}
+            >
+              <Stack
+                mx="m"
+                my="s"
+                flex-direction="row"
+                space="s"
+                align-items="center"
+                justify-content="space-between"
+              >
+                <Text color="textSecondary">Show trending mangas</Text>
+                <Switch
+                  enabled={config.useHottestUpdates}
+                  onChange={handleOnToggleHot}
+                />
+              </Stack>
+            </RectButton>
+          )}
+          {source.hasLatestMangas() && (
+            <RectButton
+              rippleColor={theme.palette.action.ripple}
+              onPress={handleOnToggleLatest}
+            >
+              <Stack
+                mx="m"
+                my="s"
+                flex-direction="row"
+                space="s"
+                align-items="center"
+                justify-content="space-between"
+              >
+                <Text color="textSecondary">Show recently updated mangas</Text>
+                <Switch
+                  enabled={config.useLatestUpdates}
+                  onChange={handleOnToggleLatest}
+                />
+              </Stack>
+            </RectButton>
+          )}
+        </Stack>
+      </Stack>
+    </ScrollView>
+  );
+};
+
+export default connector(SourceView);
