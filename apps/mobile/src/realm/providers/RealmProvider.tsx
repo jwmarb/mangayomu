704c12d700a9faedc7674a0fdfb77694213ec04a
specify_ <dieptuber@gmail.com>
2023-02-28 23:01:36 -0700

    add clean up

--- apps/mobile/src/realm/providers/RealmProvider.tsx
+++ apps/mobile/src/realm/providers/RealmProvider.tsx
@@ -8,13 +8,15 @@ import { useUser } from '@realm/react';
 import Realm from 'realm';
 import React from 'react';
 import { ChapterSchema } from '@database/schemas/Chapter';
+import useAuth0 from '@hooks/useAuth0';
 
 export const RealmEffect: React.FC<React.PropsWithChildren> = ({
   children,
 }) => {
   // const mangas = useQuery(MangaSchema);
   const realm = useRealm();
-  const user = useUser();
+  const currentUser = useUser();
+  const { user } = useAuth0();
 
   // const callback: Realm.CollectionChangeCallback<
   //   MangaSchema & Realm.Object<unknown, never>
@@ -27,22 +29,24 @@ export const RealmEffect: React.FC<React.PropsWithChildren> = ({
 
   React.useEffect(() => {
     (async () => {
-      try {
-        await realm.subscriptions.waitForSynchronization();
-      } catch (e) {
-        console.error(e);
-      } finally {
+      await realm.subscriptions.waitForSynchronization();
+      await realm.subscriptions.update((sub) => {
+        sub.add(
+          realm
+            .objects(MangaSchema)
+            .filtered(`_realmId == "${currentUser?.id}"`),
+        );
+      });
+      return async () => {
         await realm.subscriptions.update((sub) => {
-          sub.add(
-            realm.objects(MangaSchema).filtered(`_realmId == "${user?.id}"`),
-          );
+          sub.removeAll();
         });
-      }
+      };
     })();
     // mangas.addListener(callback);
     // return () => {
     //   mangas.removeListener(callback);
     // };
-  }, []);
+  }, [user]);
   return <>{children}</>;
 };
