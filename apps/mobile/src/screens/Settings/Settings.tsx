27c67d7ba05b2e6a9c198b75f9651f6484f93cf1
Joseph Marbella <dieptuber@gmail.com>
2023-03-09 10:06:32 -0700

    Add source selector to settings

--- apps/mobile/src/screens/Settings/Settings.tsx
+++ apps/mobile/src/screens/Settings/Settings.tsx
@@ -1,41 +1,51 @@
 import Divider from '@components/Divider';
 import Icon from '@components/Icon';
 import Text from '@components/Text';
+import { BottomSheetMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
 import useCollapsibleHeader from '@hooks/useCollapsibleHeader';
 import useRootNavigation from '@hooks/useRootNavigation';
 import PressableListItem from '@screens/More/components/PressableListItem';
+import MainSourceSelector from '@screens/Welcome/components/MainSourceSelector';
 import React from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
 
 const Settings: React.FC = (props) => {
   const { onScroll, contentContainerStyle, scrollViewStyle } =
     useCollapsibleHeader({ headerTitle: 'Settings' });
+  const ref = React.useRef<BottomSheetMethods>(null);
   const navigation = useRootNavigation();
   const handleOnAppearance = React.useCallback(() => {
     navigation.navigate('Appearance');
   }, [navigation]);
+  const handleOnSource = React.useCallback(() => {
+    ref.current?.snapToIndex(1);
+  }, []);
   return (
-    <ScrollView
-      onScroll={onScroll}
-      contentContainerStyle={contentContainerStyle}
-      style={scrollViewStyle}
-    >
-      <PressableListItem
-        label="Reader"
-        iconLeft={<Icon type="font" name="book" />}
-      />
-      <Divider />
-      <PressableListItem
-        label="Sources"
-        iconLeft={<Icon type="font" name="bookshelf" />}
-      />
-      <Divider />
-      <PressableListItem
-        onPress={handleOnAppearance}
-        label="Appearance"
-        iconLeft={<Icon type="font" name="palette" />}
-      />
-    </ScrollView>
+    <>
+      <MainSourceSelector ref={ref} />
+      <ScrollView
+        onScroll={onScroll}
+        contentContainerStyle={contentContainerStyle}
+        style={scrollViewStyle}
+      >
+        <PressableListItem
+          label="Reader"
+          iconLeft={<Icon type="font" name="book" />}
+        />
+        <Divider />
+        <PressableListItem
+          label="Sources"
+          onPress={handleOnSource}
+          iconLeft={<Icon type="font" name="bookshelf" />}
+        />
+        <Divider />
+        <PressableListItem
+          onPress={handleOnAppearance}
+          label="Appearance"
+          iconLeft={<Icon type="font" name="palette" />}
+        />
+      </ScrollView>
+    </>
   );
 };
 
