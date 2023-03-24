3d84c58e980f61ac774772bcae5c003c367f6e9b
specify_ <dieptuber@gmail.com>
2023-03-24 13:53:44 -0700

    create hook for booleans

---
+++ apps/mobile/src/hooks/useBoolean.ts
@@ -0,0 +1,19 @@
+import React from 'react';
+/**
+ * Drop-in replacement for boolean values for React.useState. If the setter function is not given a value, it will toggle the boolean instead.
+ * @returns Returns a useState value along with a toggle function to toggle the value.
+ */
+export default function useBoolean(): [
+  boolean,
+  (value?: React.SetStateAction<boolean>) => void,
+] {
+  const [boolean, setBoolean] = React.useState<boolean>(false);
+  const toggle = React.useCallback(
+    (value?: React.SetStateAction<boolean>) => {
+      if (value == null) setBoolean((prev) => !prev);
+      else setBoolean(value);
+    },
+    [setBoolean],
+  );
+  return [boolean, toggle];
+}
