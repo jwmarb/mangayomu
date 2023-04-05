b9d9c644abbddcf1315319fd660eecc616b68a30
specify_ <dieptuber@gmail.com>
2023-04-04 22:48:40 -0700

    use background color provided by user

--- apps/mobile/src/screens/Reader/components/NoMorePages/NoMorePages.tsx
+++ apps/mobile/src/screens/Reader/components/NoMorePages/NoMorePages.tsx
@@ -1,12 +1,20 @@
 import Box from '@components/Box';
 import Text from '@components/Text';
+import { useTheme } from '@emotion/react';
+import useReaderBackgroundColor from '@hooks/useReaderBackgroundColor';
+import { ReaderBackgroundColor } from '@redux/slices/settings';
 import React from 'react';
 import { useWindowDimensions } from 'react-native';
+import connector, { ConnectedNoMorePagesProps } from './NoMorePages.redux';
 
-const NoMorePages: React.FC = () => {
+const NoMorePages: React.FC<ConnectedNoMorePagesProps> = ({
+  backgroundColor,
+}) => {
   const { width, height } = useWindowDimensions();
+  const { background, textPrimary } = useReaderBackgroundColor(backgroundColor);
   return (
     <Box
+      background-color={background}
       width={width}
       height={height}
       justify-content="center"
@@ -14,9 +22,11 @@ const NoMorePages: React.FC = () => {
       px="m"
       py="s"
     >
-      <Text>You have reached the end of the final chapter.</Text>
+      <Text color={textPrimary}>
+        You have reached the end of the final chapter.
+      </Text>
     </Box>
   );
 };
 
-export default React.memo(NoMorePages);
+export default connector(React.memo(NoMorePages));
