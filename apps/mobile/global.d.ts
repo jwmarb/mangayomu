6f912501cf8907ad214a5216d0b5f28e65cf2e25
Joseph Marbella <dieptuber@gmail.com>
2023-04-18 14:51:57 -0700

    add types for immersive package

--- apps/mobile/global.d.ts
+++ apps/mobile/global.d.ts
@@ -1,3 +1,10 @@
 declare module '@env' {
   export const REACT_APP_REALM_ID: string;
 }
+
+declare module 'react-native-immersive' {
+  export const Immersive: {
+    on: () => void;
+    off: () => void;
+  };
+}
