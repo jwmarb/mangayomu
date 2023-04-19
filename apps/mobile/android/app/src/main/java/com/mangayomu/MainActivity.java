760df1df24101311b1f85bd07868cb4d00e8892d
specify_ <dieptuber@gmail.com>
2023-04-18 22:57:51 -0700

    set initial navigation bar color

--- apps/mobile/android/app/src/main/java/com/mangayomu/MainActivity.java
+++ apps/mobile/android/app/src/main/java/com/mangayomu/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends ReactActivity {
     Window w = getWindow();
     w.setStatusBarColor(Color.TRANSPARENT);
     w.setNavigationBarColor(Color.TRANSPARENT);
+    w.setNavigationBarColor(Color.TRANSPARENT);
+
     w.getDecorView()
         .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
     super.onCreate(null);
