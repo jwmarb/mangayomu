b5c355d3ade3c193bd3b60503fb6e100c67f2c10
specify_ <dieptuber@gmail.com>
2023-04-18 20:05:01 -0700

    add initial state to `useBoolean`

--- apps/mobile/src/hooks/useBoolean.ts
+++ apps/mobile/src/hooks/useBoolean.ts
@@ -3,11 +3,10 @@ import React from 'react';
  * Drop-in replacement for boolean values for React.useState. If the setter function is not given a value, it will toggle the boolean instead.
  * @returns Returns a useState value along with a toggle function to toggle the value.
  */
-export default function useBoolean(): [
-  boolean,
-  (value?: React.SetStateAction<boolean>) => void,
-] {
-  const [boolean, setBoolean] = React.useState<boolean>(false);
+export default function useBoolean(
+  initialState = false,
+): [boolean, (value?: React.SetStateAction<boolean>) => void] {
+  const [boolean, setBoolean] = React.useState<boolean>(initialState);
   const toggle = React.useCallback(
     (value?: React.SetStateAction<boolean>) => {
       if (value == null) setBoolean((prev) => !prev);
