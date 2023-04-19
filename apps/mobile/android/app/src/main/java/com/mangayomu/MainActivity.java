a39d01a912537bf80e2fbe90e39e9eb069d5b1dd
specify_ <dieptuber@gmail.com>
2023-04-18 20:08:26 -0700

    create native immersive mode module

--- apps/mobile/android/app/src/main/java/com/mangayomu/MainActivity.java
+++ apps/mobile/android/app/src/main/java/com/mangayomu/MainActivity.java
@@ -1,5 +1,13 @@
 package com.mangayomu;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -22,6 +30,17 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+      WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+      layoutParams.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+      getWindow().setAttributes(layoutParams);
+    }
+
+    Window w = getWindow();
+    w.setStatusBarColor(Color.TRANSPARENT);
+    w.setNavigationBarColor(Color.TRANSPARENT);
+    w.getDecorView()
+        .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
     super.onCreate(null);
   }
 
