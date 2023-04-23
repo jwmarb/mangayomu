f3aa9b22a42ce44672b1e60cbc50ed1d7bda4613
specify_ <dieptuber@gmail.com>
2023-04-23 12:54:30 -0700

    remove all unused imports

--- apps/mobile/src/screens/More/More.tsx
+++ apps/mobile/src/screens/More/More.tsx
@@ -1,30 +1,13 @@
-import Avatar from '@components/Avatar';
-import Badge from '@components/Badge';
 import Box from '@components/Box';
-import Button from '@components/Button';
 import Divider from '@components/Divider';
 import Icon from '@components/Icon';
-import IconButton from '@components/IconButton';
-import { Menu, MenuItem } from '@components/Menu';
-import Stack from '@components/Stack';
-import Switch from '@components/Switch';
-import Text from '@components/Text';
-import { useRealm } from '@database/main';
 import { useTheme } from '@emotion/react';
-import copyTextToClipboard from '@helpers/copyTextToClipboard';
 import useAuth0 from '@hooks/useAuth0';
 import useRootNavigation from '@hooks/useRootNavigation';
-import { RootStackProps } from '@navigators/Root/Root.interfaces';
-import { useUser } from '@realm/react';
 import CloudSwitch from '@screens/More/components/CloudSwitch';
 import PressableListItem from '@screens/More/components/PressableListItem';
 import React from 'react';
-import {
-  BorderlessButton,
-  RectButton,
-  ScrollView,
-} from 'react-native-gesture-handler';
-import { moderateScale } from 'react-native-size-matters';
+import { ScrollView } from 'react-native-gesture-handler';
 import User from './components/User';
 
 const Settings: React.FC = () => {
