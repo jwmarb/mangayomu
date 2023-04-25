e2efb39633277827fc325cd6ba6dca81a8127d0e
specify_ <dieptuber@gmail.com>
2023-04-24 18:26:44 -0700

    remove unused storybook

--- apps/mobile/storybook/themeDecorator.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ThemeProvider } from '@emotion/react';
-import { __storybook_theme__ } from '../src/theme';
-const ThemeDecorator = (story) => (
-  <ThemeProvider theme={__storybook_theme__}>{story()}</ThemeProvider>
-);
-
-export default ThemeDecorator;
