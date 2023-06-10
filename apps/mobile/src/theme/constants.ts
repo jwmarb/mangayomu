e1b9e1735eacb3f63d542708f784a2fdac343f94
Joseph Marbella <dieptuber@gmail.com>
2023-06-09 21:12:47 -0700

    refactor: add `constants` for ease of access

---
+++ apps/mobile/src/theme/constants.ts
@@ -0,0 +1,9 @@
+import { moderateScale } from 'react-native-size-matters';
+
+export const BOOK_DIMENSIONS = {
+  width: moderateScale(110),
+  height: moderateScale(205),
+};
+
+export const BOOK_DIMENSIONS_RATIO =
+  BOOK_DIMENSIONS.width / BOOK_DIMENSIONS.height;
