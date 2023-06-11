d5c6f09244472039092d7c3949b3ad170928eebf
Joseph Marbella <dieptuber@gmail.com>
2023-06-11 01:43:22 -0700

    feat: add layout measurement for accordion

--- apps/mobile/src/theme/constants.ts
+++ apps/mobile/src/theme/constants.ts
@@ -9,3 +9,6 @@ export const BOOK_DIMENSIONS_RATIO =
   BOOK_DIMENSIONS.width / BOOK_DIMENSIONS.height;
 
 export const UNFINISHED_MANGA_HEIGHT = moderateScale(128);
+
+export const ACCORDION_SECTION_HEADER_HEIGHT = moderateScale(48);
+export const ACCORDION_ITEM_HEIGHT = moderateScale(52);
