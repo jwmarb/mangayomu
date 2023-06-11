debe7cf52b6266003766fd1c1fdceefa36629d2f
Joseph Marbella <dieptuber@gmail.com>
2023-06-10 22:23:10 -0700

    feat: add component for collapsible header

--- apps/mobile/src/components/animated.ts
+++ apps/mobile/src/components/animated.ts
@@ -1,7 +1,15 @@
 import { FlashList } from '@shopify/flash-list';
+import { SectionList } from 'react-native';
 import Animated from 'react-native-reanimated';
+
+type FlashListComponent = typeof FlashList;
+type SectionListComponent = typeof SectionList;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const AnimatedFlashList = Animated.createAnimatedComponent(
   FlashList,
-) as unknown as FlashList<any>;
+) as unknown as FlashListComponent;
+
+export const AnimatedSectionList = Animated.createAnimatedComponent(
+  SectionList,
+) as SectionListComponent;
