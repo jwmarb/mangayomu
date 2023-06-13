44c748c3df8f57eaf0dd3de3c6b3a9b60505a48b
Joseph Marbella <dieptuber@gmail.com>
2023-06-13 15:15:23 -0700

    feat: add props for extra functionality

--- apps/mobile/src/screens/Reader/components/Overlay/Overlay.interfaces.d.ts
+++ apps/mobile/src/screens/Reader/components/Overlay/Overlay.interfaces.d.ts
@@ -5,12 +5,20 @@ import { ChapterSchema } from '@database/schemas/Chapter';
 import { MangaSchema } from '@database/schemas/Manga';
 import { ImageMenuMethods } from '@screens/Reader/components/ImageMenu/ImageMenu.interfaces';
 import useChapterFetcher from '@screens/Reader/hooks/useChapterFetcher';
+import useReaderProps from '@screens/Reader/hooks/useReaderProps';
+import { FlashList } from '@shopify/flash-list';
+import { Page } from '@redux/slices/reader';
+import { PageSliderNavigatorMethods } from '@screens/Reader/components/Overlay/components/PageSliderNavigator/PageSliderNavigator.interfaces';
 
 export interface OverlayProps extends React.PropsWithChildren {
   opacity: SharedValue<number>;
   currentPage: number;
   chapter: ChapterSchema & Realm.Object<ChapterSchema, never>;
   manga: MangaSchema & Realm.Object<MangaSchema, never>;
+  readerProps: ReturnType<typeof useReaderProps>;
+  scrollRef: React.RefObject<FlashList<Page>>;
+  pageSliderNavRef: React.RefObject<PageSliderNavigatorMethods>;
+  isFinishedInitialScrollOffset: boolean;
 }
 
 export interface ReaderSettingProps {
