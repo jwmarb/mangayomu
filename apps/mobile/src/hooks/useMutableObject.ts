2d594811a928cf8ac3d20d153c5cd21310ee44a9
Joseph Marbella <dieptuber@gmail.com>
2023-06-13 13:41:25 -0700

    feat: Add hook to assign ref value

---
+++ apps/mobile/src/hooks/useMutableObject.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * Create a ref copy of a state to be consumed in a function that does not update to state changes of a component
+ * @param initialState The state to track changes
+ * @returns Returns the state which can be consumed in any function that does not update
+ */
+export default function useMutableObject<T>(initialState: T) {
+  const ref = React.useRef<T>(initialState);
+  ref.current = initialState;
+  return ref.current;
+}
