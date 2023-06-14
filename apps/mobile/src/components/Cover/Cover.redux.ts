115cba8e9e0526483b08c04aaa6c442e0dfd8020
Joseph Marbella <dieptuber@gmail.com>
2023-06-14 01:21:33 -0700

    refactor: use constant

--- apps/mobile/src/components/Cover/Cover.redux.ts
+++ apps/mobile/src/components/Cover/Cover.redux.ts
@@ -1,15 +1,14 @@
 import { AppState } from '@redux/main';
 import { connect, ConnectedProps } from 'react-redux';
 import { CoverProps } from '@components/Cover/Cover.interfaces';
-import { BOOK_DIMENSIONS } from '@theme/constants';
+import { BOOK_COVER_HEIGHT, BOOK_DIMENSIONS } from '@theme/constants';
 import { moderateScale } from 'react-native-size-matters';
-const normalCoverDimension = moderateScale(160);
 
 const mapStateToProps = (state: AppState, props: CoverProps) => ({
   cover: props.cover,
   scale: props.scale,
   coverHeight: props.normalBookDimensions
-    ? normalCoverDimension
+    ? BOOK_COVER_HEIGHT
     : state.settings.book.coverHeight,
   width: props.normalBookDimensions
     ? BOOK_DIMENSIONS.width
