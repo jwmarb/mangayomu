02c4227a158988f4a5117da91904f5a74ecb8b9f
Joseph Marbella <dieptuber@gmail.com>
2023-06-14 01:28:55 -0700

    refactor: improve UX

--- apps/mobile/src/screens/Appearance/components/BoldFont/BoldFont.tsx
+++ apps/mobile/src/screens/Appearance/components/BoldFont/BoldFont.tsx
@@ -2,18 +2,36 @@ import Checkbox from '@components/Checkbox';
 import Stack from '@components/Stack';
 import Text from '@components/Text';
 import React from 'react';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import {
+  RectButton,
+  TouchableWithoutFeedback,
+} from 'react-native-gesture-handler';
 import { BoldFontProps } from './BoldFont.interfaces';
+import Switch from '@components/Switch/Switch';
+import { useTheme } from '@emotion/react';
 
 const BoldFont: React.FC<BoldFontProps> = (props) => {
   const { isBold, onToggleBold } = props;
+  const theme = useTheme();
   return (
-    <Stack space="s" flex-direction="row" align-items="center">
-      <TouchableWithoutFeedback onPress={onToggleBold}>
-        <Text>Bold font</Text>
-      </TouchableWithoutFeedback>
-      <Checkbox onChange={onToggleBold} checked={isBold} />
-    </Stack>
+    <RectButton
+      rippleColor={theme.palette.action.ripple}
+      onPress={onToggleBold}
+    >
+      <Stack
+        px="l"
+        py="s"
+        space="s"
+        flex-direction="row"
+        align-items="center"
+        justify-content="space-between"
+      >
+        <TouchableWithoutFeedback onPress={onToggleBold}>
+          <Text>Bold font</Text>
+        </TouchableWithoutFeedback>
+        <Switch onChange={onToggleBold} enabled={isBold} />
+      </Stack>
+    </RectButton>
   );
 };
 
