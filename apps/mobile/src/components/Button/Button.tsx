ab4f625d542c5272fc36b95fdbfb35d49ad479d0
Joseph Marbella <dieptuber@gmail.com>
2023-06-14 02:43:31 -0700

    fix: custom styles not showing

--- apps/mobile/src/components/Button/Button.tsx
+++ apps/mobile/src/components/Button/Button.tsx
@@ -5,6 +5,7 @@ import { moderateScale } from 'react-native-size-matters';
 import Text from '@components/Text';
 import { useTheme } from '@emotion/react';
 import Stack from '@components/Stack';
+import Box from '@components/Box/Box';
 
 const Button: React.FC<ButtonProps> = (props) => {
   const {
@@ -14,64 +15,55 @@ const Button: React.FC<ButtonProps> = (props) => {
     disabled = false,
     icon,
     iconPlacement = 'left',
+    style,
     ...rest
   } = props;
   const theme = useTheme();
   return (
-    <BaseButton
-      shouldCancelWhenOutside
-      disallowInterruption
-      enabled={!disabled}
-      style={{
-        borderRadius: theme.style.borderRadius,
-        backgroundColor:
-          variant === 'contained'
-            ? disabled
-              ? theme.palette.text.disabled
-              : theme.palette[color].main
-            : undefined,
-      }}
-      {...rest}
-      rippleColor={theme.palette[color].ripple}
-    >
-      <Stack
-        space={moderateScale(4)}
-        py={moderateScale(12)}
-        px={moderateScale(16)}
-        flex-direction="row"
-        align-items="center"
-        justify-content="center"
-        border-width="@theme"
-        border-radius="@theme"
-        style={{
-          borderColor:
-            variant === 'outline'
+    <Box
+      overflow="hidden"
+      style={[
+        style,
+        {
+          borderRadius: theme.style.borderRadius,
+          backgroundColor:
+            variant === 'contained'
               ? disabled
                 ? theme.palette.text.disabled
-                : theme.helpers.getColor(color)
-              : 'transparent',
-        }}
+                : theme.palette[color].main
+              : undefined,
+        },
+      ]}
+    >
+      <BaseButton
+        shouldCancelWhenOutside
+        disallowInterruption
+        enabled={!disabled}
+        {...rest}
+        rippleColor={theme.palette[color].ripple}
       >
-        {iconPlacement === 'left' &&
-          icon &&
-          React.cloneElement(icon, {
-            color: disabled
-              ? 'disabled'
-              : variant === 'contained'
-              ? {
-                  custom: theme.helpers.getContrastText(
-                    theme.palette[color].main,
-                  ),
-                }
-              : color,
-          })}
-        {label && (
-          <Text
-            variant="button"
-            align="center"
-            bold
-            color={
-              disabled
+        <Stack
+          space={moderateScale(4)}
+          py={moderateScale(12)}
+          px={moderateScale(16)}
+          flex-direction="row"
+          align-items="center"
+          justify-content="center"
+          border-width="@theme"
+          border-radius="@theme"
+          style={{
+            borderColor:
+              variant === 'outline'
+                ? disabled
+                  ? theme.palette.text.disabled
+                  : theme.helpers.getColor(color)
+                : 'transparent',
+          }}
+        >
+          {iconPlacement === 'left' &&
+            icon &&
+            React.cloneElement(icon, {
+              color: disabled
                 ? 'disabled'
                 : variant === 'contained'
                 ? {
@@ -79,27 +71,44 @@ const Button: React.FC<ButtonProps> = (props) => {
                       theme.palette[color].main,
                     ),
                   }
-                : color
-            }
-          >
-            {label}
-          </Text>
-        )}
-        {iconPlacement === 'right' &&
-          icon &&
-          React.cloneElement(icon, {
-            color: disabled
-              ? 'disabled'
-              : variant === 'contained'
-              ? {
-                  custom: theme.helpers.getContrastText(
-                    theme.palette[color].main,
-                  ),
-                }
-              : color,
-          })}
-      </Stack>
-    </BaseButton>
+                : color,
+            })}
+          {label && (
+            <Text
+              variant="button"
+              align="center"
+              bold
+              color={
+                disabled
+                  ? 'disabled'
+                  : variant === 'contained'
+                  ? {
+                      custom: theme.helpers.getContrastText(
+                        theme.palette[color].main,
+                      ),
+                    }
+                  : color
+              }
+            >
+              {label}
+            </Text>
+          )}
+          {iconPlacement === 'right' &&
+            icon &&
+            React.cloneElement(icon, {
+              color: disabled
+                ? 'disabled'
+                : variant === 'contained'
+                ? {
+                    custom: theme.helpers.getContrastText(
+                      theme.palette[color].main,
+                    ),
+                  }
+                : color,
+            })}
+        </Stack>
+      </BaseButton>
+    </Box>
   );
 };
 
