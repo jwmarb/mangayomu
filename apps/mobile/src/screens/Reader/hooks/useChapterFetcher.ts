95269805ac679ff2fe2e7e74983dab940d9b1544
Joseph Marbella <dieptuber@gmail.com>
2023-06-15 01:15:55 -0700

    refactor: change how fetches should occur

--- apps/mobile/src/screens/Reader/hooks/useChapterFetcher.ts
+++ apps/mobile/src/screens/Reader/hooks/useChapterFetcher.ts
@@ -1,16 +1,18 @@
 import { useAppDispatch } from '@redux/main';
 import {
   FetchPagesByChapterPayload,
+  Page,
   fetchPagesByChapter,
   fetchedChapters,
   fetchingChapters,
   resetReaderState,
 } from '@redux/slices/reader/reader';
-import NetInfo, { useNetInfo } from '@react-native-community/netinfo';
+import NetInfo, { NetInfoSubscription } from '@react-native-community/netinfo';
 import React from 'react';
 import { ChapterSchema } from '@database/schemas/Chapter';
 import { MangaHost } from '@mangayomu/mangascraper';
 import { useLocalRealm } from '@database/main';
+import { InteractionManager } from 'react-native';
 
 /**
  * A hook to create a fetcher to fetch a chapter
@@ -21,25 +23,21 @@ export default function useChapterFetcher(
   args: Pick<
     FetchPagesByChapterPayload,
     'availableChapters' | 'manga' | 'chapter'
-  >,
+  > & { pages: Page[] },
 ) {
   const dispatch = useAppDispatch();
   const source = MangaHost.getAvailableSources().get(args.manga.source);
   const localRealm = useLocalRealm();
   if (source == null) throw Error(`${args.manga.source} does not exist`);
   const fetchPages = React.useCallback(
-    (
-      chapter: ChapterSchema,
-      callback?: (() => void) | null,
-      mockError = false,
-    ) => {
+    (chapter: ChapterSchema, mockError = false) => {
       if (
         fetchedChapters.has(chapter._id) ||
         fetchingChapters.has(chapter._id)
       ) {
         return;
       }
-      const awaitingFetch = dispatch(
+      return dispatch(
         fetchPagesByChapter({
           source,
           localRealm,
@@ -49,67 +47,48 @@ export default function useChapterFetcher(
           mockError,
         }),
       );
-      const netListener = NetInfo.addEventListener(
-        ({ isInternetReachable }) => {
-          if (!isInternetReachable) {
-            awaitingFetch.abort();
-            netListener();
-          }
-        },
-      );
-      if (callback) awaitingFetch.finally(callback);
-      return {
-        abort: () => {
-          netListener();
-          awaitingFetch.abort();
-        },
-      };
     },
     [source, localRealm, args.manga, args.availableChapters],
   );
 
-  /**
-   * Initially fetches the chapter and cancels fetch request if there are any changes to internet availability
-   */
   React.useEffect(() => {
-    const p = fetchPages(args.chapter);
-
     return () => {
-      p?.abort();
       dispatch(resetReaderState());
     };
   }, []);
 
   /**
-   * This reacts to changes in internet availability
+   * Initially fetches current chapter
    */
   React.useEffect(() => {
+    const { then, cancel } = InteractionManager.runAfterInteractions();
     let p: ReturnType<typeof fetchPages>;
-    const listener = NetInfo.addEventListener((e) => {
-      if (e.isInternetReachable) {
-        const nextChapter = args.availableChapters[args.chapter.index - 1];
-        if (nextChapter != null) {
-          p = fetchPages(nextChapter);
-        }
-      }
+    let listener: NetInfoSubscription;
+    then(() => {
+      listener = NetInfo.addEventListener(() => {
+        p = fetchPages(args.chapter);
+      });
     });
     return () => {
-      p?.abort();
+      cancel();
       listener();
+      p?.abort();
     };
   }, []);
 
-  /**
-   * Automatically fetch next chapter
-   */
   React.useEffect(() => {
-    const nextChapter = args.availableChapters[args.chapter.index - 1];
-    if (nextChapter != null) {
-      const p = fetchPages(nextChapter);
-      return () => {
-        p?.abort();
-      };
-    }
+    let p: ReturnType<typeof fetchPages>;
+    const listener = NetInfo.addEventListener(({ isInternetReachable }) => {
+      if (isInternetReachable) {
+        const nextChapter = args.availableChapters[args.chapter.index - 1];
+        if (nextChapter != null) p = fetchPages(nextChapter);
+      }
+    });
+    return () => {
+      listener();
+      p?.abort();
+    };
   }, [args.chapter._id]);
+
   return fetchPages;
 }
