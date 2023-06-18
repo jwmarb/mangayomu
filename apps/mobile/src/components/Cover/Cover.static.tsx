f812bd0f1374875af39c1aa9146844c91e9f58f9
Joseph Marbella <dieptuber@gmail.com>
2023-06-18 01:48:58 -0700

    feat: add props for customization

--- apps/mobile/src/components/Cover/Cover.static.tsx
+++ apps/mobile/src/components/Cover/Cover.static.tsx
@@ -10,7 +10,7 @@ import { Image, StyleSheet } from 'react-native';
 import FastImage from 'react-native-fast-image';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
-import { ScaledSheet } from 'react-native-size-matters';
+import { ScaledSheet, moderateScale } from 'react-native-size-matters';
 import { MangaSchema } from '@database/schemas/Manga';
 import mangaSchemaToManga from '@helpers/mangaSchemaToManga';
 
@@ -34,10 +34,27 @@ const mangaHistoryItemStyles = StyleSheet.create({
 
 interface StaticCoverProps {
   manga: Manga | MangaSchema;
+  scale?: number;
+  sharp?: boolean;
 }
 
 const StaticCover: React.FC<StaticCoverProps> = (props) => {
-  const { manga } = props;
+  const { manga, scale = 1, sharp = false } = props;
+  const styles = React.useMemo(
+    () => ({
+      cover: {
+        width: moderateScale(scale * 64),
+        height: moderateScale(
+          Math.round(
+            ((scale * 64) / (BOOK_DIMENSIONS.width / BOOK_DIMENSIONS.height)) *
+              AUTO_HEIGHT_SCALAR,
+          ),
+        ),
+        borderRadius: sharp ? undefined : moderateScale(8),
+      },
+    }),
+    [scale, sharp],
+  );
   const loadingOpacity = useSharedValue(0);
   const navigation = useRootNavigation();
   const theme = useTheme();
@@ -64,7 +81,7 @@ const StaticCover: React.FC<StaticCoverProps> = (props) => {
   );
   const fastImageStyle = React.useMemo(
     () => [styles.cover, { backgroundColor: theme.palette.skeleton }],
-    [theme.palette.skeleton],
+    [theme.palette.skeleton, styles.cover],
   );
   return (
     <TouchableWithoutFeedback disallowInterruption onPress={handleOnPressCover}>
