13100a6e8c995e1f2390ae4216760ab63b482f40
Joseph Marbella <dieptuber@gmail.com>
2023-06-30 14:17:09 -0700

    feat: add hook for custom component styling

---
+++ apps/nextjs/app/hooks/useClassName.ts
@@ -0,0 +1,24 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React from 'react';
+
+export type OverrideClassName = {
+  /**
+   * Overrides the default className that styles the component
+   */
+  overrideClassName?: string;
+  className?: string;
+};
+
+export default function useClassName(
+  className: string | (() => string),
+  props: OverrideClassName,
+) {
+  return [
+    props.overrideClassName ?? typeof className === 'string'
+      ? className
+      : className(),
+    props.className,
+  ]
+    .filter(Boolean)
+    .join(' ');
+}
