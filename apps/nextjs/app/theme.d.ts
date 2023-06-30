c18c0cb1f7e4f80ba7cc4532886188f4a3fa53bf
Joseph Marbella <dieptuber@gmail.com>
2023-06-30 13:00:49 -0700

    feat: add type defs for theme

---
+++ apps/nextjs/app/theme.d.ts
@@ -0,0 +1,7 @@
+export type BackgroundColor = 'default' | 'paper' | 'disabled';
+export type ButtonColor = 'primary' | 'secondary';
+export type TextColor = 'text-primary' | 'text-secondary' | 'error';
+export type ButtonContrastColors = `${ButtonColor}-contrast`;
+export type ButtonVariant = 'contained' | 'outline' | 'text';
+export type AppColor = TextColor | ButtonColor | BackgroundColor;
+export type TextVariant = 'header' | 'body' | 'button' | 'sm-label' | 'label';
