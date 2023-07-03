b3ab1f6c753176d5a17cfb52753b951d0be0a143
Joseph Marbella <dieptuber@gmail.com>
2023-07-03 16:11:00 -0700

    feat: add middlewares

---
+++ apps/nextjs/app/api/middleware/mongodb.ts
@@ -0,0 +1,46 @@
+import {
+  HandlerRequest,
+  Middleware,
+  ResponseError,
+} from '@mangayomu/request-handler';
+import env from '@mangayomu/vercel-env';
+import { StatusCodes } from 'http-status-codes';
+import mongoose from 'mongoose';
+
+declare global {
+  // eslint-disable-next-line no-var
+  var cached_mongoose: typeof mongoose;
+}
+
+let cached: typeof mongoose | null = global.cached_mongoose || null;
+
+const connectMongoDB = (condition?: (request: HandlerRequest) => boolean) => {
+  const m: Middleware = async (request, response, next) => {
+    if ((condition && condition(request)) || condition == null) {
+      if (cached != null) {
+        console.log('Already found existing MongoDB connection');
+        next();
+      } else
+        try {
+          cached = await mongoose.connect(env().MONGODB_URI, {
+            bufferCommands: false,
+            dbName: env().MONGODB_DATABASE_NAME,
+          });
+          console.log('Established MongoDB connection');
+        } catch (e) {
+          response
+            .status(StatusCodes.INTERNAL_SERVER_ERROR)
+            .json(
+              ResponseError.from(StatusCodes.INTERNAL_SERVER_ERROR, `${e}`),
+            );
+          return;
+        }
+    }
+
+    next();
+  };
+
+  return m;
+};
+
+export default connectMongoDB;
---
+++ apps/nextjs/app/api/middleware/state.ts
@@ -0,0 +1,37 @@
+import redis from '@server/redis';
+import { Middleware, ResponseError } from '@mangayomu/request-handler';
+import { StatusCodes } from 'http-status-codes';
+
+interface StateQuery {
+  state?: string;
+}
+
+const stateMiddleware: Middleware = async (request, response, next) => {
+  const query = request.query<StateQuery>();
+  if (query.state == null) {
+    response
+      .status(StatusCodes.UNAUTHORIZED)
+      .json(
+        ResponseError.from(
+          StatusCodes.UNAUTHORIZED,
+          'This is a protected route',
+        ),
+      );
+    return;
+  } else {
+    const value = await redis.getdel(query.state);
+    if (value == null) {
+      response
+        .status(StatusCodes.UNAUTHORIZED)
+        .json(
+          ResponseError.from(
+            StatusCodes.UNAUTHORIZED,
+            'This is a protected route',
+          ),
+        );
+      return;
+    } else next();
+  }
+};
+
+export default stateMiddleware;
