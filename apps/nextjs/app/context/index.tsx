28e415c7f378df7d981a165f3ac50363914d3e94
Joseph Marbella <dieptuber@gmail.com>
2023-07-03 16:12:47 -0700

    feat: add auth prop

--- apps/nextjs/app/context/index.tsx
+++ apps/nextjs/app/context/index.tsx
@@ -5,9 +5,23 @@ import env from '@mangayomu/vercel-env';
  * Wraps all context to be consumed for client components
  * @returns
  */
-export function Providers({ children }: React.PropsWithChildren) {
+
+interface ProvidersProps extends React.PropsWithChildren {
+  idToken?: string;
+  idTokenInvalid?: boolean;
+}
+
+export function Providers({
+  children,
+  idToken,
+  idTokenInvalid,
+}: ProvidersProps) {
   return (
-    <ClientRealmProvider appId={env().REACT_APP_REALM_ID}>
+    <ClientRealmProvider
+      appId={env().REACT_APP_REALM_ID}
+      idToken={idToken}
+      idTokenInvalid={idTokenInvalid}
+    >
       {children}
     </ClientRealmProvider>
   );
