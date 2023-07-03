294b662dec3c0e9456ad101dd0007c629bc12f28
Joseph Marbella <dieptuber@gmail.com>
2023-07-03 16:15:17 -0700

    todo: create login from GSI client

--- apps/nextjs/app/(login)/login/hooks/useLoadGSIScript.tsx
+++ apps/nextjs/app/(login)/login/hooks/useLoadGSIScript.tsx
@@ -1,3 +1,5 @@
+import * as Realm from 'realm-web';
+import { useRealm, useUserSetter } from '@app/context/realm';
 import useBoolean from '@app/hooks/useBoolean';
 import React from 'react';
 
@@ -10,12 +12,39 @@ const button: React.FC = () => <div id="__google_sign_in_button__" />;
 export function useLoadGSIScript(clientId: string) {
   const [loaded, toggle] = useBoolean();
   const [error, setError] = React.useState<string>('');
-  React.useEffect(() => {
+  React.useInsertionEffect(() => {
     const scriptTag = document.createElement('script');
     scriptTag.src = 'https://accounts.google.com/gsi/client';
     scriptTag.async = true;
     scriptTag.defer = true;
     scriptTag.onload = () => {
+      window.google?.accounts.id.initialize({
+        client_id: clientId,
+        callback: async (response) => {
+          if (response.credential != null) {
+            // const googleCredentials = Realm.Credentials.google({
+            //   idToken: response.credential,
+            // });
+            // const user = await realm.logIn(googleCredentials);
+            /**
+             * Store id in localStorage
+             */
+            // console.log(user.profile.);
+            // setUser(user);
+          }
+        },
+        ux_mode: 'popup',
+      });
+      const placeholder = document.getElementById('__google_sign_in_button__');
+      if (placeholder != null)
+        window.google?.accounts.id.renderButton(placeholder, {
+          text: 'signin_with',
+          type: 'standard',
+          theme: 'filled_blue',
+          size: 'large',
+          logo_alignment: 'left',
+          width: '100',
+        });
       toggle(true);
     };
     scriptTag.onerror = (e) => {
@@ -27,28 +56,6 @@ export function useLoadGSIScript(clientId: string) {
       document.body.removeChild(scriptTag);
     };
   }, [toggle, setError]);
-
-  React.useEffect(() => {
-    if (loaded) {
-      window.google?.accounts.id.initialize({
-        client_id: clientId,
-        callback: (response) => {
-          console.log(response);
-        },
-        ux_mode: 'redirect',
-      });
-      const placeholder = document.getElementById('__google_sign_in_button__');
-      if (placeholder != null)
-        window.google?.accounts.id.renderButton(placeholder, {
-          text: 'signin_with',
-          type: 'standard',
-          theme: 'outline',
-          size: 'large',
-          logo_alignment: 'left',
-          width: '100',
-        });
-    }
-  }, [clientId, loaded]);
 
   return [loaded, error, button] as const;
 }
