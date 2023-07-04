eb9a3aec3913ee948a782f6c33a0526c1dd24cf4
Joseph Marbella <dieptuber@gmail.com>
2023-07-03 22:27:18 -0700

    feat: automatically check for id token

--- apps/nextjs/app/(root)/layout.tsx
+++ apps/nextjs/app/(root)/layout.tsx
@@ -1,13 +1,59 @@
-import { RealmProvider } from '@app/context';
+import { Providers } from '@app/context';
+import { EmbeddedResponseStatus } from '@mangayomu/request-handler';
+import env from '@mangayomu/vercel-env';
+import { StatusCodes } from 'http-status-codes';
+import { cookies } from 'next/headers';
+import '../globals.css';
+import Text from '@app/components/Text';
+import Navbar from '@app/(root)/components/navbar';
+import SafeArea from '@app/(root)/components/safearea';
 
-export default function RootLayout({ children }: React.PropsWithChildren) {
+type AuthTokenResponse =
+  | { type: 'INVALID_TOKEN' }
+  | { type: 'TOKEN_NOT_PROVIDED' }
+  | { type: 'TOKEN_VALID'; idToken: string };
+
+const authenticate = async (): Promise<AuthTokenResponse> => {
+  const nextCookies = cookies();
+  const id_token = nextCookies.get('id_token');
+  if (id_token) {
+    const body = JSON.stringify({ id_token: id_token.value });
+    const response = await fetch(
+      env().VERCEL_URL.clone().join('api', 'v1', 'session').toString(),
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body,
+        next: { revalidate: 0 },
+      },
+    );
+    const data: { response: EmbeddedResponseStatus } = await response.json();
+    switch (data.response.status_code) {
+      case StatusCodes.OK:
+        return { type: 'TOKEN_VALID', idToken: id_token.value };
+      case StatusCodes.UNAUTHORIZED:
+        return { type: 'INVALID_TOKEN' };
+    }
+  }
+  return { type: 'TOKEN_NOT_PROVIDED' };
+};
+
+export default async function RootLayout({
+  children,
+}: React.PropsWithChildren) {
+  const auth = await authenticate();
+  const idToken = auth.type === 'TOKEN_VALID' ? auth.idToken : undefined;
+
   return (
     <html lang="en">
-      <head></head>
-      <body>
-        <nav>Navigation</nav>
-        <RealmProvider>{children}</RealmProvider>
-        <footer>Footer</footer>
+      <body className="bg-default flex">
+        <Providers
+          idToken={idToken}
+          idTokenInvalid={auth.type === 'INVALID_TOKEN'}
+        >
+          <Navbar />
+          <SafeArea>{children}</SafeArea>
+        </Providers>
       </body>
     </html>
   );
