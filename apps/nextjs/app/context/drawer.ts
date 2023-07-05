4ccbad2fde6acdf555022b81ffa1465826546148
Joseph Marbella <dieptuber@gmail.com>
2023-07-05 01:19:47 -0700

    feat: add values to indicate drawer visibility

--- apps/nextjs/app/context/drawer.ts
+++ apps/nextjs/app/context/drawer.ts
@@ -2,13 +2,17 @@ import { create } from 'zustand';
 
 interface DrawerStore {
   active: boolean;
+  visible: boolean;
   toggle: (val: boolean) => void;
+  setVisible: (val: boolean) => void;
 }
 
 export const useDrawer = create<DrawerStore>((set) => ({
   active: localStorage.getItem('drawer') === 'active',
+  visible: false,
   toggle: (val) => {
     localStorage.setItem('drawer', val ? 'active' : 'hidden');
     set({ active: val });
   },
+  setVisible: (val) => set({ visible: val }),
 }));
