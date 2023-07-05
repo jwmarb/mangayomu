7906b46437166e8f0e9ecc4bb05923e1024816e9
Joseph Marbella <dieptuber@gmail.com>
2023-07-05 01:16:15 -0700

    feat: add ability to customize `Checkbox`

--- apps/nextjs/app/components/Checkbox/Checkbox.interfaces.d.ts
+++ apps/nextjs/app/components/Checkbox/Checkbox.interfaces.d.ts
@@ -1,6 +1,8 @@
+import { OverrideClassName } from '@app/hooks/useClassName';
 import React from 'react';
 import { AriaCheckboxProps } from 'react-aria';
 
 export interface CheckboxProps
   extends React.PropsWithChildren,
-    AriaCheckboxProps {}
+    AriaCheckboxProps,
+    OverrideClassName {}
--- apps/nextjs/app/components/Checkbox/Checkbox.tsx
+++ apps/nextjs/app/components/Checkbox/Checkbox.tsx
@@ -3,6 +3,7 @@ import type { CheckboxProps } from './Checkbox.interfaces';
 import { VisuallyHidden, useCheckbox, useFocusRing } from 'react-aria';
 import { useToggleState } from 'react-stately';
 import { MdCheck } from 'react-icons/md';
+import useClassName from '@app/hooks/useClassName';
 
 export default function Checkbox(props: CheckboxProps) {
   const ref = React.useRef<HTMLInputElement>(null);
@@ -10,6 +11,14 @@ export default function Checkbox(props: CheckboxProps) {
   const { isFocusVisible, focusProps } = useFocusRing();
   const { inputProps } = useCheckbox(props, state, ref);
   const isSelected = state.isSelected && !props.isIndeterminate;
+  const className = useClassName(
+    `select-none w-10 h-10 rounded-full transition duration-250 cursor-pointer flex items-center justify-center ${
+      isSelected
+        ? 'hover:bg-primary/[.18] active:bg-primary/[.35] focus:bg-primary/[.18]'
+        : 'hover:bg-hover active:bg-pressed focus:bg-pressed'
+    } ${isFocusVisible ? (isSelected ? 'bg-primary/[.18]' : 'bg-hover') : ''}`,
+    props,
+  );
   return (
     <label>
       <VisuallyHidden>
@@ -20,15 +29,7 @@ export default function Checkbox(props: CheckboxProps) {
           type="checkbox"
         />
       </VisuallyHidden>
-      <div
-        className={`select-none w-10 h-10 rounded-full transition duration-250 cursor-pointer flex items-center justify-center ${
-          isSelected
-            ? 'hover:bg-primary/[.18] active:bg-primary/[.35] focus:bg-primary/[.18]'
-            : 'hover:bg-hover active:bg-pressed focus:bg-pressed'
-        } ${
-          isFocusVisible ? (isSelected ? 'bg-primary/[.18]' : 'bg-hover') : ''
-        }`}
-      >
+      <div className={className}>
         <div
           className={`w-4 h-4 rounded flex items-center justify-center ${
             isSelected
