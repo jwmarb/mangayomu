f75164cacc450e77b161a1e95f0b671b5fd6c4a2
Joseph Marbella <dieptuber@gmail.com>
2023-07-05 17:31:11 -0700

    fix: Vercel not finding `URL` methods

--- packages/vercel-env/index.ts
+++ packages/vercel-env/index.ts
@@ -15,99 +15,24 @@ interface EnvironmentVariables {
   MONGODB_URI: string;
   MONGODB_DATABASE_NAME: string;
   VERCEL_ENV: 'production' | 'development';
-  VERCEL_URL: UrlBuildParse;
-  AUTH_URL: AuthUrl;
+  VERCEL_URL: string;
+  AUTH_URL: string;
   GOOGLE_OAUTH2_ID: string;
   GOOGLE_OAUTH2_SECRET: string;
   JWT_SECRET: string;
   JWT_EXP_DAYS: string;
 }
 
-export class AuthUrl {
-  private url: UrlBuildParse;
-  private providers: Map<string, string>;
-  public constructor(url: UrlBuildParse) {
-    this.url = new UrlBuildParse(url.toString()).join('api', 'v1', 'oauth2');
-    this.providers = new Map();
-  }
-  public provider(provider: string) {
-    const val = this.providers.get(provider);
-    if (val == null) {
-      const cloned = this.url.clone().join(provider).toString();
-      this.providers.set(provider, cloned);
-      return cloned;
-    }
-    return val;
-  }
-  public getProviders() {
-    return this.providers.keys();
-  }
-}
-
-export class UrlBuildParse {
-  private url: URL;
-  public constructor(url: string) {
-    this.url = new URL(url);
-  }
-  public join(...routes: string[]) {
-    this.url.pathname =
-      this.url.pathname +
-      (this.url.pathname.endsWith('/') ? '' : '/') +
-      routes.join('/');
-
-    return this;
-  }
-  public clone(): UrlBuildParse {
-    return new UrlBuildParse(this.url.toString());
-  }
-  public static clone(self: UrlBuildParse): UrlBuildParse {
-    return new UrlBuildParse(self.toString());
-  }
-
-  public get host() {
-    return this.url.host;
-  }
-
-  public unjoin() {
-    const paths = this.url.pathname.split('/');
-    paths.pop();
-    this.url.pathname = paths.join('/');
-    return this;
-  }
-
-  public isSSL() {
-    return this.url.protocol === 'https:';
-  }
-
-  public set protocol(set: string) {
-    this.url.protocol = set;
-  }
-  public get protocol() {
-    return this.url.protocol;
-  }
-
-  public query(args: Record<string, unknown>) {
-    for (const key in args) {
-      this.url.searchParams.append(key, String(args[key]));
-    }
-    return this;
-  }
-
-  public toString() {
-    return this.url.toString();
-  }
-}
-
 function generateUrl(environment: 'production' | 'development', host?: string) {
   if (host == null) {
     console.warn('host is undefined. Falling back to localhost:3000...');
-    return new UrlBuildParse('http://localhost:3000/');
+    return 'http://localhost:3000/';
   }
   switch (environment) {
     case 'development':
-      return new UrlBuildParse(`http://${host}`);
+      return `http://${host}`;
     case 'production':
-      return new UrlBuildParse(`https://${host}`);
+      return `https://${host}`;
   }
 }
 
@@ -120,7 +45,6 @@ function generateVariables() {
   cached = {
     VERCEL_ENV,
     VERCEL_URL,
-    AUTH_URL: new AuthUrl(VERCEL_URL),
   } as unknown as EnvironmentVariables;
   for (const [key, value] of Object.entries(process.env)) {
     if (requiredVars.has(key) && key !== 'VERCEL_ENV' && key !== 'VERCEL_URL')
