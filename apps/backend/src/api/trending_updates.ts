6efaf53bf6ebd58d8984f114c6993fec44433f79
Joseph Marbella <dieptuber@gmail.com>
2023-07-09 17:32:08 -0700

    fix: promise not awaited

--- apps/backend/src/api/trending_updates.ts
+++ apps/backend/src/api/trending_updates.ts
@@ -5,7 +5,11 @@ import type { UpdatesParams } from '@main';
 const get: Route = async (req, res) => {
   const query = req.query<UpdatesParams>();
   if (!Array.isArray(query.source)) query.source = [query.source];
-  const data = getListMangas(query.source, 'trending_updates', 'listHotMangas');
+  const data = await getListMangas(
+    query.source,
+    'trending_updates',
+    'listHotMangas',
+  );
 
   res.json(data);
 };
