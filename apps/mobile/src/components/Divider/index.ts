e433d55a74b4b6d66d5a017d99342f3b1c1ab691
Joseph Marbella <dieptuber@gmail.com>
2023-07-10 22:02:05 -0700

    feat: extend `BoxProps` for customization

--- apps/mobile/src/components/Divider/index.ts
+++ apps/mobile/src/components/Divider/index.ts
@@ -1,5 +1,7 @@
+import { BoxProps } from '@components/Box';
 import Divider from './Divider';
 export default Divider;
-export interface DividerProps extends React.PropsWithChildren {
+export interface DividerProps extends React.PropsWithChildren, BoxProps {
   orientation?: 'vertical' | 'horizontal';
+  shrink?: boolean;
 }
