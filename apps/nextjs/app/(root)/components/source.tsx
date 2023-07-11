651a6adfba4836386d78368793df7d99c27ca235
Joseph Marbella <dieptuber@gmail.com>
2023-07-10 20:49:37 -0700

    refactor: use `Image` instead of `img`

--- apps/nextjs/app/(root)/components/source.tsx
+++ apps/nextjs/app/(root)/components/source.tsx
@@ -1,6 +1,6 @@
-'use client';
 import Text from '@app/components/Text';
 import { MangaHost } from '@mangayomu/mangascraper';
+import Image from 'next/image';
 import React from 'react';
 
 interface ListSourceProps {
@@ -14,7 +14,14 @@ export default function ListSource(props: ListSourceProps) {
 
   return (
     <button className="flex flex-row items-center space-x-2 hover:bg-hover border-2 border-transparent focus:border-primary/[.3] p-1 rounded-lg">
-      <img src={source.icon} loading="lazy" className="w-6 h-6" />
+      <div className="relative w-6 h-6">
+        <Image
+          fill
+          src={source.icon}
+          loading="lazy"
+          alt={`${strSource} icon`}
+        />
+      </div>
       <Text variant="button">{source.name}</Text>
     </button>
   );
