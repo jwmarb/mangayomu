884e46c9bb56061da63584f7672daf5c2c3e2897
Joseph Marbella <dieptuber@gmail.com>
2023-07-10 20:49:51 -0700

    todo: finish loading layout

---
+++ apps/nextjs/app/(root)/sources/loading.tsx
@@ -0,0 +1,10 @@
+import Text from '@app/components/Text';
+import React from 'react';
+
+export default function Sources() {
+  return (
+    <div>
+      <Text>Loading...</Text>
+    </div>
+  );
+}
