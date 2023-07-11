3ed2476d6f8b0053b06ce35aca39a1a22380784b
Joseph Marbella <dieptuber@gmail.com>
2023-07-10 20:50:16 -0700

    refactor: remove unnecessary line

--- apps/backend/src/api/[source]/manga.ts
+++ apps/backend/src/api/[source]/manga.ts
@@ -1,8 +1,6 @@
 import { Handler, Route } from '@mangayomu/request-handler';
-import { redis } from '@main';
 
 const get: Route = (req, res) => {
-  redis.get('key');
   res.json({ status: 'work in progress', query: req.query() });
 };
 
