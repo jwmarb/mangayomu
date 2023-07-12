4bfba5d4173c9dea04bd06f1127e23b3eba03060
Joseph Marbella <dieptuber@gmail.com>
2023-07-12 09:54:11 -0700

    feat: create absolute path for env vars

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -18,6 +18,7 @@ module.exports = {
           '@assets': './src/assets',
           '@helpers': './src/helpers',
           '@database': './src/realm',
+          env: './src/constants',
         },
       },
     ],
