5aa9fbb1e4f5d6d785c52ad83722abd39146487d
Joseph Marbella <dieptuber@gmail.com>
2023-07-13 17:01:05 -0700

    fix: text not aligned properly

--- apps/mobile/src/screens/MangaView/components/MangaViewerHeader/components/MangaSupportedLanguages/MangaSupportedLanguages.tsx
+++ apps/mobile/src/screens/MangaView/components/MangaViewerHeader/components/MangaSupportedLanguages/MangaSupportedLanguages.tsx
@@ -1,11 +1,9 @@
 import Box from '@components/Box';
-import Skeleton from '@components/Skeleton';
 import Stack from '@components/Stack';
 import Text from '@components/Text';
 import languages, { ISOLangCode } from '@mangayomu/language-codes';
 import React from 'react';
 import { MangaSupportedLanguagesProps } from './MangaSupportedLanguages.interfaces';
-import { moderateScale } from 'react-native-size-matters';
 
 const MangaSupportedLanguages: React.FC<MangaSupportedLanguagesProps> = (
   props,
@@ -15,14 +13,18 @@ const MangaSupportedLanguages: React.FC<MangaSupportedLanguagesProps> = (
   return (
     <Stack space="s" flex-direction="row" justify-content="space-between">
       <Text color="textSecondary">Supported languages</Text>
-      <Box maxWidth="50%" align-self="flex-end" flex-grow>
+      <Box maxWidth="50%" flex-grow>
         {data == null ? (
           <Text.Skeleton
             numberOfLines={3}
-            lineStyles={[{}, {}, { width: '50%' }]}
+            lineStyles={[
+              {},
+              { width: '90%', 'align-self': 'flex-end' },
+              { width: '50%', 'align-self': 'flex-end' },
+            ]}
           />
         ) : (
-          <Text bold>
+          <Text bold align="right">
             {data
               .map((x: unknown) => languages[x as ISOLangCode].name)
               .join(', ')}
