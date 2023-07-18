5ed690fa952f3ec1d938c1bce71b103cde4eec08
Joseph Marbella <dieptuber@gmail.com>
2023-07-18 10:55:21 -0700

    refactor: remove `auth0` package and traces

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -11,8 +11,6 @@ import { AppState, AppStateStatus, StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import Root from './src/Root';
 import { store, persistor } from '@redux/main';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Auth0Provider } from 'react-native-auth0';
 import { PersistGate } from 'redux-persist/integration/react';
 import { enableFreeze } from 'react-native-screens';
 import {
@@ -26,10 +24,6 @@ import { RealmEffect } from '@database/providers/RealmProvider';
 import { AppearanceProvider } from '@theme/provider';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 enableFreeze(true);
-enum Auth0 {
-  DOMAIN = 'dev-wq6wbghv.us.auth0.com',
-  CLIENT_ID = '2dLESXiDyJFgpzKU2FYZJKb0s9yzXWX8',
-}
 
 function App(): JSX.Element {
   React.useEffect(() => {
@@ -51,28 +45,26 @@ function App(): JSX.Element {
       <SafeAreaProvider>
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
-            <Auth0Provider domain={Auth0.DOMAIN} clientId={Auth0.CLIENT_ID}>
-              <AppProvider id={REACT_APP_REALM_ID}>
-                <UserProvider fallback={<RealmUserProvider />}>
-                  <RealmProvider
-                    sync={{
-                      flexible: true,
-                      onError: (_, error) => {
-                        console.error(error);
-                      },
-                    }}
-                  >
-                    <LocalRealmProvider>
-                      <RealmEffect>
-                        <AppearanceProvider>
-                          <Root />
-                        </AppearanceProvider>
-                      </RealmEffect>
-                    </LocalRealmProvider>
-                  </RealmProvider>
-                </UserProvider>
-              </AppProvider>
-            </Auth0Provider>
+            <AppProvider id={REACT_APP_REALM_ID}>
+              <UserProvider fallback={<RealmUserProvider />}>
+                <RealmProvider
+                  sync={{
+                    flexible: true,
+                    onError: (_, error) => {
+                      console.error(error);
+                    },
+                  }}
+                >
+                  <LocalRealmProvider>
+                    <RealmEffect>
+                      <AppearanceProvider>
+                        <Root />
+                      </AppearanceProvider>
+                    </RealmEffect>
+                  </LocalRealmProvider>
+                </RealmProvider>
+              </UserProvider>
+            </AppProvider>
           </PersistGate>
         </Provider>
       </SafeAreaProvider>
