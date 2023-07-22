4cf29d21aa81224a98c87ba266bf3a409674092c
Joseph Marbella <dieptuber@gmail.com>
2023-07-21 23:17:36 -0700

    refactor: move type defs to `index`

--- apps/mobile/src/screens/Reader/components/ChapterPage/index.ts
+++ apps/mobile/src/screens/Reader/components/ChapterPage/index.ts
@@ -1,1 +1,26 @@
 export { default } from './ChapterPage';
+import {
+  ChapterPage,
+  ExtendedReaderPageState,
+} from '@redux/slices/reader/reader';
+import { ReadingDirection } from '@redux/slices/settings';
+import { ImageMenuMethods } from '@screens/Reader/components/ImageMenu/ImageMenu.interfaces';
+import { PageGestures } from '@screens/Reader/hooks/useOverlayGesture';
+import { PanGesture, PinchGesture } from 'react-native-gesture-handler';
+import { SharedValue } from 'react-native-reanimated';
+
+export interface ChapterPageProps {
+  page: ChapterPage;
+  extendedPageState?: ExtendedReaderPageState;
+}
+
+export interface ChapterPageContextState {
+  velocityX: SharedValue<number>;
+  rootPanGesture: PanGesture;
+  rootPinchGesture: PinchGesture;
+  readingDirection: ReadingDirection;
+  sourceName: string;
+  mangaTitle: string;
+  imageMenuRef: React.RefObject<ImageMenuMethods>;
+  pageGestures: PageGestures;
+}
