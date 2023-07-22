9154478671dd77ebc5f308b5fb53eb43aabd9481
Joseph Marbella <dieptuber@gmail.com>
2023-07-21 17:34:57 -0700

    refactor: improvise code

--- apps/mobile/src/screens/Reader/hooks/useData.ts
+++ apps/mobile/src/screens/Reader/hooks/useData.ts
@@ -1,4 +1,9 @@
-import { useRealm, useLocalRealm } from '@database/main';
+import {
+  useRealm,
+  useLocalRealm,
+  useLocalQuery,
+  useLocalObject,
+} from '@database/main';
 import { ChapterSchema } from '@database/schemas/Chapter';
 import { LocalChapterSchema } from '@database/schemas/LocalChapter';
 import useCombinedMangaWithLocal from '@hooks/useCombinedMangaWithLocal';
@@ -12,49 +17,38 @@ import React from 'react';
  * @returns Returns objects derived from Realm
  */
 export default function useData(mangaKey: string, chapterKey: string) {
-  const localRealm = useLocalRealm();
   const realm = useRealm();
   const user = useUser();
   const manga = useCombinedMangaWithLocal(mangaKey, true);
 
-  const [chapter, setChapter] = React.useState(
-    localRealm.objectForPrimaryKey(LocalChapterSchema, chapterKey),
-  );
+  const chapter = useLocalObject(LocalChapterSchema, chapterKey);
+
   const chapterWithDataInitializer = () => {
-    const existingChapter = realm.objectForPrimaryKey(
+    let existingChapter: ChapterSchema | undefined = realm.objectForPrimaryKey(
       ChapterSchema,
       chapterKey,
     );
     if (existingChapter == null) {
-      let newChapter: ChapterSchema = {} as ChapterSchema;
-      const localChapter = localRealm.objectForPrimaryKey(
-        LocalChapterSchema,
-        chapterKey,
-      );
-      if (localChapter == null)
-        throw new Error(
-          `Tried to use ${chapterKey} from LocalChapter collection, but the value was undefined. To fix this, sync all the mangas first`,
-        );
       realm.write(() => {
-        newChapter = realm.create(ChapterSchema, {
-          _id: chapterKey,
-          _realmId: user.id,
-          _mangaId: mangaKey,
-          indexPage: 0,
-        });
+        existingChapter = realm.create(
+          ChapterSchema,
+          {
+            _id: chapterKey,
+            _realmId: user.id,
+            _mangaId: mangaKey,
+            indexPage: 0,
+          },
+          Realm.UpdateMode.All,
+        );
       });
-      return newChapter;
     }
-    return existingChapter;
+    return existingChapter as ChapterSchema;
   };
   const [chapterWithData, setChapterWithData] = React.useState<ChapterSchema>(
     chapterWithDataInitializer,
   );
 
-  const collection = localRealm.objects(LocalChapterSchema);
-
   React.useEffect(() => {
-    setChapter(localRealm.objectForPrimaryKey(LocalChapterSchema, chapterKey));
     setChapterWithData(chapterWithDataInitializer);
   }, [chapterKey]);
 
@@ -63,15 +57,16 @@ export default function useData(mangaKey: string, chapterKey: string) {
       'Chapter does not exist. This error is thrown because data about the chapter is null. The user should fetch the manga first before reading a chapter.',
     );
 
-  const readableChapters = React.useMemo(
-    () =>
-      collection.filtered(
+  const readableChapters = useLocalQuery(
+    LocalChapterSchema,
+    (localChapters) =>
+      localChapters.filtered(
         'language == $0 && _mangaId == $1 SORT(index ASC)',
         chapter.language,
         chapter._mangaId,
-      ) as unknown,
+      ),
     [chapter.language, chapter._mangaId],
-  ) as LocalChapterSchema[];
+  );
 
   return [manga, chapter, readableChapters, chapterWithData] as const;
 }
