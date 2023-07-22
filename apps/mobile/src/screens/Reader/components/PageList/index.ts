6173ba951f788a90436c4c8612a957c227d748db
Joseph Marbella <dieptuber@gmail.com>
2023-07-21 23:19:44 -0700

    refactor: move type defs into `index`

--- apps/mobile/src/screens/Reader/components/PageList/index.ts
+++ apps/mobile/src/screens/Reader/components/PageList/index.ts
@@ -1,1 +1,14 @@
 export { default } from './PageList';
+import { Page } from '@redux/slices/reader';
+import { ReadingDirection } from '@redux/slices/settings';
+import { FlashListProps } from '@shopify/flash-list';
+import { FlatListProps } from 'react-native';
+import { GestureType } from 'react-native-gesture-handler';
+
+export interface PageListProps
+  extends Omit<FlatListProps<Page>, keyof FlashListProps<Page>>,
+    FlashListProps<Page> {
+  readingDirection: ReadingDirection;
+  panRef: React.MutableRefObject<GestureType | undefined>;
+  pinchRef: React.MutableRefObject<GestureType | undefined>;
+}
