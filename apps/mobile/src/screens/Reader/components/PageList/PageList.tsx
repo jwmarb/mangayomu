f1a8f73811fcb97832be35a06603ec858970ee11
Joseph Marbella <dieptuber@gmail.com>
2023-07-21 21:20:41 -0700

    feat: use `FlashList` compatible with gestures

--- apps/mobile/src/screens/Reader/components/PageList/PageList.tsx
+++ apps/mobile/src/screens/Reader/components/PageList/PageList.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { ReadingDirection } from '@redux/slices/settings';
 import { PageListProps } from '@screens/Reader/components/PageList/PageList.interfaces';
-import { FlatListProps } from 'react-native';
+import { FlatListProps, ScrollViewProps } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import {
   FlashList,
   ListRenderItemInfo as FlashListRenderItemInfo,
 } from '@shopify/flash-list';
 import { Page } from '@redux/slices/reader';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const PageList: React.ForwardRefRenderFunction<
   FlashList<Page> | FlatList<Page>,
@@ -22,8 +23,15 @@ const PageList: React.ForwardRefRenderFunction<
     panRef,
     ...rest
   } = props;
+  const renderScrollComponent = React.useMemo(
+    () =>
+      React.forwardRef<ScrollView>((props, ref) => (
+        <ScrollView ref={ref} {...props} simultaneousHandlers={panRef} />
+      )),
+    [],
+  ) as unknown as React.FC<ScrollViewProps>;
   switch (readingDirection) {
-    default: {
+    case ReadingDirection.WEBTOON: {
       return (
         <FlatList
           // scrollEnabled={false}
@@ -52,17 +60,18 @@ const PageList: React.ForwardRefRenderFunction<
         />
       );
     }
-    // default:
-    //   return (
-    //     <FlashList
-    //       ref={ref as React.ForwardedRef<FlashList<Page>>}
-    //       {...rest}
-    //       estimatedFirstItemOffset={estimatedFirstItemOffset}
-    //       initialScrollIndex={initialScrollIndex}
-    //       renderItem={preDeterminedRenderItem}
-    //       extraData={extraData}
-    //     />
-    //   );
+    default:
+      return (
+        <FlashList
+          renderScrollComponent={renderScrollComponent}
+          ref={ref as React.ForwardedRef<FlashList<Page>>}
+          {...rest}
+          estimatedFirstItemOffset={estimatedFirstItemOffset}
+          initialScrollIndex={initialScrollIndex}
+          renderItem={preDeterminedRenderItem}
+          extraData={extraData}
+        />
+      );
   }
 };
 
