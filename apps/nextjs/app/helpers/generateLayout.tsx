3612964df7e21e38c3548df7f366c2477d2ad690
Joseph Marbella <dieptuber@gmail.com>
2023-07-25 00:44:40 -0700

    feat: generate layout helper

---
+++ apps/nextjs/app/helpers/generateLayout.tsx
@@ -0,0 +1,47 @@
+import { Providers } from '@app/context';
+import '../globals.css';
+import Navbar from '@app/(root)/components/navbar';
+import SafeArea from '@app/(root)/components/safearea';
+import authenticate from '@app/helpers/authenticate';
+import React from 'react';
+
+export interface RootLayoutProps extends React.PropsWithChildren {
+  paper?: boolean;
+  idToken?: string;
+  idTokenInvalid?: boolean;
+}
+
+async function RootLayout({
+  children,
+  paper,
+  idToken,
+  idTokenInvalid,
+}: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body className={`${paper ? 'bg-paper' : 'bg-default'} flex`}>
+        <Providers idToken={idToken} idTokenInvalid={idTokenInvalid}>
+          <Navbar />
+          <SafeArea>{children}</SafeArea>
+        </Providers>
+      </body>
+    </html>
+  );
+}
+
+export default function generateLayout(layout?: RootLayoutProps) {
+  async function Layout(props?: RootLayoutProps) {
+    const auth = await authenticate();
+    const idToken = auth.type === 'TOKEN_VALID' ? auth.idToken : undefined;
+    return (
+      <RootLayout
+        {...props}
+        {...layout}
+        idToken={idToken}
+        idTokenInvalid={auth.type === 'INVALID_TOKEN'}
+      />
+    );
+  }
+
+  return Layout;
+}
