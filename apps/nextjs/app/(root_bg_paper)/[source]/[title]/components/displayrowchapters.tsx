557cbea3078d485d68f2095003ee52f21d7dcfd0
Joseph Marbella <dieptuber@gmail.com>
2023-07-26 01:21:14 -0700

    todo

--- apps/nextjs/app/(root_bg_paper)/[source]/[title]/components/displayrowchapters.tsx
+++ apps/nextjs/app/(root_bg_paper)/[source]/[title]/components/displayrowchapters.tsx
@@ -3,7 +3,9 @@ import { MangaChapter } from '@mangayomu/mangascraper';
 import RowChapter from './rowchapter';
 import React from 'react';
 import useMongoClient from '@app/hooks/useMongoClient';
-import MangaSchema from '@app/realm/Manga';
+import MangaSchema, { IMangaSchema } from '@app/realm/Manga';
+import useObject from '@app/hooks/useObject';
+import Button from '@app/components/Button';
 
 interface DisplayRowChaptersProps {
   chapters: MangaChapter[];
@@ -12,24 +14,29 @@ interface DisplayRowChaptersProps {
 
 export default function DisplayRowChapters(props: DisplayRowChaptersProps) {
   const { chapters, mangaId } = props;
-  const mangas = useMongoClient(MangaSchema);
-  const getManga = React.useCallback(async () => {
-    const p = await mangas.findOne({ _id: mangaId });
-    console.log(p);
-  }, [mangas, mangaId]);
-  React.useEffect(() => {
-    getManga();
-  }, [getManga]);
+  const manga = useObject<IMangaSchema>(MangaSchema, mangaId);
 
   return (
-    <div className="max-w-screen-md mx-auto w-full flex flex-col">
-      {chapters.map((x, i, self) => (
-        <RowChapter
-          chapter={x}
-          key={x.link}
-          isLastItem={i === self.length - 1}
-        />
-      ))}
-    </div>
+    <>
+      <Button
+        onPress={() => {
+          // console.log(manga);
+          manga?.update((prev) => {
+            prev.inLibrary = !prev.inLibrary;
+          });
+        }}
+      >
+        Test
+      </Button>
+      <div className="max-w-screen-md mx-auto w-full flex flex-col">
+        {chapters.map((x, i, self) => (
+          <RowChapter
+            chapter={x}
+            key={x.link}
+            isLastItem={i === self.length - 1}
+          />
+        ))}
+      </div>
+    </>
   );
 }
