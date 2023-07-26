0958635ca691bc9a250b6c107e50fd7d4fc94717
Joseph Marbella <dieptuber@gmail.com>
2023-07-26 15:03:16 -0700

    fix: webtoon page not scaling correctly

--- apps/mobile/src/screens/Reader/components/ChapterPage/hooks/usePageScale.ts
+++ apps/mobile/src/screens/Reader/components/ChapterPage/hooks/usePageScale.ts
@@ -1,21 +1,15 @@
 import useScreenDimensions from '@hooks/useScreenDimensions';
-import { ReadingDirection } from '@redux/slices/settings';
 import { ConnectedChapterPageProps } from '@screens/Reader/components/ChapterPage/ChapterPage.redux';
-import { useChapterPageContext } from '@screens/Reader/components/ChapterPage/context/ChapterPageContext';
 import React from 'react';
 
 export default function usePageScale(props: ConnectedChapterPageProps) {
-  const { readingDirection } = useChapterPageContext();
-  const { width, height } = useScreenDimensions();
+  const { width } = useScreenDimensions();
   const imageWidth = props.page.width;
   const imageHeight = props.page.height;
   const scale = React.useMemo(() => width / imageWidth, [imageWidth, width]);
   const stylizedHeight = React.useMemo(
-    () =>
-      readingDirection !== ReadingDirection.WEBTOON
-        ? scale * imageHeight
-        : height,
-    [scale, imageHeight, height, readingDirection],
+    () => scale * imageHeight,
+    [scale, imageHeight],
   );
 
   return { scale, stylizedHeight };
