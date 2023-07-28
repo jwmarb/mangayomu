0368732388039c771fc3eb9781f8eac4e5bccd11
Joseph Marbella <dieptuber@gmail.com>
2023-07-28 09:44:34 -0700

    fix: blank author

--- apps/nextjs/app/(root_bg_paper)/[source]/[title]/components/authors.tsx
+++ apps/nextjs/app/(root_bg_paper)/[source]/[title]/components/authors.tsx
@@ -10,7 +10,7 @@ export default function Authors(props: AuthorsProps) {
   if (authors == null) return <Text.Skeleton width="100%" />;
   return (
     <Text color="text-secondary" className="text-center">
-      by {authors?.join(', ') ?? 'unknown'}
+      by {authors.length > 0 ? authors.join(', ') : 'unknown'}
     </Text>
   );
 }
