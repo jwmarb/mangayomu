8e73ac20ce95618e96fbc8d53ba105b3f5f021de
Joseph Marbella <dieptuber@gmail.com>
2023-07-27 18:29:23 -0700

    feat: add custom text to manipulate width

--- apps/nextjs/app/components/Text/skeleton.tsx
+++ apps/nextjs/app/components/Text/skeleton.tsx
@@ -7,20 +7,27 @@ interface SkeletonProps<T extends TextComponentType>
   extends OverrideClassName,
     TextProps<T> {
   numberOfLines?: number;
+  text?: string[];
   classNames?: string[];
 }
 
 export default function Skeleton<T extends TextComponentType>(
   props: SkeletonProps<T>,
 ) {
-  const { className: _, numberOfLines = 1, classNames = [], ...rest } = props;
+  const {
+    className: _,
+    numberOfLines = 1,
+    classNames = [],
+    text = new Array(numberOfLines).fill('placeholder'),
+    ...rest
+  } = props;
   const className = useClassName(
     'skeleton rounded-full inline absolute left-0 right-0 bottom-0 top-0 h-[50%] my-auto',
     props,
   );
   return (
     <div className="flex flex-col flex-grow w-full">
-      {new Array(numberOfLines).fill('placeholder').map((x, i) => (
+      {new Array(numberOfLines).fill(0).map((x, i) => (
         <Text
           component="span"
           key={i}
@@ -28,7 +35,7 @@ export default function Skeleton<T extends TextComponentType>(
           className={[classNames[i], 'relative'].join(' ')}
         >
           <Text component="span" {...rest} className="opacity-0">
-            {x}
+            {text[i]}
           </Text>
           <div className={className} />
         </Text>
