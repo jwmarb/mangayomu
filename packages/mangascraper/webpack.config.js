5dd99dd901db0411b658c6d384f715b6de0c3490
Joseph Marbella <dieptuber@gmail.com>
2023-07-30 10:14:35 -0700

    refactor: remove unused deps

--- packages/mangascraper/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-module.exports = {
-  entry: './dist/src/index.js',
-  output: {
-    // eslint-disable-next-line no-undef
-    path: path.resolve(__dirname, 'bundle'),
-    filename: 'index.bundle.js',
-  },
-};
