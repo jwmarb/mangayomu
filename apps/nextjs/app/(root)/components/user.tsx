c148f43bf4e03f1c53b448a4771cfc62ea47f595
Joseph Marbella <dieptuber@gmail.com>
2023-07-29 20:30:43 -0700

    feat: navigate to settings

--- apps/nextjs/app/(root)/components/user.tsx
+++ apps/nextjs/app/(root)/components/user.tsx
@@ -3,6 +3,7 @@ import IconButton from '@app/components/IconButton';
 import Text from '@app/components/Text';
 import { useUser } from '@app/context/realm';
 import { useSafeArea } from '@app/context/safearea';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 import { MdChevronLeft, MdPerson, MdSettings } from 'react-icons/md';
 
@@ -14,17 +15,22 @@ export default function User(props: UserProps) {
   const { onDrawerClose } = props;
   const user = useUser();
   const mobile = useSafeArea((store) => store.mobile);
+  const router = useRouter();
+  const onSettings = () => {
+    onDrawerClose();
+    router.push('/settings');
+  };
   return (
     <div className="flex flex-row justify-between gap-2">
       <div className="flex flex-row gap-2 items-center">
         <div className="w-10 h-10 bg-default rounded-lg items-center justify-center flex">
           <MdPerson className="text-text-primary" />
         </div>
-        <Text>{user.profile.name ?? 'Guest'}</Text>
+        <Text className="break-all">{user.profile.name ?? 'Guest'}</Text>
       </div>
       <div className="flex flex-row">
-        <DarkMode />
-        <IconButton icon={<MdSettings />} onPress={onDrawerClose} />
+        {/* <DarkMode /> */}
+        <IconButton icon={<MdSettings />} onPress={onSettings} />
         {mobile && (
           <IconButton icon={<MdChevronLeft />} onPress={onDrawerClose} />
         )}
