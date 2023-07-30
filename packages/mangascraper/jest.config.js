bbd59f55031cdff7b1db37cadc5a6aed449737c5
Joseph Marbella <dieptuber@gmail.com>
2023-07-30 10:14:05 -0700

    fix: tests

--- packages/mangascraper/jest.config.js
+++ packages/mangascraper/jest.config.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-undef */
 module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: [
     '**/__tests__/**/*.+(ts|tsx|js)',
