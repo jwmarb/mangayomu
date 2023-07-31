dd9260390111c750ab5fdb6b69c13ef997769f14
Joseph Marbella <dieptuber@gmail.com>
2023-07-31 03:17:20 -0700

    feat: add library

---
+++ apps/nextjs/app/(root)/library/components/filter.tsx
@@ -0,0 +1,86 @@
+import { Accordion } from '@app/components/Accordion';
+import Button from '@app/components/Button';
+import Modal, { ModalMethods } from '@app/components/Modal';
+import Tabs from '@app/components/Tabs';
+import Text from '@app/components/Text';
+import Filter from '@app/components/Filter';
+import React from 'react';
+import {
+  SORT_LIBRARY_BY_KEYS,
+  useMangaLibrary,
+  useMangaLibraryFilters,
+} from '@app/context/library';
+import { shallow } from 'zustand/shallow';
+
+function LibraryFilter(
+  props: React.PropsWithChildren,
+  ref: React.ForwardedRef<ModalMethods>,
+) {
+  const mangas = useMangaLibrary((s) => s.mangas);
+  const [sources, setSources, reset, sortBy, setSortBy, reversed] =
+    useMangaLibraryFilters(
+      (s) => [
+        s.includeSources,
+        s.setIncludeSources,
+        s.resetFilters,
+        s.sortLibraryBy,
+        s.setSortBy,
+        s.reversedSort,
+      ],
+      shallow,
+    );
+  const numberInLibrary = React.useMemo(
+    () =>
+      Object.entries(
+        mangas.reduce((prev, curr) => {
+          if (prev[curr.source] == null) prev[curr.source] = 0;
+          prev[curr.source] += 1;
+          return prev;
+        }, {} as Record<string, number>),
+      ),
+    [mangas],
+  );
+  return (
+    <Modal ref={ref} className="h-96">
+      <Tabs>
+        <Tabs.List>
+          <Tabs.Tab>Filter</Tabs.Tab>
+          <Tabs.Tab>Sort</Tabs.Tab>
+        </Tabs.List>
+        <Tabs.Panel>
+          <div className="flex flex-col p-4">
+            <Button onPress={reset} className="w-full">
+              Reset Filters
+            </Button>
+          </div>
+          <Accordion title="Sources">
+            <Filter type="checkbox" selected={sources} onChange={setSources}>
+              {numberInLibrary.map(([source, count]) => (
+                <Filter.Checkbox
+                  key={source}
+                  value={source}
+                  text={source}
+                  subtitle={`(${count})`}
+                />
+              ))}
+            </Filter>
+          </Accordion>
+        </Tabs.Panel>
+        <Tabs.Panel>
+          <Filter
+            type="sort"
+            selected={sortBy}
+            reversed={reversed}
+            onChange={setSortBy}
+          >
+            {SORT_LIBRARY_BY_KEYS.map((x) => (
+              <Filter.Sort value={x} key={x} />
+            ))}
+          </Filter>
+        </Tabs.Panel>
+      </Tabs>
+    </Modal>
+  );
+}
+
+export default React.forwardRef(LibraryFilter);
---
+++ apps/nextjs/app/(root)/library/page.tsx
@@ -0,0 +1,99 @@
+'use client';
+import Book from '@app/components/Book';
+import IconButton from '@app/components/IconButton';
+import { ModalMethods } from '@app/components/Modal';
+import Screen from '@app/components/Screen';
+import Text from '@app/components/Text';
+import TextField from '@app/components/TextField';
+import {
+  SORT_LIBRARY_BY,
+  useMangaLibrary,
+  useMangaLibraryFilters,
+} from '@app/context/library';
+import React from 'react';
+import { MdFilterAlt } from 'react-icons/md';
+import { shallow } from 'zustand/shallow';
+import Filter from './components/filter';
+import NoMangasInLibrary from '@app/(root)/library/components/nomangasinlibrary';
+import { inPlaceSort } from 'fast-sort';
+
+export default function Page() {
+  const [includedSrc, sortBy, reversed] = useMangaLibraryFilters(
+    (s) => [s.includeSources, s.sortLibraryBy, s.reversedSort],
+    shallow,
+  );
+  const includedSrcUniq = React.useMemo(
+    () => new Set(includedSrc),
+    [includedSrc],
+  );
+  const query = useMangaLibrary((s) => s.query);
+  const mangas = useMangaLibrary((s) => s.mangas, shallow);
+  const syncStatus = useMangaLibrary((s) => s.syncStatus);
+  const filtered = React.useMemo(() => {
+    const parsedQuery = query.trim().toLowerCase();
+    return mangas.filter(
+      (prev) =>
+        prev.title.toLowerCase().trim().includes(parsedQuery) &&
+        includedSrcUniq.has(prev.source),
+    );
+  }, [mangas, query, includedSrcUniq]);
+  const sorted = React.useMemo(
+    () =>
+      inPlaceSort(filtered).by(
+        reversed
+          ? [{ asc: SORT_LIBRARY_BY[sortBy] }]
+          : [{ desc: SORT_LIBRARY_BY[sortBy] }],
+      ),
+    [filtered, reversed, sortBy],
+  );
+  const setQuery = useMangaLibrary((s) => s.setQuery);
+  const ref = React.useRef<ModalMethods>(null);
+  const handleOnOpenModal = () => {
+    ref.current?.open();
+  };
+  return (
+    <Screen>
+      <Filter ref={ref} />
+      <Screen.Header className="pb-2 z-10">
+        <div className="max-w-screen-xl mx-auto w-full grid grid-cols-[1fr_min-content] justify-between items-center gap-2">
+          <TextField
+            onChange={setQuery}
+            className="w-full"
+            placeholder="Search for a title..."
+          />
+          <div>
+            <IconButton icon={<MdFilterAlt />} onPress={handleOnOpenModal} />
+          </div>
+        </div>
+      </Screen.Header>
+      <Screen.Content className="flex flex-row flex-wrap justify-center">
+        {syncStatus === 'initializing' &&
+          new Array(30).fill('').map((x, i) => <Book.Skeleton key={i} />)}
+        {syncStatus === 'done' && mangas.length === 0 && <NoMangasInLibrary />}
+        {syncStatus === 'done' &&
+          (filtered.length > 0 ? (
+            sorted.map((x) => (
+              <Book
+                key={x._id}
+                manga={{
+                  title: x.title,
+                  link: x._id,
+                  imageCover: x.imageCover,
+                  source: x.source,
+                }}
+              />
+            ))
+          ) : (
+            <div className="flex flex-col justify-start w-full">
+              <Text variant="header-emphasized">No results found</Text>
+              <Text color="text-secondary">
+                {query.length > 0
+                  ? `There are no mangas that match "${query}"`
+                  : 'No mangas match the filter'}
+              </Text>
+            </div>
+          ))}
+      </Screen.Content>
+    </Screen>
+  );
+}
