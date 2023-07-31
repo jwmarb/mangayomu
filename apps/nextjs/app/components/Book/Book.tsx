1cbd266e677bd8d23376d7dec425cb007fa07f65
Joseph Marbella <dieptuber@gmail.com>
2023-07-31 03:11:36 -0700

    feat: add skeleton loader to image

--- apps/nextjs/app/components/Book/Book.tsx
+++ apps/nextjs/app/components/Book/Book.tsx
@@ -6,23 +6,29 @@ import getMangaHost from '@app/helpers/getMangaHost';
 import { IconBadge } from '@app/components/Badge';
 import Link from 'next/link';
 import getSlug from '@app/helpers/getSlug';
+import useBoolean from '@app/hooks/useBoolean';
 
 function Book(props: BookProps) {
   const {
     manga: { source, imageCover, title, link },
   } = props;
   const host = getMangaHost(source);
+  const [loading, toggle] = useBoolean(true);
   return (
     <Link href={`/${getSlug(source)}/${getSlug(title)}`}>
       <IconBadge src={host.icon} alt={source + ' Icon'}>
         <button className="transition duration-250 ease-in-out flex flex-col items-start w-[7rem] sm:w-[7.5rem] md:w-[8.5rem] gap-2 hover:bg-hover active:bg-pressed rounded-lg p-2 focus:bg-hover">
           <div className="w-full md:h-[10.5rem] sm:h-[9.26471rem] h-[8.64706rem] relative">
             <img
+              onLoad={() => toggle(false)}
               loading="lazy"
               src={imageCover}
               alt="cover image"
               className="rounded-lg object-cover w-full h-full"
             />
+            {loading && (
+              <div className="skeleton w-full md:h-[10.5rem] sm:h-[9.26471rem] h-[8.64706rem] absolute left-0 top-0 right-0 rounded-lg" />
+            )}
           </div>
           <Text
             variant="book-title"
