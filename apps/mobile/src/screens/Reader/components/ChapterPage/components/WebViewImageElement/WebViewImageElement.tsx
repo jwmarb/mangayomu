eccf99db0aaa3e8f1025aee50e5a2a86b4f3c0cc
Joseph Marbella <dieptuber@gmail.com>
2023-08-01 12:02:26 -0700

    refactor: reduce elements

--- apps/mobile/src/screens/Reader/components/ChapterPage/components/WebViewImageElement/WebViewImageElement.tsx
+++ apps/mobile/src/screens/Reader/components/ChapterPage/components/WebViewImageElement/WebViewImageElement.tsx
@@ -12,19 +12,14 @@ const WebViewImageElement: React.ForwardRefRenderFunction<
 > = (props, ref) => {
   const { style, onMessage, uri, backgroundColor, animatedStyle } = props;
   return (
-    <AnimatedBox
-      style={animatedStyle as ViewStyle}
-      renderToHardwareTextureAndroid
-    >
+    <AnimatedBox style={animatedStyle as ViewStyle} pointerEvents="none">
       <WebView
-        androidLayerType="hardware"
-        style={{ opacity: 0.99 }}
+        style={{ flex: 1 }}
         scrollEnabled={false}
         scalesPageToFit={false}
         setDisplayZoomControls={false}
         nestedScrollEnabled={false}
         setBuiltInZoomControls={false}
-        useWebView2
         pointerEvents="none"
         ref={ref}
         onMessage={onMessage}
@@ -37,15 +32,25 @@ const WebViewImageElement: React.ForwardRefRenderFunction<
            window.ReactNativeWebView.postMessage(JSON.stringify({ type: "load", width: img.naturalWidth, height: img.naturalHeight }));
          })
        `}
+        startInLoadingState
+        androidLayerType="software"
         source={{
           html: `
       <html>
         <head>
           <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0">
         </head>
-        <body style="margin: 0;background-color: ${backgroundColor};">
-          <img src="${uri}" id="page" style="pointer-events: none;object-fit: contain;width: ${style.width}px;height: ${style.height}px"  />
-        </body>
+        <style>
+          body {
+            margin: 0;
+            background-color: ${backgroundColor};
+            width: ${style.width}px;
+            height: ${style.height}px;
+            background-image: url(${uri});
+            background-size: contain;
+          }
+        </style>
+        <body></body>
       </html>
    `,
         }}
