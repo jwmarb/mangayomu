250f246d16834941f0a75ec0250db45cc952e21b
Joseph Marbella <dieptuber@gmail.com>
2023-08-02 15:17:34 -0700

    refactor: experimental use Flashlist

--- apps/mobile/src/screens/Reader/components/PageList/PageList.tsx
+++ apps/mobile/src/screens/Reader/components/PageList/PageList.tsx
@@ -28,45 +28,56 @@ const PageList: React.ForwardRefRenderFunction<
   const handleOnActivated = () => {
     pageGestures.current[currentPageKey.current].onFlashlistActive();
   };
-  return (
-    <FlatList
-      // scrollEnabled={false}
-      simultaneousHandlers={panRef}
-      onActivated={
-        readingDirection !== ReadingDirection.WEBTOON
-          ? handleOnActivated
-          : undefined
-      }
-      ref={ref as React.ForwardedRef<FlatList<Page>>}
-      {...(rest as FlatListProps<Page>)}
-      initialScrollIndex={
-        initialScrollIndex != null
-          ? estimatedFirstItemOffset !== 0
-            ? initialScrollIndex + 1
-            : initialScrollIndex
-          : undefined
-      }
-      extraData={extraData}
-      renderItem={
-        preDeterminedRenderItem != null
-          ? (info) => {
-              (info as unknown as FlashListRenderItemInfo<Page>).extraData =
-                extraData;
-              return preDeterminedRenderItem(
-                info as unknown as FlashListRenderItemInfo<Page>,
-              );
-            }
-          : null
-      }
-    />
-  );
-  // const renderScrollComponent = React.useMemo(
-  //   () =>
-  //     React.forwardRef<ScrollView>((props, ref) => (
-  //       <ScrollView ref={ref} {...props} simultaneousHandlers={panRef} />
-  //     )),
-  //   [],
-  // ) as unknown as React.FC<ScrollViewProps>;
+  // return (
+  //   <FlatList
+  //     // scrollEnabled={false}
+  //     simultaneousHandlers={panRef}
+  //     onActivated={
+  //       readingDirection !== ReadingDirection.WEBTOON
+  //         ? handleOnActivated
+  //         : undefined
+  //     }
+  //     ref={ref as React.ForwardedRef<FlatList<Page>>}
+  //     {...(rest as FlatListProps<Page>)}
+  //     initialScrollIndex={
+  //       initialScrollIndex != null
+  //         ? estimatedFirstItemOffset !== 0
+  //           ? initialScrollIndex + 1
+  //           : initialScrollIndex
+  //         : undefined
+  //     }
+  //     extraData={extraData}
+  //     renderItem={
+  //       preDeterminedRenderItem != null
+  //         ? (info) => {
+  //             (info as unknown as FlashListRenderItemInfo<Page>).extraData =
+  //               extraData;
+  //             return preDeterminedRenderItem(
+  //               info as unknown as FlashListRenderItemInfo<Page>,
+  //             );
+  //           }
+  //         : null
+  //     }
+  //   />
+  // );
+  const renderScrollComponent = React.useMemo(
+    () =>
+      React.forwardRef<ScrollView>((props, ref) => (
+        <ScrollView
+          ref={ref}
+          {...props}
+          simultaneousHandlers={panRef}
+          onActivated={
+            readingDirection !== ReadingDirection.WEBTOON
+              ? handleOnActivated
+              : undefined
+          }
+          overScrollMode="never"
+        />
+      )),
+    [readingDirection !== ReadingDirection.WEBTOON],
+  ) as unknown as React.FC<ScrollViewProps>;
+
   // switch (readingDirection) {
   //   default:
   //     return (
@@ -98,17 +109,17 @@ const PageList: React.ForwardRefRenderFunction<
   //       />
   //     );
   // case ReadingDirection.WEBTOON:
-  //   return (
-  //     <FlashList
-  //       renderScrollComponent={renderScrollComponent}
-  //       ref={ref as React.ForwardedRef<FlashList<Page>>}
-  //       {...rest}
-  //       estimatedFirstItemOffset={estimatedFirstItemOffset}
-  //       initialScrollIndex={initialScrollIndex}
-  //       renderItem={preDeterminedRenderItem}
-  //       extraData={extraData}
-  //     />
-  //   );
+  return (
+    <FlashList
+      renderScrollComponent={renderScrollComponent}
+      ref={ref as React.ForwardedRef<FlashList<Page>>}
+      {...rest}
+      estimatedFirstItemOffset={estimatedFirstItemOffset}
+      initialScrollIndex={initialScrollIndex}
+      renderItem={preDeterminedRenderItem}
+      extraData={extraData}
+    />
+  );
   // }
 };
 
