633fafefa577e2e3b3e64fb7d117b2ed99060a45
Joseph Marbella <dieptuber@gmail.com>
2023-08-04 23:27:56 -0700

    fix: wrong assertion

--- apps/mobile/src/helpers/mangaSchemaToManga.ts
+++ apps/mobile/src/helpers/mangaSchemaToManga.ts
@@ -1,12 +1,20 @@
 import { LocalMangaSchema } from '@database/schemas/LocalManga';
 import { MangaSchema } from '@database/schemas/Manga';
-import assertIsManga from '@helpers/assertIsManga';
+import assertIsLocalMangaSchema from '@helpers/assertIsLocalMangaSchema';
+import assertIsMangaSchema from '@helpers/assertIsMangaSchema';
 import { Manga } from '@mangayomu/mangascraper/src';
 
 export default function mangaSchemaToManga(
   manga: Manga | MangaSchema | LocalMangaSchema,
 ): Manga {
-  if (!assertIsManga(manga))
+  if (assertIsMangaSchema(manga))
+    return {
+      imageCover: manga.imageCover,
+      link: manga.link,
+      title: manga.title,
+      source: manga.source,
+    };
+  else if (assertIsLocalMangaSchema(manga))
     return {
       imageCover: manga.imageCover,
       link: manga._id,
