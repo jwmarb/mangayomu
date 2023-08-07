8a99ae72ee31abd9b740388fb3804fed1082df9b
Joseph Marbella <dieptuber@gmail.com>
2023-08-07 13:05:55 -0700

    feat: override URL option

--- apps/nextjs/app/helpers/getMangaMeta.ts
+++ apps/nextjs/app/helpers/getMangaMeta.ts
@@ -2,8 +2,9 @@ import type { Manga, MangaChapter, MangaMeta } from '@mangayomu/mangascraper';
 
 export default async function getMangaMeta(
   manga: Manga,
+  overrideUrl?: string,
 ): Promise<Manga & MangaMeta<MangaChapter>> {
-  const res = await fetch('/api/v1/manga', {
+  const res = await fetch(overrideUrl ?? '/api/v1/manga', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(manga),
