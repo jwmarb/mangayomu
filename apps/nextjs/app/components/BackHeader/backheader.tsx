aca4ebd370efb7ae8152acc4066cea6ef47b1cf8
Joseph Marbella <dieptuber@gmail.com>
2023-08-07 19:29:54 -0700

    feat: add ability to add subheader

--- apps/nextjs/app/components/BackHeader/backheader.tsx
+++ apps/nextjs/app/components/BackHeader/backheader.tsx
@@ -6,17 +6,36 @@ import { useRouter } from 'next/navigation';
 import React from 'react';
 import { MdArrowBack } from 'react-icons/md';
 
-interface BackHeader {
+interface BackHeader extends React.PropsWithChildren {
   title?: React.ReactNode;
   headerRight?: React.ReactNode;
 }
 
 export default function BackHeader(props: BackHeader) {
-  const { title, headerRight } = props;
+  const { title, headerRight, children } = props;
   const router = useRouter();
   const onBack = () => {
     router.back();
   };
+  const count = React.Children.count(children);
+  if (count > 0)
+    return (
+      <Screen.Header className="z-20 pb-2 gap-2 flex flex-col">
+        <div className="flex flex-row gap-2 items-center justify-between">
+          <div className="flex flex-row gap-2 items-center">
+            <IconButton onPress={onBack} icon={<MdArrowBack />} />
+            {typeof title === 'string' ? (
+              <Text className="font-medium">{title}</Text>
+            ) : (
+              title
+            )}
+          </div>
+          {headerRight}
+        </div>
+
+        {children}
+      </Screen.Header>
+    );
   return (
     <Screen.Header className="z-20 pb-2 flex flex-row gap-2 items-center justify-between">
       <div className="flex flex-row gap-2 items-center">
