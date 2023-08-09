5881be39dbaeed811dc4cc5d7c6adb1a83d44e94
Joseph Marbella <dieptuber@gmail.com>
2023-08-09 03:29:49 -0700

    refactor: resolve import from `@mangayomu/schemas`

--- apps/nextjs/app/realm/SourceManga/index.ts
+++ apps/nextjs/app/realm/SourceManga/index.ts
@@ -1,9 +1,5 @@
 import Collection from '@app/realm/collection';
-import { Manga } from '@mangayomu/mangascraper';
-
-export interface ISourceMangaSchema extends Manga {
-  _id: string;
-}
+import { ISourceMangaSchema } from '@mangayomu/schemas';
 
 export default class SourceMangaSchema extends Collection<ISourceMangaSchema>({
   name: 'SourceManga',
