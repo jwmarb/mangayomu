2af253321b5dea9869bb065b15d3801d3ef617ae
Joseph Marbella <dieptuber@gmail.com>
2023-08-11 01:25:40 -0700

    feat: add new helpers

--- apps/backend/src/helpers/index.ts
+++ apps/backend/src/helpers/index.ts
@@ -1,3 +1,5 @@
 export { default as getErrorMessage } from './getErrorMessage';
 export { default as slugify } from './slugify';
 export { default as getListMangas } from './getListMangas';
+export { default as getSourceMangaId } from './getSourceMangaId';
+export { default as getSourceChapterId } from './getSourceChapterId';
