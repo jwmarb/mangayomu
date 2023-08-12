e5cc83f7a00ee139b1d7583bb731b8eb98838a97
Joseph Marbella <dieptuber@gmail.com>
2023-08-12 00:14:02 -0700

    refactor: include . in slug

--- apps/backend/src/helpers/slugify.ts
+++ apps/backend/src/helpers/slugify.ts
@@ -2,7 +2,7 @@ export default function slugify(str: string) {
   return str
     .toLowerCase()
     .trim()
-    .replace(/[^\w\s-]/g, '')
+    .replace(/[^\w\s.-]/g, '')
     .replace(/[\s_-]+/g, '-')
     .replace(/^-+|-+$/g, '');
 }
--- apps/nextjs/app/helpers/getSlug.ts
+++ apps/nextjs/app/helpers/getSlug.ts
@@ -2,7 +2,7 @@ export default function getSlug(title: string) {
   return title
     .toLowerCase()
     .trim()
-    .replace(/[^\w\s-]/g, '')
+    .replace(/[^\w\s.-]/g, '')
     .replace(/[\s_-]+/g, '-')
     .replace(/^-+|-+$/g, '');
 }
