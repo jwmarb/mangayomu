1c56f4efba6a3d0a51916e357929ce5fb7ecafa9
Joseph Marbella <dieptuber@gmail.com>
2023-08-21 22:55:19 -0700

    fix: extremely large image overlapping

--- apps/nextjs/app/(reader)/[source]/[title]/[chapter]/components/chapterpage.tsx
+++ apps/nextjs/app/(reader)/[source]/[title]/[chapter]/components/chapterpage.tsx
@@ -6,7 +6,7 @@ interface ChapterPageProps {
 
 function ChapterPage(props: ChapterPageProps) {
   const { page } = props;
-  return <img src={page} className="object-contain" />;
+  return <img src={page} className="object-contain max-h-[100vh]" />;
 }
 
 export default React.memo(ChapterPage);
