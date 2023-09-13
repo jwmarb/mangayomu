37799b9579f7cae03aedd86d798bf2ff3399c982
Joseph Marbella <dieptuber@gmail.com>
2023-09-13 16:04:19 -0700

    feat: add reader settings

--- apps/nextjs/app/context/readersettings.tsx
+++ apps/nextjs/app/context/readersettings.tsx
@@ -1,4 +1,8 @@
-import { ReadingDirection } from '@mangayomu/schemas';
+import {
+  ImageScaling,
+  ReadingDirection,
+  ZoomStartPosition,
+} from '@mangayomu/schemas';
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
@@ -9,19 +13,27 @@ export enum BackgroundColor {
 }
 
 export interface ReaderSettings {
+  zoomStartPosition: ZoomStartPosition;
+  imageScaling: ImageScaling;
   backgroundColor: BackgroundColor;
   readingDirection: ReadingDirection;
   setBackgroundColor: (bg: BackgroundColor) => void;
   setReadingDirection: (rd: ReadingDirection) => void;
+  setImageScaling: (scale: ImageScaling) => void;
+  setZoomStartPosition: (zsp: ZoomStartPosition) => void;
 }
 
 export const useReaderSettings = create(
   persist<ReaderSettings>(
     (set) => ({
+      zoomStartPosition: ZoomStartPosition.AUTOMATIC,
+      imageScaling: ImageScaling.SMART_FIT,
       backgroundColor: BackgroundColor.WHITE,
       readingDirection: ReadingDirection.RIGHT_TO_LEFT,
       setReadingDirection: (rd) => set({ readingDirection: rd }),
       setBackgroundColor: (bg) => set({ backgroundColor: bg }),
+      setZoomStartPosition: (zsp) => set({ zoomStartPosition: zsp }),
+      setImageScaling: (scale) => set({ imageScaling: scale }),
     }),
     {
       name: 'reader',
