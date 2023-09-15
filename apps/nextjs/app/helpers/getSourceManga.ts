2eb8b4faa84e51f58bb1e8e3003c11492e03562b
Joseph Marbella <dieptuber@gmail.com>
2023-09-15 06:59:52 -0700

    fix: calling mongodb before connection

--- apps/nextjs/app/helpers/getSourceManga.ts
+++ apps/nextjs/app/helpers/getSourceManga.ts
@@ -2,21 +2,24 @@ import React from 'react';
 import { redis, SourceManga, getMongooseConnection } from '@mangayomu/backend';
 import { ISourceMangaSchema } from '@mangayomu/schemas';
 const getSourceManga = React.cache(
-  async (pathName: string): Promise<ISourceMangaSchema | null> => {
-    const cached = await redis.get(pathName);
-    if (cached == null) {
-      const { connect, close } = getMongooseConnection();
-      await connect();
-      const value = await SourceManga.findById(pathName);
-      if (value == null) return null;
-      await Promise.all([
-        redis.setex(pathName, 300, JSON.stringify(value.toJSON())),
-        close,
-      ]);
-      return value.toObject();
-    }
-
-    return JSON.parse(cached);
+  (pathName: string): Promise<ISourceMangaSchema | null> => {
+    return new Promise((res, rej) => {
+      redis.get(pathName).then((cached) => {
+        if (cached == null) {
+          const { connect, close } = getMongooseConnection();
+          connect().then(() => {
+            SourceManga.findById(pathName).then((value) => {
+              if (value == null) rej();
+              else
+                Promise.all([
+                  redis.setex(pathName, 300, JSON.stringify(value.toJSON())),
+                  close,
+                ]).then(() => res(value.toObject()));
+            });
+          });
+        } else res(JSON.parse(cached));
+      });
+    });
   },
 );
 
