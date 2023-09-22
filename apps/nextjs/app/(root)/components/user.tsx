6a26173fedab6eddbb9d760c0fbce1b89227d92e
Joseph Marbella <dieptuber@gmail.com>
2023-09-21 23:53:42 -0700

    feat: add login/register navigation

--- apps/nextjs/app/(root)/components/user.tsx
+++ apps/nextjs/app/(root)/components/user.tsx
@@ -1,8 +1,10 @@
+import Button from '@app/components/Button';
 import DarkMode from '@app/components/DarkMode';
 import IconButton from '@app/components/IconButton';
 import Text from '@app/components/Text';
 import { useUser } from '@app/context/realm';
 import { useSafeArea } from '@app/context/safearea';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 import { MdChevronLeft, MdPerson, MdSettings } from 'react-icons/md';
@@ -21,19 +23,38 @@ export default function User(props: UserProps) {
     router.push('/settings');
   };
   return (
-    <div className="flex flex-row justify-between gap-2">
-      <div className="flex flex-row gap-2 items-center">
-        <div className="w-10 h-10 bg-default rounded-lg items-center justify-center flex">
-          <MdPerson className="text-text-primary" />
+    <div className="flex flex-col gap-1 items-center">
+      {user.profile.name == null && (
+        <div className="flex flex-row gap-1">
+          <Link href="/login">
+            <Text color="primary" variant="sm-label">
+              Login
+            </Text>
+          </Link>
+          <Text variant="sm-label" color="text-secondary">
+            or
+          </Text>
+          <Link href="/register">
+            <Text color="primary" variant="sm-label">
+              Create an account
+            </Text>
+          </Link>
         </div>
-        <Text className="break-all">{user.profile.name ?? 'Guest'}</Text>
-      </div>
-      <div className="flex flex-row">
-        {/* <DarkMode /> */}
-        <IconButton icon={<MdSettings />} onPress={onSettings} />
-        {mobile && (
-          <IconButton icon={<MdChevronLeft />} onPress={onDrawerClose} />
-        )}
+      )}
+      <div className="flex flex-row justify-between gap-2 w-full">
+        <div className="flex flex-row gap-2 items-center">
+          <div className="w-10 h-10 bg-default rounded-lg items-center justify-center flex">
+            <MdPerson className="text-text-primary" />
+          </div>
+          <Text className="break-all">{user.profile.name ?? 'Guest'}</Text>
+        </div>
+        <div className="flex flex-row">
+          {/* <DarkMode /> */}
+          <IconButton icon={<MdSettings />} onPress={onSettings} />
+          {mobile && (
+            <IconButton icon={<MdChevronLeft />} onPress={onDrawerClose} />
+          )}
+        </div>
       </div>
     </div>
   );
