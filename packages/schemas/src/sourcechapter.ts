796b1f259baa2d2acdd3e4bef449bead493a51d6
Joseph Marbella <dieptuber@gmail.com>
2023-09-24 16:53:29 -0700

    feat: reflect changes from db to typedef

--- packages/schemas/src/sourcechapter.ts
+++ packages/schemas/src/sourcechapter.ts
@@ -1,8 +1,10 @@
 import { ISOLangCode } from '@mangayomu/language-codes';
 
 export interface ISourceChapterSchema {
-  _id: string;
-  _mangaId: string;
+  _id: string; // slugified url
+  _mangaId: string; // normal url to manga
+  _nextId: string | null; // previous chapter's _id
+  _prevId: string | null; // next chapter's _id
   language: ISOLangCode;
   name: string;
   link: string;
