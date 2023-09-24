fdaa8116ad1de4244ae24602c9b61fcbf11f35a9
Joseph Marbella <dieptuber@gmail.com>
2023-09-23 23:31:31 -0700

    feat: add hook to access manga setting

---
+++ apps/nextjs/app/(reader)/[source]/[title]/[chapter]/hooks/useMangaSetting.ts
@@ -0,0 +1,43 @@
+import { useManga } from '@app/(reader)/[source]/[title]/[chapter]/context/MangaContext';
+import { useUser } from '@app/context/realm';
+import useObject from '@app/hooks/useObject';
+import MangaSchema from '@app/realm/Manga';
+import { IMangaSchema } from '@mangayomu/schemas';
+import React from 'react';
+
+export default function useMangaSetting<
+  T extends
+    | 'readerDirection'
+    | 'readerImageScaling'
+    | 'readerZoomStartPosition',
+>(
+  setting: T,
+  options: Record<string, Exclude<IMangaSchema[T], 'Use global setting'>>,
+): [
+  IMangaSchema[T],
+  (val: IMangaSchema[T]) => void,
+  Record<string, IMangaSchema[T]>,
+] {
+  const manga = useManga();
+  const userManga = useObject(MangaSchema, { link: manga.link });
+  const setSetting = React.useCallback(
+    (val: IMangaSchema[T]) => {
+      userManga.update(
+        (draft) => {
+          draft[setting] = val;
+        },
+        { upsert: true },
+      );
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [setting],
+  );
+  return [
+    userManga[setting] as IMangaSchema[T],
+    setSetting,
+    {
+      ...options,
+      'Use global setting': 'Use global setting',
+    },
+  ];
+}
