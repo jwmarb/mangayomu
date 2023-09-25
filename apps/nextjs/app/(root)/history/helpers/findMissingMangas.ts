c030bc5aba414f3a561ce04eb6c4b3ec5872588e
Joseph Marbella <dieptuber@gmail.com>
2023-09-25 08:16:06 -0700

    refactor: map links in server instead of client

--- apps/nextjs/app/(root)/history/helpers/findMissingMangas.ts
+++ apps/nextjs/app/(root)/history/helpers/findMissingMangas.ts
@@ -5,6 +5,14 @@ import {
   IUserHistorySchema,
 } from '@mangayomu/schemas';
 
+type AggregationResult<T> = [
+  | {
+      links: string[];
+      result: T[];
+    }
+  | undefined,
+];
+
 export default async function findMissingMangas(
   userHistory: IUserHistorySchema[],
   sourceMangas: ReturnType<typeof useMongoClient<ISourceMangaSchema>>,
@@ -13,15 +21,27 @@ export default async function findMissingMangas(
   /**
    * Get mangas from DB
    */
-  const [sourceMangasResult, sourceChaptersResult]: [
-    ISourceMangaSchema[],
-    ISourceChapterSchema[],
+
+  const [[sourceMangasAggregationResult], [sourceChaptersAggregationResult]]: [
+    AggregationResult<ISourceMangaSchema>,
+    AggregationResult<ISourceChapterSchema>,
   ] = await Promise.all([
     sourceMangas.aggregate([
       {
         $match: {
           link: {
             $in: Array.from(new Set(userHistory.map((x) => x.manga))),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          links: {
+            $push: '$link',
+          },
+          result: {
+            $push: '$$ROOT',
           },
         },
       },
@@ -34,19 +54,37 @@ export default async function findMissingMangas(
           },
         },
       },
+      {
+        $group: {
+          _id: null,
+          links: {
+            $push: '$link',
+          },
+          result: {
+            $push: '$$ROOT',
+          },
+        },
+      },
     ]),
   ]);
+
+  const sourceMangaLinks = sourceMangasAggregationResult?.links ?? [];
+  const sourceMangasResult = sourceMangasAggregationResult?.result ?? [];
+  const sourceChaptersResult = sourceChaptersAggregationResult?.result ?? [];
+  const sourceChaptersLinks = sourceChaptersAggregationResult?.links ?? [];
+
+  // { links: sourceMangaLinks, result: sourceMangasResult }
 
   /**
    * Filter out mangas that the DB found. This gives the missing mangas from DB.
    */
-  const sourceMangaLookup = new Set(sourceMangasResult.map((x) => x.link));
-  const sourceChapterLookup = new Set(sourceChaptersResult.map((x) => x.link));
-  const missingSourceMangas = Array.from(
-    new Set(userHistory.filter((x) => !sourceMangaLookup.has(x.manga))),
+  const sourceMangaLookup = new Set(sourceMangaLinks);
+  const sourceChapterLookup = new Set(sourceChaptersLinks);
+  const missingSourceMangas = userHistory.filter(
+    (x) => !sourceMangaLookup.has(x.manga),
   );
-  const missingSourceChapters = Array.from(
-    new Set(userHistory.filter((x) => !sourceChapterLookup.has(x.chapter))),
+  const missingSourceChapters = userHistory.filter(
+    (x) => !sourceChapterLookup.has(x.chapter),
   );
   return {
     found: [sourceMangasResult, sourceChaptersResult] as const,
