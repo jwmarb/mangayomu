9a5a30e599cc68c6cb29a0388e85644202fefbe1
Joseph Marbella <dieptuber@gmail.com>
2023-09-29 12:23:52 -0700

    feat: add loading indicator

--- apps/nextjs/app/(root)/[source]/[title]/[chapter]/loading.tsx
+++ apps/nextjs/app/(root)/[source]/[title]/[chapter]/loading.tsx
@@ -1,7 +1,16 @@
 'use client';
+import Progress from '@app/components/Progress';
+import Text from '@app/components/Text';
 import { useReaderSettings } from '@app/context/readersettings';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 
 export default function Loading() {
   const background = useReaderSettings((s) => s.backgroundColor);
-  return <div className={`${background} w-[100vw] h-[100vh]`} />;
+  return (
+    <div
+      className={`${background} w-[100vw] h-[100vh] flex flex-col items-center justify-center`}
+    >
+      <Progress size={6} />
+    </div>
+  );
 }
