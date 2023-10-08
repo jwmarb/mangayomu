e1b6889b79cf10d317a17b98543ca26929245b63
Joseph Marbella <dieptuber@gmail.com>
2023-10-08 11:32:35 -0700

    feat: add image scaling to pages

--- apps/nextjs/app/(root)/[source]/[title]/[chapter]/components/chapterpage.tsx
+++ apps/nextjs/app/(root)/[source]/[title]/[chapter]/components/chapterpage.tsx
@@ -1,3 +1,6 @@
+import { useManga } from '@app/(root)/[source]/[title]/[chapter]/context/MangaContext';
+import useReaderSetting from '@app/(root)/[source]/[title]/[chapter]/hooks/useReaderSetting';
+import { ImageScaling } from '@mangayomu/schemas';
 import React from 'react';
 
 interface ChapterPageProps {
@@ -6,7 +9,24 @@ interface ChapterPageProps {
 
 function ChapterPage(props: ChapterPageProps) {
   const { page } = props;
-  return <img src={page} className="object-contain max-h-[100vh]" />;
+  const manga = useManga();
+  const scaling = useReaderSetting('readerImageScaling', manga);
+
+  switch (scaling) {
+    case ImageScaling.FIT_SCREEN:
+      return <img src={page} className="object-contain h-[100vh] w-[100vw]" />;
+    case ImageScaling.FIT_HEIGHT:
+      return <img src={page} className="object-contain min-h-[100vh]" />;
+    case ImageScaling.FIT_WIDTH:
+      return <img src={page} className="object-contain min-w-[100vw]" />;
+    case ImageScaling.SMART_FIT:
+      return (
+        <img
+          src={page}
+          className="object-contain min-h-[100vh] min-w-[100vw]"
+        />
+      );
+  }
 }
 
 export default React.memo(ChapterPage);
