5a6319943bd1332b711c2e944bfd3a16ba2f2c63
Joseph Marbella <dieptuber@gmail.com>
2023-10-08 09:59:08 -0700

    feat: allow icon color to be overridden

--- apps/nextjs/app/components/IconButton/index.ts
+++ apps/nextjs/app/components/IconButton/index.ts
@@ -1,11 +1,11 @@
 import IconButton from './IconButton';
 export default IconButton;
-import type { ButtonColor } from '@app/theme';
+import type { ButtonColor, TextColor } from '@app/theme';
 import type { OverrideClassName } from '@app/hooks/useClassName';
 import type { AriaButtonProps } from 'react-aria';
 export interface IconButtonProps
   extends AriaProps<HTMLButtonElement, AriaButtonProps>,
     OverrideClassName {
-  color?: ButtonColor;
+  color?: ButtonColor | TextColor;
   icon: React.ReactElement;
 }
