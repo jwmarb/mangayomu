737a94cca77d65f243155c35abead1951abdd00a
Joseph Marbella <dieptuber@gmail.com>
2023-10-23 20:33:59 -0700

    todo: adjust pages based on reader direction

--- apps/nextjs/app/(root)/[source]/[title]/[chapter]/components/chapterpage.tsx
+++ apps/nextjs/app/(root)/[source]/[title]/[chapter]/components/chapterpage.tsx
@@ -1,6 +1,6 @@
 import { useManga } from '@app/(root)/[source]/[title]/[chapter]/context/MangaContext';
 import useReaderSetting from '@app/(root)/[source]/[title]/[chapter]/hooks/useReaderSetting';
-import { ImageScaling } from '@mangayomu/schemas';
+import { ImageScaling, ReadingDirection } from '@mangayomu/schemas';
 import React from 'react';
 
 interface ChapterPageProps {
@@ -11,22 +11,27 @@ function ChapterPage(props: ChapterPageProps) {
   const { page } = props;
   const manga = useManga();
   const scaling = useReaderSetting('readerImageScaling', manga);
+  const readingDirection = useReaderSetting('readerDirection', manga);
 
-  switch (scaling) {
-    case ImageScaling.FIT_SCREEN:
-      return <img src={page} className="object-contain h-[100vh] w-[100vw]" />;
-    case ImageScaling.FIT_HEIGHT:
-      return <img src={page} className="object-contain min-h-[100vh]" />;
-    case ImageScaling.FIT_WIDTH:
-      return <img src={page} className="object-contain min-w-[100vw]" />;
-    case ImageScaling.SMART_FIT:
-      return (
-        <img
-          src={page}
-          className="object-contain min-h-[100vh] min-w-[100vw]"
-        />
-      );
-  }
+  if (readingDirection !== ReadingDirection.WEBTOON)
+    switch (scaling) {
+      case ImageScaling.FIT_SCREEN:
+        return (
+          <img src={page} className="object-contain h-[100vh] w-[100vw]" />
+        );
+      case ImageScaling.FIT_HEIGHT:
+        return <img src={page} className="object-contain min-h-[100vh]" />;
+      case ImageScaling.FIT_WIDTH:
+        return <img src={page} className="object-contain min-w-[100vw]" />;
+      case ImageScaling.SMART_FIT:
+        return (
+          <img
+            src={page}
+            className="object-contain min-h-[100vh] min-w-[100vw]"
+          />
+        );
+    }
+  return <img src={page} className="w-[100vw]" />;
 }
 
 export default React.memo(ChapterPage);
