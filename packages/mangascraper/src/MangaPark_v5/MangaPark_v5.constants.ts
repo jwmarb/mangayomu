92c56103a6952d6a07ca3d87781ac1be835f9e58
Joseph Marbella <dieptuber@gmail.com>
2023-12-04 16:15:58 -0700

    chore: bump version

--- packages/mangascraper/src/MangaPark_v5/MangaPark_v5.constants.ts
+++ packages/mangascraper/src/MangaPark_v5/MangaPark_v5.constants.ts
@@ -348,7 +348,7 @@ export const MANGAPARKV5_INFO: MangaHostFiltersInfo<MangaParkV5Filter> = {
   hasLatestMangas: true,
   hasMangaDirectory: false,
   host: 'https://mangapark.net/',
-  version: '2.0.1',
+  version: '2.0.2',
   name: 'MangaPark v3',
   icon: 'https://mangapark.io/client-script/img/favicon.gif',
 };
