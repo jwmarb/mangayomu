e7989467c7ab079402e8ebc9c6c287d6a7983cd7
Joseph Marbella <dieptuber@gmail.com>
2023-12-26 23:07:05 -0700

    refactor: set to fixed dimensions

--- apps/nextjs/app/(root)/[source]/[title]/[chapter]/components/chapterpage.tsx
+++ apps/nextjs/app/(root)/[source]/[title]/[chapter]/components/chapterpage.tsx
@@ -20,15 +20,12 @@ function ChapterPage(props: ChapterPageProps) {
           <img src={page} className="object-contain h-[100vh] w-[100vw]" />
         );
       case ImageScaling.FIT_HEIGHT:
-        return <img src={page} className="object-contain min-h-[100vh]" />;
+        return <img src={page} className="object-contain h-[100vh]" />;
       case ImageScaling.FIT_WIDTH:
-        return <img src={page} className="object-contain min-w-[100vw]" />;
+        return <img src={page} className="object-contain w-[100vw]" />;
       case ImageScaling.SMART_FIT:
         return (
-          <img
-            src={page}
-            className="object-contain min-h-[100vh] min-w-[100vw]"
-          />
+          <img src={page} className="object-contain h-[100vh] w-[100vw]" />
         );
     }
   return <img src={page} className="w-[100vw]" />;
