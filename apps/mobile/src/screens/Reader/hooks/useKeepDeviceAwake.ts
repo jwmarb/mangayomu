0a87ded5adf03b835512ce32134aaa39c11b89d8
Joseph Marbella <dieptuber@gmail.com>
2024-01-09 16:05:35 -0700

    fix: keeping device awake not working

--- apps/mobile/src/screens/Reader/hooks/useKeepDeviceAwake.ts
+++ apps/mobile/src/screens/Reader/hooks/useKeepDeviceAwake.ts
@@ -1,17 +1,19 @@
 import useAppSelector from '@hooks/useAppSelector';
-import { useWakeLock } from 'react-screen-wake-lock';
+import {
+  activateKeepAwake,
+  deactivateKeepAwake,
+} from '@sayem314/react-native-keep-awake';
 import React from 'react';
 
 export default function useKeepDeviceAwake() {
-  const { request, release, released } = useWakeLock();
   const keepDeviceAwake = useAppSelector(
     (state) => state.settings.reader.keepDeviceAwake,
   );
   React.useEffect(() => {
     if (keepDeviceAwake) {
-      if (released) request();
+      activateKeepAwake();
       return () => {
-        if (!released) release();
+        deactivateKeepAwake();
       };
     }
   }, [keepDeviceAwake]);
