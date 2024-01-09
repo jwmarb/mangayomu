e33327bbc2a30e7087804c95baa6cbbd1cf207cb
Joseph Marbella <dieptuber@gmail.com>
2024-01-08 17:50:49 -0700

    feat: create `Pressable` component

---
+++ apps/mobile/src/components/Pressable/Pressable.tsx
@@ -0,0 +1,51 @@
+import { useTheme } from '@emotion/react';
+import { ButtonColors } from '@mangayomu/theme';
+import React from 'react';
+import {
+  PressableAndroidRippleConfig,
+  Pressable as NativePressable,
+  PressableProps as NativePressableProps,
+} from 'react-native';
+
+interface PressableProps extends NativePressableProps {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  color?: ButtonColors | (string & {});
+  ripple?: boolean;
+  borderless?: boolean;
+  foreground?: boolean;
+  rippleRadius?: number | null;
+}
+
+/**
+ * MangaYomu theme-wrapped `Pressable` from React Native
+ * @component
+ */
+function Pressable(
+  props: PressableProps,
+  ref: React.ForwardedRef<React.ElementRef<typeof NativePressable>>,
+) {
+  const {
+    ripple = true,
+    borderless = false,
+    foreground = false,
+    rippleRadius,
+  } = props;
+  const theme = useTheme();
+  const android_ripple: PressableAndroidRippleConfig | null = ripple
+    ? {
+        color: props.color
+          ? props.color in theme.palette
+            ? theme.palette[props.color as ButtonColors].ripple
+            : props.color
+          : theme.palette.action.ripple,
+        borderless,
+        foreground,
+        radius: rippleRadius,
+      }
+    : null;
+  return (
+    <NativePressable {...props} ref={ref} android_ripple={android_ripple} />
+  );
+}
+
+export default React.forwardRef(Pressable);
---
+++ apps/mobile/src/components/Pressable/index.ts
@@ -0,0 +1,4 @@
+import Animated from 'react-native-reanimated';
+import Pressable from './Pressable';
+export default Pressable;
+export const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
