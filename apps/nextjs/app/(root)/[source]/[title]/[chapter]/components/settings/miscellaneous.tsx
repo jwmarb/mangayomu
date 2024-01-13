a3a635ecd83be547a38d2f6644fb5e1e9b7f00f1
Joseph Marbella <dieptuber@gmail.com>
2024-01-12 23:39:46 -0700

    feat: add misc settings

--- apps/nextjs/app/(root)/[source]/[title]/[chapter]/components/settings/miscellaneous.tsx
+++ apps/nextjs/app/(root)/[source]/[title]/[chapter]/components/settings/miscellaneous.tsx
@@ -1,10 +1,16 @@
+import BackgroundColor from '@app/(root)/settings/components/reader_components/BackgroundColor';
+import ReadingDirection from '@app/(root)/settings/components/reader_components/ReadingDirection';
 import Text from '@app/components/Text';
 
 export default function Miscellaneous() {
   return (
-    <div className="py-2 px-4">
-      <Text variant="header">Miscellaneous</Text>
-      <Text color="text-secondary">Customize functionality and features</Text>
+    <div className="py-2 bg-default flex flex-col gap-2">
+      <div className="mx-4">
+        <Text variant="header">Miscellaneous</Text>
+        <Text color="text-secondary">Customize functionality and features</Text>
+      </div>
+      <BackgroundColor />
+      <ReadingDirection />
     </div>
   );
 }
