3925f21aef8030b76e8b1ab1c8311999ba352bb3
Joseph Marbella <dieptuber@gmail.com>
2024-01-26 10:13:20 -0700

    refactor: improve readability

--- apps/mobile/src/components/Button/Button.tsx
+++ apps/mobile/src/components/Button/Button.tsx
@@ -7,6 +7,10 @@ import Stack from '@components/Stack';
 import Box from '@components/Box/Box';
 import Pressable from '@components/Pressable';
 
+const SPACE = moderateScale(4);
+const PY = moderateScale(12);
+const PX = moderateScale(16);
+
 const Button: React.FC<ButtonProps> = (props) => {
   const {
     label,
@@ -20,73 +24,55 @@ const Button: React.FC<ButtonProps> = (props) => {
     ...rest
   } = props;
   const theme = useTheme();
+  const borderColor =
+    variant === 'outline' ? (disabled ? 'disabled' : color) : 'transparent';
+
+  const textColor = disabled
+    ? 'disabled'
+    : variant === 'contained'
+    ? theme.helpers.getContrastText(theme.palette[color].main)
+    : color;
+
+  const containerStyle = {
+    borderRadius: sharp ? undefined : theme.style.borderRadius,
+    backgroundColor:
+      variant === 'contained'
+        ? disabled
+          ? theme.palette.text.disabled
+          : theme.palette[color].main
+        : undefined,
+  };
+
+  const boxStyle = [style, containerStyle];
+
   return (
-    <Box
-      overflow="hidden"
-      style={[
-        style,
-        {
-          borderRadius: sharp ? undefined : theme.style.borderRadius,
-          backgroundColor:
-            variant === 'contained'
-              ? disabled
-                ? theme.palette.text.disabled
-                : theme.palette[color].main
-              : undefined,
-        },
-      ]}
-    >
+    <Box overflow="hidden" style={boxStyle}>
       <Pressable disabled={disabled} color={color} {...rest}>
         <Stack
-          space={moderateScale(4)}
-          py={moderateScale(12)}
-          px={moderateScale(16)}
+          space={SPACE}
+          py={PY}
+          px={PX}
           flex-direction="row"
           align-items="center"
           justify-content="center"
           border-width="@theme"
           border-radius="@theme"
-          border-color={
-            variant === 'outline'
-              ? disabled
-                ? 'disabled'
-                : color
-              : 'transparent'
-          }
+          border-color={borderColor}
         >
           {iconPlacement === 'left' &&
             icon &&
             React.cloneElement(icon, {
-              color: disabled
-                ? 'disabled'
-                : variant === 'contained'
-                ? theme.helpers.getContrastText(theme.palette[color].main)
-                : color,
+              color: textColor,
             })}
           {label && (
-            <Text
-              variant="button"
-              align="center"
-              bold
-              color={
-                disabled
-                  ? 'disabled'
-                  : variant === 'contained'
-                  ? theme.helpers.getContrastText(theme.palette[color].main)
-                  : color
-              }
-            >
+            <Text variant="button" align="center" bold color={textColor}>
               {label}
             </Text>
           )}
           {iconPlacement === 'right' &&
             icon &&
             React.cloneElement(icon, {
-              color: disabled
-                ? 'disabled'
-                : variant === 'contained'
-                ? theme.helpers.getContrastText(theme.palette[color].main)
-                : color,
+              color: textColor,
             })}
         </Stack>
       </Pressable>
