cff53bbdbf370e125f4e6cad60245022d2b06cef
Joseph Marbella <dieptuber@gmail.com>
2024-02-05 11:51:37 -0700

    feat: make custom bottom sheet suspense

--- apps/mobile/src/components/CustomBottomSheet/index.tsx
+++ apps/mobile/src/components/CustomBottomSheet/index.tsx
@@ -1,13 +1,21 @@
 // export { default as CustomBottomSheet } from './CustomBottomSheet';
 import { BottomSheetMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
 import React from 'react';
-import { register } from 'react-native-bundle-splitter';
-export const CustomBottomSheet = register<CustomBottomSheetProps>({
-  loader: () => import('./CustomBottomSheet'),
-}) as unknown as React.ForwardRefExoticComponent<
-  CustomBottomSheetProps & React.RefAttributes<BottomSheetMethods>
->;
-import { BottomSheetProps } from '@gorhom/bottom-sheet';
+const LazyCustomBottomSheet = React.lazy(() => import('./CustomBottomSheet'));
+
+function CustomBottomSheetWrapper(
+  props: CustomBottomSheetProps,
+  ref: React.ForwardedRef<BottomSheetMethods>,
+) {
+  return (
+    <React.Suspense>
+      <LazyCustomBottomSheet ref={ref} {...props} />
+    </React.Suspense>
+  );
+}
+
+export const CustomBottomSheet = React.forwardRef(CustomBottomSheetWrapper);
+import type { BottomSheetProps } from '@gorhom/bottom-sheet';
 
 type SelectivePartial<T, K extends keyof T, U = Pick<T, K>> = Omit<T, K> & {
   [L in keyof U]?: U[L];
