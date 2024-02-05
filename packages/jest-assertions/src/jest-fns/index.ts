de7b383d2121feed0d86fdb968c67ae54d4de74e
Joseph Marbella <dieptuber@gmail.com>
2024-02-05 14:26:38 -0700

    fix: imports

---
+++ packages/jest-assertions/src/jest-fns/index.ts
@@ -0,0 +1,3 @@
+export { default as assertArrayType } from './assertArrayType';
+export { default as assertObjectType } from './assertObjectType';
+export { default as assertType } from './assertType';
