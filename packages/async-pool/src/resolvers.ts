9024883af6a514bbf748654f7586e4523c5ac248
Joseph Marbella <dieptuber@gmail.com>
2024-02-25 02:17:37 -0700

    feat: create `MapFn` type

--- packages/async-pool/src/resolvers.ts
+++ packages/async-pool/src/resolvers.ts
@@ -5,3 +5,5 @@ export function promiseAllResolver<T>(arr: Promise<T>[]) {
 export function promiseAllSettledResolver<T>(arr: Promise<T>[]) {
   return Promise.allSettled(arr);
 }
+
+export type MapFn<T, R> = (element: T, i: number, self: T[]) => Promise<R>;
