9a06aa885701165cc6bca2ee204caad1f9120a06
Joseph Marbella <dieptuber@gmail.com>
2024-02-28 13:10:52 -0700

    todo

---
+++ apps/mobile/src/components/Text/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+export const variants = StyleSheet.create({
+  h3: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  h2: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  body: {
+    fontSize: 14,
+    fontWeight: 'normal',
+  },
+});
