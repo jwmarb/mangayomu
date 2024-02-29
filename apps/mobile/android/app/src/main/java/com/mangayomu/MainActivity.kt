741e754c791631ad36396bf7f6591c4e24ad77c4
Joseph Marbella <dieptuber@gmail.com>
2024-02-28 22:18:02 -0700

    feat: render app behind status bar

--- apps/mobile/android/app/src/main/java/com/mangayomu/MainActivity.kt
+++ apps/mobile/android/app/src/main/java/com/mangayomu/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.mangayomu
 
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -19,4 +25,21 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+      val layoutParams: WindowManager.LayoutParams = WindowManager.LayoutParams()
+      layoutParams.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+      this.window.attributes = layoutParams
+    }
+    this.window.statusBarColor = Color.TRANSPARENT
+    this.window.navigationBarColor = Color.TRANSPARENT
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+      this.window.setDecorFitsSystemWindows(false);
+    } else {
+      @Suppress("DEPRECATION")
+      this.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    }
+    super.onCreate(savedInstanceState)
+  }
 }
