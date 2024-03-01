d7bf05cb2bfb544cc7cdf6ef5868c6d4152b83ab
Joseph Marbella <dieptuber@gmail.com>
2024-03-01 02:57:03 -0700

    feat(docs): add documentation

--- apps/mobile/src/hooks/useDarkModeSetter.ts
+++ apps/mobile/src/hooks/useDarkModeSetter.ts
@@ -1,6 +1,10 @@
 import React from 'react';
 import { ThemeSetDarkModeContext } from '../providers/theme';
 
+/**
+ * A hook that gets the setter for isDarkMode
+ * @returns Returns a setter for modifying isDarkMode
+ */
 export default function useDarkModeSetter() {
   const context = React.useContext(ThemeSetDarkModeContext);
   if (context == null)
