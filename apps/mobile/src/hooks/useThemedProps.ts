f7abf47abee9552b6c6384f5b788029e369e4cd3
Joseph Marbella <dieptuber@gmail.com>
2024-03-03 20:48:08 -0700

    feat: add `useThemedProps`

---
+++ apps/mobile/src/hooks/useThemedProps.ts
@@ -0,0 +1,20 @@
+import useIsDarkMode from './useIsDarkMode';
+
+// Converts any boolean expression to a number...
+function toInt(b: unknown) {
+  return b ? 1 : 0;
+}
+
+/**
+ * Themed props with the proper theme applied to it
+ * @param themedProps Props that have been themed
+ * @param isContrast Determines whether the props should use an opposite theme
+ * @returns Returns props with the theme applied to it
+ */
+export default function useThemedProps<T>(
+  themedProps: readonly [T, T],
+  isContrast = false,
+) {
+  const isDarkTheme = useIsDarkMode();
+  return themedProps[isContrast ? toInt(!isDarkTheme) : toInt(isDarkTheme)];
+}
