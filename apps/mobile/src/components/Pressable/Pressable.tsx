036d44eb54095588b9f51289a742719f19e33cb2
Joseph Marbella <dieptuber@gmail.com>
2024-03-03 20:53:34 -0700

    feat(docs): add documentation

--- apps/mobile/src/components/Pressable/Pressable.tsx
+++ apps/mobile/src/components/Pressable/Pressable.tsx
@@ -19,6 +19,10 @@ export type PressableProps = NativePressableProps & {
   contrast?: boolean;
 };
 
+/**
+ * A basic Pressable component with essential theming applied to it. It is the fundamental building block of every component that requires user interaction.
+ * @returns
+ */
 export default function Pressable(props: PressableProps) {
   const { androidRipple } = useThemedProps(themedProps, props.contrast);
   props.android_ripple = {
