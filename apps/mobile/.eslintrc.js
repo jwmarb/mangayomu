d96908829da184d0112f4c68a851cb1db00e4ff9
Joseph Marbella <dieptuber@gmail.com>
2024-03-05 21:26:26 -0700

    feat: add eslint rule regarding imports

--- apps/mobile/.eslintrc.js
+++ apps/mobile/.eslintrc.js
@@ -4,5 +4,37 @@
  */
 module.exports = {
   extends: '../../.eslintrc.js',
-  ignorePatterns: ['metro.config.js', 'index.js'],
+  ignorePatterns: ['metro.config.js', 'index.js', 'cosmos.imports.ts'],
+  plugins: ['import'],
+  overrides: [
+    {
+      files: ['**/index.ts'],
+      rules: {
+        'no-restricted-imports': 'off',
+      },
+    },
+  ],
+  rules: {
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['.*'],
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'index',
+          'sibling',
+          'parent',
+          'internal',
+          'external',
+          'builtin',
+          'object',
+          'type',
+        ],
+      },
+    ],
+  },
 };
