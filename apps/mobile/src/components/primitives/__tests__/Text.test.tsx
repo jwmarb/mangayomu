829250db3c9d6a4c113e4957b03c88b14b04364b
Joseph Marbella <dieptuber@gmail.com>
2024-03-07 13:46:08 -0700

    feat(tests): update to use new style props

--- apps/mobile/src/components/primitives/__tests__/Text.test.tsx
+++ apps/mobile/src/components/primitives/__tests__/Text.test.tsx
@@ -1,13 +1,22 @@
 import 'react-native';
 import React from 'react';
 import { it } from '@jest/globals';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react-native';
 import Text from '@/components/primitives/Text';
 
 jest.mock('react-native-mmkv');
 
 it('renders correctly', () => {
-  const tree = renderer.create(<Text>Hello World</Text>).toJSON();
+  const tree = render(<Text>Hello World</Text>);
   expect(tree).toMatchSnapshot();
-  expect(tree).toHaveTextContent('Hello World');
+  expect(tree.root).toHaveTextContent('Hello World');
+});
+
+it('correctly has styles', () => {
+  const tree = render(
+    <Text bold italic>
+      Hello World
+    </Text>,
+  );
+  expect(tree.root).toHaveStyle({ fontWeight: 'bold', fontStyle: 'italic' });
 });
