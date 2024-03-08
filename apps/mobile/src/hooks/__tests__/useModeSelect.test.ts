0f3ef3844fd83b4fd3c8362142c71bb69eb170a3
Joseph Marbella <dieptuber@gmail.com>
2024-03-07 22:15:22 -0700

    feat(tests): add for coverage

--- apps/mobile/src/hooks/__tests__/useModeSelect.test.ts
+++ apps/mobile/src/hooks/__tests__/useModeSelect.test.ts
@@ -1,5 +1,6 @@
 import { useSelect } from 'react-cosmos/client';
 import React from 'react';
+import { StatusBar } from 'react-native';
 import useModeSelect from '@/hooks/useModeSelect';
 import useIsDarkMode from '@/hooks/useIsDarkMode';
 
@@ -18,16 +19,22 @@ jest.mock('@/hooks/useIsDarkMode', () => ({
 // It is not necessary to change StatusBar in a testing environment like this
 jest.spyOn(React, 'useEffect').mockImplementation((f) => f());
 
+jest.spyOn(StatusBar, 'setBarStyle').mockImplementation();
+
 test('uses system theme', () => {
-  mockedUseIsDarkMode.mockReturnValue('dark');
+  mockedUseIsDarkMode.mockReturnValue(true);
   mockedUseSelect.mockReturnValue(['system']);
 
   expect(useModeSelect()).toEqual(undefined);
+  expect(StatusBar.setBarStyle).toHaveBeenCalledWith('light-content');
 
-  mockedUseIsDarkMode.mockReturnValue('light');
+  mockedUseIsDarkMode.mockReturnValue(false);
   mockedUseSelect.mockReturnValue(['system']);
 
   expect(useModeSelect()).toEqual(undefined);
+  expect(StatusBar.setBarStyle).toHaveBeenCalledWith<
+    Parameters<typeof StatusBar.setBarStyle>
+  >('dark-content');
 });
 
 test('uses dark theme when device is light theme', () => {
@@ -35,6 +42,7 @@ test('uses dark theme when device is light theme', () => {
   mockedUseSelect.mockReturnValue(['dark']);
 
   expect(useModeSelect()).toEqual(true); // contrast true to get dark theme
+  expect(StatusBar.setBarStyle).toHaveBeenCalledWith('light-content');
 });
 
 test('uses light theme when device is light theme', () => {
@@ -42,6 +50,7 @@ test('uses light theme when device is light theme', () => {
   mockedUseSelect.mockReturnValue(['light']);
 
   expect(useModeSelect()).toEqual(false); // contrast false to get light theme
+  expect(StatusBar.setBarStyle).toHaveBeenCalledWith('dark-content');
 });
 
 test('uses light theme when device is dark theme', () => {
@@ -49,6 +58,7 @@ test('uses light theme when device is dark theme', () => {
   mockedUseSelect.mockReturnValue(['light']);
 
   expect(useModeSelect()).toEqual(true); // contrast true to get light theme
+  expect(StatusBar.setBarStyle).toHaveBeenCalledWith('dark-content');
 });
 
 test('uses dark theme when device is dark theme', () => {
@@ -56,4 +66,5 @@ test('uses dark theme when device is dark theme', () => {
   mockedUseSelect.mockReturnValue(['dark']);
 
   expect(useModeSelect()).toEqual(false); // contrast false to get dark theme
+  expect(StatusBar.setBarStyle).toHaveBeenCalledWith('light-content');
 });
