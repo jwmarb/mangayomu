9f201115aab7c0aa5062c5897b7636e4917da410
Joseph Marbella <dieptuber@gmail.com>
2024-03-09 14:22:48 -0700

    feat: type safety for `useNavigation`

--- apps/mobile/src/screens/navigator.ts
+++ apps/mobile/src/screens/navigator.ts
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+/* eslint-disable @typescript-eslint/no-namespace */
 // This is the root navigator for all screens
 import {
   NativeStackScreenProps,
   createNativeStackNavigator,
 } from '@react-navigation/native-stack';
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
 
 export type RootStackParamList = {
   Home: undefined;
