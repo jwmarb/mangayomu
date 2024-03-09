128ed71e4344b56153794efcab3b57f7505d74bc
Joseph Marbella <dieptuber@gmail.com>
2024-03-09 00:10:32 -0700

    feat: `react-native-gesture-handler` gesture provider

--- apps/mobile/src/providers/app.tsx
+++ apps/mobile/src/providers/app.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
 import { ThemeProvider } from '@/providers/theme';
 
 export type AppProviderProps = React.PropsWithChildren;
 
+const styles = StyleSheet.create({
+  gestureHandlerRootView: { flex: 1 },
+});
+
 export default function AppProvider({ children }: AppProviderProps) {
   return (
-    <SafeAreaProvider>
-      <ThemeProvider>{children}</ThemeProvider>
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={styles.gestureHandlerRootView}>
+      <SafeAreaProvider>
+        <ThemeProvider>{children}</ThemeProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
