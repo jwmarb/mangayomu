3b921d8fe4c64a436b6436d8691e439b836e5733
Joseph Marbella <dieptuber@gmail.com>
2024-03-09 17:40:26 -0700

    feat: animated component

---
+++ apps/mobile/src/components/animated/SafeAreaView.ts
@@ -0,0 +1,6 @@
+import Animated from 'react-native-reanimated';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const AnimatedSafeAreaView = Animated.createAnimatedComponent(SafeAreaView);
+
+export default AnimatedSafeAreaView;
