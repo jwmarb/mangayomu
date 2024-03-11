42932032bc5581d1581b1600573881bcf8fc8fa8
Joseph Marbella <dieptuber@gmail.com>
2024-03-11 00:27:49 -0700

    fix: hide normal header

--- apps/mobile/App.main.tsx
+++ apps/mobile/App.main.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { RootStack } from '@/screens/navigator';
 import Home from '@/screens/Home';
 
+const HOME_SCREEN_OPTIONS = { headerShown: false };
+
 function App(): React.JSX.Element {
   return (
     <RootStack.Navigator initialRouteName="Home">
-      <RootStack.Screen name="Home" component={Home} />
+      <RootStack.Screen
+        name="Home"
+        component={Home}
+        options={HOME_SCREEN_OPTIONS}
+      />
     </RootStack.Navigator>
   );
 }
