9792613273a16f3e1776056d9a968d3324826329
Joseph Marbella <dieptuber@gmail.com>
2024-03-13 15:47:52 -0700

    todo: finish `useExplorerStore` state

--- apps/mobile/src/stores/explore.ts
+++ apps/mobile/src/stores/explore.ts
@@ -4,13 +4,32 @@ import { MangaSource } from '@mangayomu/mangascraper';
 import { createPersistConfig } from '@/utils/persist';
 
 type ExploreState = {
-  sources: MangaSource[];
+  pinnedSources: MangaSource[];
+  pinSource(source: string): void;
+  unpinSource(source: string): void;
 };
 
 const useExploreStore = create(
   persist<ExploreState>(
     (set, get) => ({
-      sources: [],
+      pinnedSources: [],
+      pinSource(source: string) {
+        get().pinnedSources.push(MangaSource.getSource(source) as MangaSource);
+        set({
+          pinnedSources: Array.from(get().pinnedSources),
+        });
+      },
+      unpinSource(source: string) {
+        get().pinnedSources.splice(
+          get().pinnedSources.indexOf(
+            MangaSource.getSource(source) as MangaSource,
+          ),
+          1,
+        );
+        set({
+          pinnedSources: Array.from(get().pinnedSources),
+        });
+      },
     }),
     createPersistConfig({
       name: 'explore',
