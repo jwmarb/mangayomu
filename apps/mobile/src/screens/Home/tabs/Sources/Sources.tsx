15bc0d5cb831510111e5e76b394d068a15d92e77
Joseph Marbella <dieptuber@gmail.com>
2024-03-13 15:48:10 -0700

    todo: finish screen

--- apps/mobile/src/screens/Home/tabs/Sources/Sources.tsx
+++ apps/mobile/src/screens/Home/tabs/Sources/Sources.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { View } from 'react-native';
 import Screen from '@/components/primitives/Screen';
 import useCollapsibleHeader from '@/hooks/useCollapsibleHeader';
+import Text from '@/components/primitives/Text';
+
+type MangaSourceListItem = { name: string } | string;
 
 export default function Sources() {
   const collapsible = useCollapsibleHeader({ title: 'Sources' });
   return (
     <Screen collapsible={collapsible}>
+      <Text variant="h4">Pinned Sources</Text>
+      <Text variant="h4">All Sources</Text>
       <View style={{ height: 2000 }} />
     </Screen>
   );
