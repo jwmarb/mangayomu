56d509e7def5cf6bffa427160cee5f8621e2e198
Joseph Marbella <dieptuber@gmail.com>
2024-03-18 15:46:29 -0700

    feat(deps): add react query

--- apps/mobile/src/providers/app.tsx
+++ apps/mobile/src/providers/app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StyleSheet } from 'react-native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@/providers/theme';
 
 export type AppProviderProps = React.PropsWithChildren;
@@ -10,11 +11,15 @@ const styles = StyleSheet.create({
   gestureHandlerRootView: { flex: 1 },
 });
 
+const queryClient = new QueryClient();
+
 export default function AppProvider({ children }: AppProviderProps) {
   return (
     <GestureHandlerRootView style={styles.gestureHandlerRootView}>
       <SafeAreaProvider>
-        <ThemeProvider>{children}</ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider>{children}</ThemeProvider>
+        </QueryClientProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
   );
