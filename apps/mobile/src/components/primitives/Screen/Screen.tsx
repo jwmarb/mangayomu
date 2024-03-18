2004d6b2c882c043a0c161363dbfccf9e1eb7b71
Joseph Marbella <dieptuber@gmail.com>
2024-03-18 02:40:32 -0700

    feat: screen lists

--- apps/mobile/src/components/primitives/Screen/Screen.tsx
+++ apps/mobile/src/components/primitives/Screen/Screen.tsx
@@ -1,18 +1,20 @@
 import Animated, { AnimatedScrollViewProps } from 'react-native-reanimated';
+import { FlatListProps, SectionListProps } from 'react-native';
 import useCollapsibleHeader from '@/hooks/useCollapsibleHeader';
 import { styles } from '@/components/primitives/Screen/styles';
 import useStyles from '@/hooks/useStyles';
 import useContrast from '@/hooks/useContrast';
+import AnimatedSectionList from '@/components/animated/SectionList';
 
 export type ScreenProps = AnimatedScrollViewProps & {
   collapsible: ReturnType<typeof useCollapsibleHeader>;
   contrast?: boolean;
 };
 
-export default function Screen(props: ScreenProps) {
+function Screen(props: ScreenProps) {
   const {
     collapsible,
-    style: styleProp,
+    contentContainerStyle: styleProp,
     contrast: contrastProp,
     ...rest
   } = props;
@@ -28,3 +30,39 @@ export default function Screen(props: ScreenProps) {
     />
   );
 }
+
+Screen.FlatList = function <T>(
+  props: FlatListProps<T> & Omit<ScreenProps, keyof AnimatedScrollViewProps>,
+) {
+  const { collapsible, contrast: contrastProp, ...rest } = props;
+
+  const contrast = useContrast(contrastProp);
+  const style = useStyles(styles, contrast);
+
+  return (
+    <Animated.FlatList
+      onScroll={collapsible.onScroll}
+      contentContainerStyle={style.contentContainerStyle}
+      {...rest}
+    />
+  );
+};
+
+Screen.SectionList = function <T>(
+  props: SectionListProps<T> & Omit<ScreenProps, keyof AnimatedScrollViewProps>,
+) {
+  const { collapsible, contrast: contrastProp, ...rest } = props;
+
+  const contrast = useContrast(contrastProp);
+  const style = useStyles(styles, contrast);
+
+  return (
+    <AnimatedSectionList
+      onScroll={collapsible.onScroll}
+      contentContainerStyle={style.contentContainerStyle}
+      {...rest}
+    />
+  );
+};
+
+export default Screen;
