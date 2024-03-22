f57fe6f010cb61f4435e2de2e7559ca0a82ec538
Joseph Marbella <dieptuber@gmail.com>
2024-03-21 21:49:24 -0700

    feat: create `Browse` screen

--- apps/mobile/src/screens/Home/tabs/Browse/Browse.tsx
+++ apps/mobile/src/screens/Home/tabs/Browse/Browse.tsx
@@ -1,20 +1,47 @@
-import { View } from 'react-native';
+import { ListRenderItem, View } from 'react-native';
 import React from 'react';
 import { useFocusEffect } from '@react-navigation/native';
 import { TextInput as NativeTextInput } from 'react-native-gesture-handler';
+import { MangaSource } from '@mangayomu/mangascraper';
 import Icon from '@/components/primitives/Icon';
 import Screen from '@/components/primitives/Screen';
-import Text from '@/components/primitives/Text';
 import TextInput from '@/components/primitives/TextInput';
 import useCollapsibleHeader from '@/hooks/useCollapsibleHeader';
 import { useBrowseStore } from '@/stores/browse';
+import { useExploreStore } from '@/stores/explore';
+import MangaBrowseList from '@/screens/Home/tabs/Browse/components/MangaBrowseList';
+import Divider from '@/components/primitives/Divider';
+import { createStyles } from '@/utils/theme';
+import useStyles from '@/hooks/useStyles';
+import useContrast from '@/hooks/useContrast';
+
+const styles = createStyles((theme) => ({
+  divider: {
+    marginVertical: theme.style.size.m,
+  },
+}));
+
+const renderItem: ListRenderItem<MangaSource> = ({ item }) => (
+  <MangaBrowseList source={item} />
+);
+
+const keyExtractor = (item: MangaSource) => item.NAME;
+
+function ItemSeparatorComponent() {
+  const contrast = useContrast();
+  const style = useStyles(styles, contrast);
+  return <Divider style={style.divider} />;
+}
 
 export default function Browse() {
   const setQuery = useBrowseStore((state) => state.setQuery);
+  const pinnedSources = useExploreStore((state) => state.pinnedSources);
   const inputRef = React.useRef<NativeTextInput>(null);
+
   const collapsible = useCollapsibleHeader({
     showHeaderLeft: false,
     showHeaderRight: false,
+    disableCollapsing: true,
     headerCenter: (
       <TextInput
         ref={inputRef}
@@ -30,9 +57,12 @@ export default function Browse() {
     }, []),
   );
   return (
-    <Screen collapsible={collapsible}>
-      <Text variant="h4">MangaSee</Text>
-      <View style={{ height: 2000 }} />
-    </Screen>
+    <Screen.FlatList
+      data={pinnedSources}
+      ItemSeparatorComponent={ItemSeparatorComponent}
+      keyExtractor={keyExtractor}
+      renderItem={renderItem}
+      collapsible={collapsible}
+    />
   );
 }
