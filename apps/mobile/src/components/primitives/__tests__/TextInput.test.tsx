49b080c126ddf4d920f991ca13422eea17052e3a
Joseph Marbella <dieptuber@gmail.com>
2024-03-27 11:10:32 -0700

    tests(rollback): remove snapshot test tests: more `TextInput` tests

--- apps/mobile/src/components/primitives/__tests__/TextInput.test.tsx
+++ apps/mobile/src/components/primitives/__tests__/TextInput.test.tsx
@@ -1,5 +1,8 @@
 import { render, userEvent } from '@testing-library/react-native';
 import TextInput from '@/components/primitives/TextInput';
+import Icon from '@/components/primitives/Icon';
+import IconButton from '@/components/primitives/IconButton';
+import { styles } from '@/components/primitives/TextInput/styles';
 
 beforeAll(() => {
   jest.useFakeTimers();
@@ -11,14 +14,11 @@ afterAll(() => {
 
 test('renders correctly', () => {
   const { root } = render(<TextInput />);
-  expect(root).toMatchSnapshot();
+  expect(root).toBeOnTheScreen();
 });
 
 test('clears text properly', async () => {
-  let value = '';
-  function onChangeText(e: string) {
-    value = e;
-  }
+  const onChangeText = jest.fn((e) => e);
   const tree = render(
     <TextInput
       testID="input"
@@ -31,13 +31,40 @@ test('clears text properly', async () => {
 
   await user.type(inputEl, 'hello world!');
 
-  jest.runOnlyPendingTimers();
-
-  expect(value).toEqual('hello world!');
+  expect(onChangeText).toHaveReturnedWith('hello');
+  expect(onChangeText).toHaveLastReturnedWith('hello world!');
 
   const clearInput = tree.getByTestId('right');
 
   await user.press(clearInput);
 
-  expect(value).toEqual('');
+  jest.runOnlyPendingTimers();
+
+  expect(onChangeText).toHaveLastReturnedWith('');
+
+  expect(inputEl).toHaveStyle(styles[0].containerNoIcon);
+});
+
+test('displays icon & iconButton properly', async () => {
+  const iconEl = <Icon type="icon" name="ab-testing" testID="icon" />;
+  let tree = render(<TextInput icon={iconEl} testID="input" />);
+  expect(tree.getByTestId('icon')).toBeOnTheScreen();
+  expect(tree.getByTestId('input')).toHaveStyle(styles[0].container);
+
+  const user = userEvent.setup();
+  const onPress = jest.fn();
+  tree = render(
+    <TextInput
+      icon={<IconButton icon={iconEl} onPress={onPress} testID="iconBtn" />}
+      iconButton
+      testID="input"
+    />,
+  );
+
+  expect(tree.getByTestId('input')).toHaveStyle(styles[0].containerIconButton);
+
+  await user.press(tree.getByTestId('iconBtn'));
+  expect(onPress).toHaveBeenCalled();
+
+  jest.runOnlyPendingTimers();
 });
