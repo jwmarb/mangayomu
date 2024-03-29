d01a551f21dc13d11a42ed72ab8c55f89ffddb33
Joseph Marbella <dieptuber@gmail.com>
2024-03-28 18:46:29 -0700

    feat: add back handler to close `BottomSheet`

--- apps/mobile/src/components/composites/BottomSheet/BottomSheet.tsx
+++ apps/mobile/src/components/composites/BottomSheet/BottomSheet.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import GorhomBottomSheet from '@gorhom/bottom-sheet';
+import { BackHandler } from 'react-native';
 import useContrast from '@/hooks/useContrast';
 import useStyles from '@/hooks/useStyles';
 import { styles } from '@/components/composites/BottomSheet/styles';
+import useBoolean from '@/hooks/useBoolean';
 
 export type BottomSheet = {
   open: () => void;
@@ -22,6 +24,7 @@ function BottomSheetComponent(
 ) {
   const { children, contrast: contrastProp } = props;
   const bottomSheetRef = React.useRef<GorhomBottomSheet>(null);
+  const [hidden, toggle] = useBoolean();
   const contrast = useContrast(contrastProp);
   const style = useStyles(styles, contrast);
   React.useImperativeHandle(ref, () => ({
@@ -32,8 +35,31 @@ function BottomSheetComponent(
       bottomSheetRef.current?.close();
     },
   }));
+
+  function handleOnChange(index: number) {
+    if (index === -1) toggle(true);
+    else toggle(false);
+  }
+
+  React.useEffect(() => {
+    const subscription = BackHandler.addEventListener(
+      'hardwareBackPress',
+      () => {
+        if (!hidden) {
+          bottomSheetRef.current?.close();
+          return true;
+        }
+        return false;
+      },
+    );
+    return () => {
+      subscription.remove();
+    };
+  }, [hidden]);
+
   return (
     <GorhomBottomSheet
+      onChange={handleOnChange}
       ref={bottomSheetRef}
       backgroundStyle={style.backgroundStyle}
       snapPoints={SNAP_POINTS}
