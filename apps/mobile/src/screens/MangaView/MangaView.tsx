fe93ec6bbaa199c65e4879cbf9fd3315155d54fb
Joseph Marbella <dieptuber@gmail.com>
2024-04-01 15:48:59 -0700

    todo

--- apps/mobile/src/screens/MangaView/MangaView.tsx
+++ apps/mobile/src/screens/MangaView/MangaView.tsx
@@ -1,5 +1,18 @@
+import Screen from '@/components/primitives/Screen';
+import useCollapsibleHeader from '@/hooks/useCollapsibleHeader';
+import useManga from '@/hooks/useManga';
+import useMangaSource from '@/hooks/useMangaSource';
 import { RootStackProps } from '@/screens/navigator';
 
 export default function MangaView(props: RootStackProps<'MangaView'>) {
-  return null;
+  const {
+    navigation,
+    route: {
+      params: { manga: unparsedManga, source: sourceStr },
+    },
+  } = props;
+  // const source = useMangaSource(sourceStr ?? unparsedManga.__source__);
+  // const manga = useManga(unparsedManga);
+  // const collapsible = useCollapsibleHeader({ title: })
+  return <Screen.FlatList data={[]} renderItem={() => null} />;
 }
