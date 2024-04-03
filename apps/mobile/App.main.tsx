dcbae04a359c3b49d74ad809be3b3e360c9bb768
Joseph Marbella <dieptuber@gmail.com>
2024-04-03 02:18:39 -0700

    feat: initialize image module

--- apps/mobile/App.main.tsx
+++ apps/mobile/App.main.tsx
@@ -3,10 +3,15 @@ import { RootStack } from '@/screens/navigator';
 import Home from '@/screens/Home';
 import MangaView from '@/screens/MangaView';
 import SourceBrowser from '@/screens/SourceBrowser';
+import { initialize } from '@/utils/image';
+import ExtendedMangaList from '@/screens/ExtendedMangaList';
 
 const HOME_SCREEN_OPTIONS = { headerShown: false };
 
 function App(): React.JSX.Element {
+  React.useEffect(() => {
+    initialize();
+  }, []);
   return (
     <RootStack.Navigator initialRouteName="Home">
       <RootStack.Screen
@@ -16,6 +21,10 @@ function App(): React.JSX.Element {
       />
       <RootStack.Screen name="MangaView" component={MangaView} />
       <RootStack.Screen name="SourceBrowser" component={SourceBrowser} />
+      <RootStack.Screen
+        name="ExtendedMangaList"
+        component={ExtendedMangaList}
+      />
     </RootStack.Navigator>
   );
 }
