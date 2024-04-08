c50964f6244afddb999b66bded1b477d61ddb640
Joseph Marbella <dieptuber@gmail.com>
2024-04-08 12:24:47 -0700

    feat: listen to changes for localmanga only feat: `onInitialize` to execute

--- apps/mobile/src/hooks/useLocalManga.ts
+++ apps/mobile/src/hooks/useLocalManga.ts
@@ -1,5 +1,5 @@
 import { Manga } from '@mangayomu/mangascraper';
-import { Q } from '@nozbe/watermelondb';
+import { Model, Q } from '@nozbe/watermelondb';
 import { useDatabase } from '@nozbe/watermelondb/react';
 import React from 'react';
 import { Table } from '@/models/schema';
@@ -10,24 +10,51 @@ type Selector<T> = (model: LocalManga) => T;
 export default function useLocalManga<T>(
   manga: Pick<Manga, 'link'>,
   selector: Selector<T>,
+  onInitialize?: (model: LocalManga) => void,
 ) {
   const database = useDatabase();
   const [state, setState] = React.useState<T | undefined>(undefined);
+  const id = React.useRef<string>('');
   React.useEffect(() => {
-    const observer = database
-      .get(Table.LOCAL_MANGAS)
-      .query(Q.where('link', manga.link))
-      .observe();
-    const subscription = observer.subscribe((updated) => {
-      if (updated.length > 0) {
-        const [localManga] = updated;
-        setState(selector(localManga as LocalManga));
-      }
-    });
+    function initialize() {
+      const observer = database
+        .get(Table.LOCAL_MANGAS)
+        .query(Q.where('link', manga.link))
+        .observe();
+      const callback = (updated: Model[]) => {
+        if (updated.length > 0) {
+          const [localManga] = updated;
+          id.current = localManga.id;
+          setState(selector(localManga as LocalManga));
+          if (onInitialize != null) onInitialize(localManga as LocalManga);
+        }
+      };
+      const subscription = observer.subscribe(callback);
 
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, []);
+      return () => {
+        subscription.unsubscribe();
+      };
+    }
+
+    async function listenToChanges() {
+      const observer = database
+        .get(Table.LOCAL_MANGAS)
+        .findAndObserve(id.current);
+
+      const callback = (updated: Model) => {
+        setState(selector(updated as LocalManga));
+      };
+      const subscription = observer.subscribe(callback);
+      return () => {
+        subscription.unsubscribe();
+      };
+    }
+    if (state == null) {
+      initialize();
+    } else {
+      listenToChanges();
+    }
+  }, [state != null]);
+
   return state;
 }
