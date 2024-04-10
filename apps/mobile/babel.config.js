fc234bd91abafc98e47c20a10f63fd38e8b378ef
Joseph Marbella <dieptuber@gmail.com>
2024-04-09 22:01:50 -0700

    feat(deps): add `react-native-worklets-core`

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    ['react-native-worklets-core/plugin'],
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     [
       'module-resolver',
