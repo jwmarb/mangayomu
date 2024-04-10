8ed4474c492b436bf499c0e35b4311ea445cdff2
Joseph Marbella <dieptuber@gmail.com>
2024-04-10 12:14:42 -0700

    feat: hook to access row in db

--- apps/mobile/src/models/Manga.ts
+++ apps/mobile/src/models/Manga.ts
@@ -1,13 +1,18 @@
 import { date, field, relation } from '@nozbe/watermelondb/decorators';
-import { Model, Relation } from '@nozbe/watermelondb';
+import { Collection, Database, Model, Q, Relation } from '@nozbe/watermelondb';
 import { Associations } from '@nozbe/watermelondb/Model';
+import { Manga as MManga } from '@mangayomu/mangascraper';
+import { useDatabase } from '@nozbe/watermelondb/react';
+import React from 'react';
 import type { ISOLangCode } from '@mangayomu/language-codes';
 import {
   CURRENTLY_READING_CHAPTER_ID,
   ImageScaling,
   ReadingDirection,
   ReadingOrientation,
+  Selector,
   Table,
+  UseRowOptions,
   ZoomStartPosition,
 } from '@/models/schema';
 import { Chapter } from '@/models/Chapter';
@@ -19,6 +24,7 @@ export class Manga extends Model {
   };
 
   @field('title') title!: string;
+  @field('link') link!: string;
   @relation(Table.CHAPTERS, CURRENTLY_READING_CHAPTER_ID)
   currentlyReadingChapter?: Relation<Chapter>;
   @date('created_at') createdAt!: Date;
@@ -26,9 +32,9 @@ export class Manga extends Model {
 
   // UNIX timestamp for algorithms
   @field('date_added_in_library') dateAddedInLibrary?: number | null;
-  @field('is_in_library') isInLibrary!: boolean;
+  @field('is_in_library') isInLibrary!: 1 | 0;
 
-  @field('selected_language') selectedLanguage!: ISOLangCode;
+  @field('selected_language') selectedLanguage?: ISOLangCode | null;
 
   @field('reading_direction') readingDirection!: ReadingDirection;
   @field('page_image_scaling') imageScaling!: ImageScaling;
@@ -36,4 +42,67 @@ export class Manga extends Model {
   @field('reading_orientation') readingOrientation!: ReadingOrientation;
 
   @field('new_chapters_count') newChaptersCount!: number;
+
+  /**
+   * Returns an existing manga row or creates and returns it the row does not exist
+   * @param manga A manga object from a manga source
+   * @param database WatermelonDB database
+   * @returns
+   */
+  static async toManga(manga: MManga, database: Database) {
+    const mangas = database.get(Table.MANGAS) as Collection<Manga>;
+    const found = await mangas.query(Q.where('link', manga.link));
+
+    if (found.length === 0) {
+      return await database.write<Manga>(() => {
+        return mangas.create((_model) => {
+          const model = _model as Manga;
+          model.title = manga.title;
+          model.link = manga.link;
+          model.dateAddedInLibrary = null;
+          model.isInLibrary = 0;
+          model.selectedLanguage = manga.language;
+          model.readingDirection = ReadingDirection.DEFAULT;
+          model.imageScaling = ImageScaling.DEFAULT;
+          model.zoomStartPosition = ZoomStartPosition.DEFAULT;
+          model.readingOrientation = ReadingOrientation.DEFAULT;
+          model.newChaptersCount = 0;
+        });
+      });
+    }
+    return found[0];
+  }
+
+  static useRow<T>(
+    manga: MManga,
+    selector: Selector<T, Manga>,
+    options?: Omit<UseRowOptions<T, Manga>, 'default'>,
+  ): T | undefined {
+    const database = useDatabase();
+    const [state, setState] = React.useState<T>();
+    const init = React.useRef(false);
+
+    React.useEffect(() => {
+      async function initialize() {
+        const row = await Manga.toManga(manga, database);
+        const observer = row.observe();
+        const subscription = observer.subscribe((model: Manga) => {
+          if (!init.current) {
+            options?.onInitialize?.(model);
+            init.current = true;
+          } else {
+            options?.onUpdate?.(model);
+          }
+          setState(selector(model));
+        });
+
+        return () => {
+          subscription.unsubscribe();
+        };
+      }
+      initialize();
+    }, []);
+
+    return state;
+  }
 }
