a3e759e916c21c97f1ab98f5380602ad6716704e
Joseph Marbella <dieptuber@gmail.com>
2024-06-20 08:01:07 -0700

    tests: mock watermelondb with sqlite adapter

--- apps/mobile/jest-setup.ts
+++ apps/mobile/jest-setup.ts
@@ -1,1 +1,8 @@
 import '@testing-library/react-native/extend-expect';
+jest.mock(
+  '@nozbe/watermelondb/adapters/sqlite/makeDispatcher/index.native.js',
+  () =>
+    jest.requireActual(
+      '@nozbe/watermelondb/adapters/sqlite/makeDispatcher/index.js',
+    ),
+);
