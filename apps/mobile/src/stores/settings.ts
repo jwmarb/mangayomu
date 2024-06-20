e989e5688bc98dcf306d9bf808370b06a4985ce7
Joseph Marbella <dieptuber@gmail.com>
2024-06-20 07:58:00 -0700

    refactor: use enums exported from `schema.ts`

--- apps/mobile/src/stores/settings.ts
+++ apps/mobile/src/stores/settings.ts
@@ -1,31 +1,39 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
-import {
-  ReadingDirection,
-  ImageScaling,
-  ReaderScreenOrientation,
-  ZoomStartPosition,
-} from '@mangayomu/schemas';
 import { createPersistConfig } from '@/utils/persist';
+import {
+  ImageScaling,
+  ReadingDirection,
+  ReadingOrientation,
+  ZoomStartPosition,
+} from '@/models/schema';
 
 export type SettingsState = {
   reader: {
     readingDirection: ReadingDirection;
     imageScaling: ImageScaling;
-    readingOrientation: ReaderScreenOrientation;
+    readingOrientation: ReadingOrientation;
     zoomStartPosition: ZoomStartPosition;
   };
+  setReaderState: <T extends keyof SettingsState['reader']>(
+    key: T,
+    value: SettingsState['reader'][T],
+  ) => void;
 };
 
 export const useSettingsStore = create(
   persist<SettingsState>(
-    () => ({
+    (set, get) => ({
       reader: {
-        readingDirection: ReadingDirection.LEFT_TO_RIGHT,
-        imageScaling: ImageScaling.SMART_FIT,
-        readingOrientation: ReaderScreenOrientation.FREE,
-        zoomStartPosition: ZoomStartPosition.AUTOMATIC,
+        readingDirection: ReadingDirection.DEFAULT,
+        imageScaling: ImageScaling.DEFAULT,
+        readingOrientation: ReadingOrientation.DEFAULT,
+        zoomStartPosition: ZoomStartPosition.DEFAULT,
       },
+      setReaderState: <T extends keyof SettingsState['reader']>(
+        key: T,
+        value: SettingsState['reader'][T],
+      ) => set({ reader: { ...get().reader, [key]: value } }),
     }),
     createPersistConfig({
       name: 'settings',
