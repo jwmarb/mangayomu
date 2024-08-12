83ca220ff52a2496c931e7d5b87448786729e97f
Joseph Marbella <dieptuber@gmail.com>
2024-08-12 11:52:55 -0700

    fix: updating record outside of write operation

--- apps/mobile/src/screens/Reader/hooks/useHistoryEntry.ts
+++ apps/mobile/src/screens/Reader/hooks/useHistoryEntry.ts
@@ -48,7 +48,9 @@ export default function useHistoryEntry(currentChapter: MangaChapter) {
       } else {
         const [historyEntry] = results;
         await database.write(async () => {
-          historyEntry.updatedAt = new Date();
+          await historyEntry.update((self) => {
+            self.updatedAt = new Date();
+          });
         });
       }
     }
