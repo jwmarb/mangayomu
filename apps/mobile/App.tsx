cceaf280952b52207da776ed6e249bbe5ccc5cdb
Joseph Marbella <dieptuber@gmail.com>
2024-08-12 11:42:16 -0700

    chore: remove Reanimated logs

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,7 +1,15 @@
 /* eslint-disable no-restricted-imports */
+import { LogBox } from 'react-native';
 import CosmosApp from './App.cosmos';
 import MainApp from './App.main';
 import AppProvider from '@/providers/app';
+
+console.warn = (...args: any[]) => {
+  const output = args.join(' ');
+  if (!output.includes('[Reanimated]')) {
+    console.warn(args);
+  }
+};
 
 export default function App() {
   return <AppProvider>{__DEV__ ? <CosmosApp /> : <MainApp />}</AppProvider>;
