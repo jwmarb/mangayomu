ead3fc66080773927e679c25ca66967921b52382
Joseph Marbella <dieptuber@gmail.com>
2024-09-07 18:54:59 -0700

    feat: optional side effect callback

--- apps/mobile/src/hooks/useUserInput.ts
+++ apps/mobile/src/hooks/useUserInput.ts
@@ -4,14 +4,19 @@ import useBoolean from '@/hooks/useBoolean';
 /**
  * A hook to use user input
  * @param initial The initial user input (this does not count as dirty)
+ * @param mutater A callback that modifies the string on input
  * @returns Returns an object with properties for handling user input components
  */
-export default function useUserInput(initial = '') {
+export default function useUserInput(
+  initial = '',
+  mutater: (sanitized?: string) => string | undefined = (sanitized) =>
+    sanitized,
+) {
   const [input, setInput] = React.useState<string>(initial);
   const [isDirty, toggleDirty] = useBoolean();
 
   const setter = React.useCallback((text?: string) => {
-    const sanitized = text?.trim().toLowerCase();
+    const sanitized = mutater(text?.trim().toLowerCase());
     if (sanitized != null) {
       toggleDirty(true);
       setInput(sanitized);
