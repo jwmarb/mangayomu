cf7459fa0fc1dd92af28e43ae666e47c7fc2cef1
Joseph Marbella <dieptuber@gmail.com>
2024-09-29 01:14:19 -0700

    chore: comments

--- apps/mobile/src/screens/Reader/hooks/useDeviceOrientation.ts
+++ apps/mobile/src/screens/Reader/hooks/useDeviceOrientation.ts
@@ -4,23 +4,33 @@ import { ReadingOrientation } from '@/models/schema';
 import React from 'react';
 import { Manga } from '@mangayomu/mangascraper';
 
+/**
+ * Hook to manage the device orientation based on the reading setting
+ * @param manga - The current manga being read
+ */
 export default function useDeviceOrientation(manga: Manga) {
   const { state: readingOrientation } = useReaderSetting(
     'readingOrientation',
     manga,
   );
+
+  // Effect to lock or unlock device orientation based on current setting
   React.useEffect(() => {
     switch (readingOrientation) {
       case ReadingOrientation.FREE:
+        // Allow all orientations
         orientation.unlockAllOrientations();
         break;
       case ReadingOrientation.LANDSCAPE:
+        // Lock to landscape orientation
         orientation.lockToLandscape();
         break;
       case ReadingOrientation.PORTRAIT:
         orientation.lockToPortrait();
         break;
     }
+
+    // Cleanup function to unlock all orientations on component unmount
     return () => {
       orientation.unlockAllOrientations();
     };
