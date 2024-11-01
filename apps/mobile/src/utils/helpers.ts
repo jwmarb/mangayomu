74805c2c182eb1c9e4df6edb2fae5776bcf74915
Joseph Marbella <dieptuber@gmail.com>
2024-11-01 14:49:20 -0700

    docs: comments

--- apps/mobile/src/utils/helpers.ts
+++ apps/mobile/src/utils/helpers.ts
@@ -1,18 +1,43 @@
 import { Manga, MangaChapter } from '@mangayomu/mangascraper';
 import { MangaResult } from '@/stores/explore';
 
+/**
+ * Extracts a human-readable error message from an unknown error object.
+ *
+ * This function checks the provided `err` parameter for common properties that
+ * might contain an error message, such as `message`, `msg`, and `stack`. If none
+ * of these properties are found or if they are empty, it returns a default message.
+ *
+ * @param {unknown} err - The error object to extract the message from.
+ * @returns {string} A human-readable error message.
+ */
 export function getErrorMessage(err: unknown): string {
+  // Check if `err` is a non-empty string and return it directly
   if (typeof err === 'string' && err) return err;
   if (typeof err === 'object' && err != null) {
+    // Check if the `message` property exists, is a non-empty string, and return it
     if ('message' in err && typeof err.message === 'string' && err.message)
       return err.message;
     if ('msg' in err && typeof err.msg === 'string' && err.msg) return err.msg;
     if ('stack' in err && typeof err.stack === 'string' && err.stack)
       return err.stack;
   }
+
+  // Return a default message if no error message properties are found
   return 'No error code/message has been provided';
 }
 
+/**
+ * Checks if the given object is a valid `Manga` instance.
+ *
+ * This function performs several checks to ensure that the provided object
+ * meets the criteria for being a `Manga`. It verifies the presence and types
+ * of required properties such as `source`, `title`, and `link`, and optionally
+ * checks for `imageCover` and `language`.
+ *
+ * @param {unknown} obj - The object to check.
+ * @returns {obj is Manga} A boolean indicating whether the object is a valid `Manga`.
+ */
 export function isManga(obj: unknown): obj is Manga {
   return (
     obj != null &&
