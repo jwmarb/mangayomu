6a85c916abb131775a061b865704ce687cc6d22f
Joseph Marbella <dieptuber@gmail.com>
2024-11-01 20:28:25 -0700

    feat(deps)!: upgrade to `react-native` 0.76

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    ['react-native-worklets-core/plugin'],
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     [
       'module-resolver',
