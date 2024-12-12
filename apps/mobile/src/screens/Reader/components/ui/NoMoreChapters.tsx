2e113c087e3ee0e136ecd3b04f60291cc708e4fd
Joseph Marbella <dieptuber@gmail.com>
2024-12-11 22:42:09 -0700

    fix: end of last chapter not full screen

--- apps/mobile/src/screens/Reader/components/ui/NoMoreChapters.tsx
+++ apps/mobile/src/screens/Reader/components/ui/NoMoreChapters.tsx
@@ -1,13 +1,15 @@
-import { View } from 'react-native';
+import { Dimensions, View } from 'react-native';
 import React from 'react';
 import Text from '@/components/primitives/Text';
 import useStyles from '@/hooks/useStyles';
 import { createStyles } from '@/utils/theme';
 
+const { width, height } = Dimensions.get('window');
+
 const styles = createStyles((theme) => ({
   container: {
-    width: '100%',
-    height: '100%',
+    width,
+    height,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
